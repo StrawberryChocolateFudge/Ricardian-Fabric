6b093751c0bf43c213933660672717e55452fdf3
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-02 13:03:49 +0100

    added testweave

--- lib/arweave/arweave.ts
+++ lib/arweave/arweave.ts
@@ -1,10 +1,19 @@
+import TestWeave from "../../node_modules/testweave-sdk";
+import { CreateTransaqctionResult } from "../types";
+let testweave: TestWeave;
+
 export async function getArweaveCall() {
   //@ts-ignore
   const arweave = await Arweave.init({
     host: "127.0.0.1",
     port: 1984,
     protocol: "http",
+    timeout: 20000,
+    logging: false,
   });
+
+  testweave = await TestWeave.init(arweave);
+
   await getInfoCall(arweave);
   return arweave;
 }
@@ -19,7 +28,6 @@ export async function getAddressCall(arweave: any, key: any): Promise<string> {
 
 export async function getBalanceCall(arweave: any, key: any): Promise<number> {
   const address = await getAddressCall(arweave, key);
-  console.log(address);
   const winston = await arweave.wallets.getBalance(address);
   return winston as number;
 }
@@ -27,22 +35,35 @@ export async function getBalanceCall(arweave: any, key: any): Promise<number> {
 export async function createTransactionSend(
   arweave: any,
   key: any,
-  legalContract: string
-): Promise<{ transaction: string; id: string; statusCode: number }> {
-  //TODO: DATA!
-  const transaction = await arweave.createTransaction(
+  page: string
+): Promise<CreateTransaqctionResult> {
+  const dataTransaction = await arweave.createTransaction(
     {
-      data: `<html><head><meta charset="UTF-8"><title></title></head><body>${legalContract}</body></html>`,
+      data: page,
     },
-    key
+    testweave.rootJWK
   );
+  dataTransaction.addTag("Content-Type", "text/html");
+  console.log(page);
+  await arweave.transactions.sign(dataTransaction, testweave.rootJWK);
+  const response: any = await arweave.transactions.post(dataTransaction);
 
-  await arweave.transactions.sign(transaction, key);
-  const response: any = await arweave.transactions.post(transaction);
+  await arweave.transactions.post(dataTransaction);
+  const statusAfterPost = await arweave.transactions.getStatus(
+    dataTransaction.id
+  );
+  console.log(response.status);
+  await testweave.mine();
+  console.log(`http://localhost:1984/tx/${dataTransaction.id}/data.html`);
+  await testweave.mine();
+  await testweave.mine();
+  await testweave.mine();
+  await testweave.mine();
+  await testweave.mine();
 
   return {
     statusCode: response.status,
-    transaction: transaction.last_tx,
-    id: transaction.id,
+    id: dataTransaction.id,
+    path: `http://localhost:1984/tx/${dataTransaction.id}/data.html`,
   };
 }
