80bb9e0e9f39cbb383b976617dff35dc7fe587de
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-02 13:04:06 +0100

    add createAcceptpage

--- lib/business/bloc.ts
+++ lib/business/bloc.ts
@@ -3,7 +3,18 @@ import {
   getArweaveCall,
   getBalanceCall,
 } from "../arweave/arweave";
-import { dispatch_getArweave } from "../dispatch/stateChange";
+import {
+  dispatch_getArweave,
+} from "../dispatch/stateChange";
+import { fetchDependency } from "../fetch";
+import { AcceptablePageProps } from "../types";
+import createNewEditor from "../view/editor";
+import {
+  getArweaveDependencyUrl,
+  getBundleSrcUrl,
+  getCommunityJsDependencyUrl,
+} from "../view/utils";
+import { getAcceptablePage } from "../view/vDom";
 
 export async function getArweave() {
   const arweave = await getArweaveCall();
@@ -18,8 +29,30 @@ export async function getBalance(arweave: any, key: any): Promise<number> {
 export async function createAcceptableContract(
   arweave: any,
   key: any,
-  legalContract: string
+  data: AcceptablePageProps
 ) {
-  const result = await createTransactionSend(arweave, key, legalContract);
+  const arweaveDepUrl = getArweaveDependencyUrl();
+  const communityJsDepUrl = getCommunityJsDependencyUrl();
+  const bundleSrcUrl = getBundleSrcUrl();
+  const arweaveDepCode = await fetchDependency(arweaveDepUrl);
+  const communityJsDepCode = await fetchDependency(communityJsDepUrl);
+  const bundleDepCode = await fetchDependency(bundleSrcUrl);
+  console.log(data.legalContract);
+  const page = await getAcceptablePage({
+    ...data,
+    arweaveDeps: {
+      src: arweaveDepUrl,
+      code: arweaveDepCode,
+    },
+    mainDep: {
+      src: bundleSrcUrl,
+      code: bundleDepCode,
+    },
+    communityJsDep: {
+      src: communityJsDepUrl,
+      code: communityJsDepCode,
+    },
+  });
+  const result = await createTransactionSend(arweave, key, page);
   return result;
 }
