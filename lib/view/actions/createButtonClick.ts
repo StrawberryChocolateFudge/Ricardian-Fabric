7e08f35e99471712626f01bfd7eea8b337172d86
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-03 13:58:09 +0100

    update, createButton click props getters added

--- lib/view/actions/createButtonClick.ts
+++ lib/view/actions/createButtonClick.ts
@@ -1,11 +1,11 @@
-import { createAcceptableContract } from "../../business/bloc";
+import {
+  createAcceptableContract,
+  getCreatorWallet,
+} from "../../business/bloc";
 import { State } from "../../types";
-import { getById, readFile } from "../utils";
-
+import { getById, getPrice, getRedirect, readFile } from "../utils";
 export function renderCreateButtonClick(props: State) {
   getById("save-contract").onclick = function () {
-    const price = getById("price-input") as HTMLInputElement;
-    const redirect = getById("redirect-input") as HTMLInputElement;
     const wallet_file = getById("select-file-input") as HTMLInputElement;
 
     if (wallet_file.files !== null) {
@@ -16,14 +16,13 @@ export function renderCreateButtonClick(props: State) {
       } else {
         const getKey = async (key: any) => {
           //Here I call the business logic to do stuff with the key and the other values
-          //   await getBalance(props.arweave, key);
-          console.log(key);
-          const result = await createAcceptableContract(props.arweave, key, {
+          await createAcceptableContract(props.arweave, key, {
             legalContract: props.editor.getContent(),
             createdDate: new Date().toISOString(),
-            price: price.value,
-            redirect: redirect.value,
+            price: getPrice(),
+            redirect: getRedirect(),
             domParser: props.domParser,
+            creatorWallet: await getCreatorWallet(props.arweave, key),
           });
         };
 
