c6ead1a4dce612f98dd1b160b9e376b3e0e5ee69
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-03 14:03:37 +0100

    update, more states have been added

--- lib/state/setStateHook.ts
+++ lib/state/setStateHook.ts
@@ -1,18 +1,23 @@
 import {
   dispatch_renderAcceptButton,
+  dispatch_renderBalance,
   dispatch_renderCreateButton,
 } from "../dispatch/render";
 import { ContractTypes, SetHookArgs, State, StateProperties } from "../types";
-import { getCurrentPage } from "../view/utils";
+import { getCurrentPageDataProp } from "../view/utils";
 
 export const setStateHook = {
   [StateProperties.arweave]: (args: SetHookArgs) => {
-    const currentPage = getCurrentPage();
+    const currentPage = getCurrentPageDataProp();
     if (currentPage === ContractTypes.create) {
-      dispatch_renderCreateButton(args.obj);
+      dispatch_renderCreateButton(Object.assign({}, args.obj));
     } else if (currentPage === ContractTypes.acceptable) {
-      dispatch_renderAcceptButton(args.obj);
+      dispatch_renderAcceptButton(Object.assign({}, args.obj));
     }
   },
-  [StateProperties.editor]: (args: SetHookArgs) => {}
+  [StateProperties.editor]: (args: SetHookArgs) => {},
+  [StateProperties.balance]: (args: SetHookArgs) => {
+    dispatch_renderBalance(Object.assign({}, args.obj));
+  },
+  [StateProperties.address]: (args: SetHookArgs) => {},
 };
