88abd265e561054be805ba287909cafc887d4025
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-03 13:53:45 +0100

    add, loading indicator template

---
+++ lib/view/templates/loadingIndicator.ts
@@ -0,0 +1,31 @@
+import { html } from "lit-html";
+
+export const loadingIndicator = html`
+  <style>
+    .lds-dual-ring {
+      display: inline-block;
+      width: 80px;
+      height: 80px;
+    }
+    .lds-dual-ring:after {
+      content: " ";
+      display: block;
+      width: 64px;
+      height: 64px;
+      margin: 8px;
+      border-radius: 50%;
+      border: 6px solid #cef;
+      border-color: #cef transparent #cef transparent;
+      animation: lds-dual-ring 1.2s linear infinite;
+    }
+    @keyframes lds-dual-ring {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  </style>
+  <div id="loader" class="lds-dual-ring center"></div>
+`;
