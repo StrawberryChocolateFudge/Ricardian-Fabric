6a1ae968c4059f199994cd3d06e458e799920046
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-03 14:02:51 +0100

    update, more renderes have been implemented

--- lib/view/index.ts
+++ lib/view/index.ts
@@ -1,14 +1,24 @@
-import { render } from "lit-html";
 import { Events, RenderType, State } from "../types";
 import { renderCreateButtonClick } from "./actions/createButtonClick";
+import { onFileSelect } from "./actions/onFileSelect";
 import renderAcceptButton from "./actions/renderAcceptButton";
-import { acceptButton } from "./templates/acceptButton";
-import { getById } from "./utils";
+import { renderbalance } from "./actions/renderBalance";
+
 const Render = {
   [RenderType.successMessage]: (props: State) => {},
   [RenderType.errorMessage]: (props: State) => {},
-  [RenderType.createButton]: (props: State) => renderCreateButtonClick(props),
-  [RenderType.acceptButton]: (props: State) => renderAcceptButton(props),
+  [RenderType.createButton]: (props: State) => {
+    // The order of attaching listeners is important
+    onFileSelect(props);
+    renderCreateButtonClick(props);
+  },
+  [RenderType.acceptButton]: (props: State) => {
+    renderAcceptButton(props);
+    onFileSelect(props);
+  },
+  [RenderType.balance]: (props: State) => {
+    renderbalance(props.balance);
+  }
 };
 
 document.body.addEventListener(Events.render, (e: any) => {
