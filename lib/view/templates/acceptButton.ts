bb4b8edb3728878bc755e3efa5fc5f809d8d5104
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-04 14:46:46 +0100

    update, refactoring accept button

--- lib/view/templates/acceptButton.ts
+++ lib/view/templates/acceptButton.ts
@@ -2,17 +2,23 @@ import { html } from "lit-html";
 import { styleMap } from "lit-html/directives/style-map.js";
 
 export const acceptButton = () => {
-  const outter = {
-    display: "flex",
-    flexDirection: "column",
-  };
-//   const button = {
-// 	  width: ""
-//   }
-  return html` <div style=${styleMap(outter)}>
-    <label id="select-file-label" for="select-file-input">Wallet</label>
-    <input id="select-file-input" type="file" />
-    <hr />
-    <button>Accept and Sign</button>
-  </div>`;
+  return html` <style>
+      .outter {
+        display: flex;
+        flex-direction: column;
+      }
+      .red {
+        color: red;
+      }
+    </style>
+    <div class="outter">
+      <label id="select-file-label" for="select-file-input">Wallet</label>
+      <input id="select-file-input" type="file" />
+      <div id="balance" class="center"></div>
+      <div class="center red" id="error-display"></div>
+      <div class="center" id="transaction-display"></div>
+      <button id="accept-button" class="center width-200" disabled>
+        Accept and Sign
+      </button>
+    </div>`;
 };
