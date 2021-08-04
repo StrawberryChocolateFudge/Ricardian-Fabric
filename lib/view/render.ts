32029bb0c750b63b563ef2164529f21e122be832
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-04 14:44:08 +0100

    update, moved rendering functions

---
+++ lib/view/render.ts
@@ -0,0 +1,60 @@
+import { html, render } from "lit-html";
+import { ContractTypes, State } from "../types";
+import { acceptButton } from "./templates/acceptButton";
+import { balanceTemplate } from "./templates/balance";
+import { loadingIndicator } from "./templates/loadingIndicator";
+import { transactionUrl } from "./templates/transaction";
+import { getById } from "./utils";
+
+export async function renderbalance(balance: number) {
+  const balanceEl = getById("balance");
+  render(balanceTemplate(balance), balanceEl);
+}
+
+export function renderAcceptButton(props: State) {
+  const actionContainer = getById("action-container");
+  actionContainer.innerHTML = "";
+  render(acceptButton(), actionContainer);
+}
+
+export async function renderLoadingIndicator(to: string) {
+  render(loadingIndicator, getById(to));
+}
+
+export async function removeLoadingIndicator(from: string) {
+  render(html`<div></div>`, getById(from));
+}
+
+export async function renderTransaction(url: string) {
+  render(transactionUrl(url), getById("transaction-display"));
+}
+
+export function renderError(message: string) {
+  getById("error-display").textContent = message;
+}
+
+export function removeError() {
+  getById("error-display").innerHTML = "";
+}
+
+export function enableButton(props: State) {
+  const currentPage = props.contracttype;
+  if (currentPage === ContractTypes.create) {
+    const saveButton = getById("save-contract") as HTMLButtonElement;
+    saveButton.disabled = false;
+  } else if (currentPage === ContractTypes.acceptable) {
+    const saveButton = getById("accept-button") as HTMLButtonElement;
+    saveButton.disabled = false;
+  }
+}
+
+export function disableButton(props: State) {
+  const currentPage = props.contracttype;
+  if (currentPage === ContractTypes.create) {
+    const saveButton = getById("save-contract") as HTMLButtonElement;
+    saveButton.disabled = true;
+  } else if (currentPage === ContractTypes.acceptable) {
+    const saveButton = getById("accept-button") as HTMLButtonElement;
+    saveButton.disabled = true;
+  }
+}
