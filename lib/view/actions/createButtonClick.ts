7cda07531bd9aa7cc6e700aa0fed02a935f927c3
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-04 21:07:49 +0100

    add, expiry handling

--- lib/view/actions/createButtonClick.ts
+++ lib/view/actions/createButtonClick.ts
@@ -2,12 +2,33 @@ import {
   createAcceptableContract,
   getCreatorWallet,
 } from "../../business/bloc";
+import {
+  dispatch_removeError,
+  dispatch_renderError,
+} from "../../dispatch/render";
 import { State } from "../../types";
-import { getById, getExpires, getPrice, getRedirect, readFile } from "../utils";
+import {
+  getById,
+  getExpires,
+  getPrice,
+  getPostTo,
+  readFile,
+  getWebhookCheckbox,
+  getRedirectCheckbox,
+  didExpire,
+} from "../utils";
 export function renderCreateButtonClick(props: State) {
   getById("save-contract").onclick = function () {
-    const wallet_file = getById("select-file-input") as HTMLInputElement;
+    dispatch_removeError();
+    const expires = getExpires();
+    const expired = didExpire(expires);
 
+    if (expired) {
+      dispatch_renderError("Date expired!");
+      return;
+    }
+
+    const wallet_file = getById("select-file-input") as HTMLInputElement;
     if (wallet_file.files !== null) {
       const getKey = async (key: any) => {
         //Here I call the business logic to do stuff with the key and the other values
@@ -18,8 +39,10 @@ export function renderCreateButtonClick(props: State) {
             legalContract: props.editor.getContent(),
             createdDate: new Date().toISOString(),
             price: getPrice(),
-            post: getRedirect(),
-            expires: getExpires(),
+            post: getPostTo(),
+            webhook: getWebhookCheckbox(),
+            redirect: getRedirectCheckbox(),
+            expires,
             domParser: props.domParser,
             creatorAddress: await getCreatorWallet(props.arweave, key),
           },
