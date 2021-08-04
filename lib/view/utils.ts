94f6ded7478e60c0e264b5a7c7170b574c666f56
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-04 14:45:04 +0100

    refactored utils

--- lib/view/utils.ts
+++ lib/view/utils.ts
@@ -1,4 +1,4 @@
-import { ContractTypes } from "../types";
+import { ContractTypes, State } from "../types";
 
 export function getById(id: string): HTMLElement {
   const el = document.getElementById(id);
@@ -10,32 +10,13 @@ export function getById(id: string): HTMLElement {
   }
 }
 
-export function getCurrentPageDataProp() {
-  const page = getById("page");
-  const contractType = page.dataset.contracttype;
-
-  return contractType as ContractTypes;
+export function getAcceptableContract() {
+  const contractEl = getById("contract-display");
+  return contractEl.innerHTML;
 }
 
-export function getRedirectDataProp(): string {
-  const page = getById("page");
-  return page.dataset.redirect;
-}
-
-//TODO: change where the dependencies come from
-export function getBundleSrcUrl(): string {
-  const mainScript = getById("main-script") as HTMLScriptElement;
-  return mainScript.src;
-}
-
-export function getArweaveDependencyUrl(): string {
-  const arweaveScript = getById("arweave-script") as HTMLScriptElement;
-  return arweaveScript.src;
-}
-
-export function getCommunityJsDependencyUrl(): string {
-  const communityScript = getById("community-js-script") as HTMLScriptElement;
-  return communityScript.src;
+export function getFromUrl() {
+  return window.location.pathname;
 }
 
 export function readFile(files: FileList, getKey: CallableFunction) {
@@ -63,43 +44,12 @@ export function getKeyFromFile(fileEvent: ProgressEvent) {
   }
 }
 
-export function parseDOMfromString(
-  parser: DOMParser,
-  initialDom: string
-): Document {
-  const doc = parser.parseFromString(initialDom, "text/html");
-  return doc;
-}
-
-export function serialize(doc: Document): string {
-  const XMLS = new XMLSerializer();
-  return XMLS.serializeToString(doc);
-}
-
-export function renderError(message: string) {
-  getById("error-display").textContent = message;
-}
-
-export function removeError() {
-  getById("error-display").innerHTML = "";
-}
-
-export function enableCreateButton() {
-  const saveButton = getById("save-contract") as HTMLButtonElement;
-  saveButton.disabled = false;
-}
-
-export function disableCreateButton() {
-  const saveButton = getById("save-contract") as HTMLButtonElement;
-  saveButton.disabled = true;
-}
-
 export function getPrice() {
   const price = getById("price-input") as HTMLInputElement;
   if (price.value === "") {
     return "NONE";
   } else {
-    price.value;
+    return price.value;
   }
 }
 
@@ -111,3 +61,14 @@ export function getRedirect() {
     return redirect.value;
   }
 }
+
+//Util functions to call when redirecting
+// export function formatUrl() {}
+
+// export function redirect() {
+//   const url = getRedirectDataProp();
+
+//   const formatted = formatUrl();
+
+//   window.location.href = url;
+// }
