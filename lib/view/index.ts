573a885ff0e5d34415dba221332b1d7ff9ab32b7
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-04 14:43:56 +0100

    update, render types impl added to Renderer

--- lib/view/index.ts
+++ lib/view/index.ts
@@ -1,12 +1,23 @@
-import { Events, RenderType, State } from "../types";
+import { Events, Renderer, RenderType, State } from "../types";
 import { renderCreateButtonClick } from "./actions/createButtonClick";
 import { onFileSelect } from "./actions/onFileSelect";
-import renderAcceptButton from "./actions/renderAcceptButton";
-import { renderbalance } from "./actions/renderBalance";
+import { renderAcceptOnCLick } from "./actions/renderAcceptButton";
+import {
+  disableButton,
+  enableButton,
+  removeError,
+  removeLoadingIndicator,
+  renderbalance,
+  renderError,
+  renderLoadingIndicator,
+  renderTransaction,
+} from "./render";
+import { renderAcceptButton } from "./render";
 
-const Render = {
+const Render: Renderer = {
   [RenderType.successMessage]: (props: State) => {},
   [RenderType.errorMessage]: (props: State) => {},
+  [RenderType.createPage]: (props: State) => {},
   [RenderType.createButton]: (props: State) => {
     // The order of attaching listeners is important
     onFileSelect(props);
@@ -15,10 +26,32 @@ const Render = {
   [RenderType.acceptButton]: (props: State) => {
     renderAcceptButton(props);
     onFileSelect(props);
+    renderAcceptOnCLick(props);
   },
   [RenderType.balance]: (props: State) => {
     renderbalance(props.balance);
-  }
+  },
+  [RenderType.addLoadingIndicator]: (props: { to: string }) => {
+    renderLoadingIndicator(props.to);
+  },
+  [RenderType.removeLoadingIndicator]: (props: { from: string }) => {
+    removeLoadingIndicator(props.from);
+  },
+  [RenderType.transaction]: (props: { url: string }) => {
+    renderTransaction(props.url);
+  },
+  [RenderType.renderError]: (props: { message: string }) => {
+    renderError(props.message);
+  },
+  [RenderType.removeError]: () => {
+    removeError();
+  },
+  [RenderType.enableButton]: (props: State) => {
+    enableButton(props);
+  },
+  [RenderType.disableButton]: (props: State) => {
+    disableButton(props);
+  },
 };
 
 document.body.addEventListener(Events.render, (e: any) => {
