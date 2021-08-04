cf092b0ed32c649adbe258acc5b8b50385cf5c14
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-04 14:46:09 +0100

    add a renderer for accept button ,WIP!

--- lib/view/actions/renderAcceptButton.ts
+++ lib/view/actions/renderAcceptButton.ts
@@ -1,10 +1,34 @@
 import { render } from "lit-html";
+import { toWinston } from "../../arweave/arweave";
+import { acceptAndPayContract, acceptContract } from "../../business/bloc";
 import { State } from "../../types";
 import { acceptButton } from "../templates/acceptButton";
-import { getById } from "../utils";
+import {
+  getById,
+  // getCreatorAddressDataProp,
+  // getPriceFromDataProp,
+  readFile,
+} from "../utils";
 
-export default function renderAcceptButton(props: State) {
-  const actionContainer = getById("action-container");
-  actionContainer.innerHTML = "";
-  render(acceptButton(), actionContainer);
+export function renderAcceptOnCLick(props: State) {
+  const acceptButton = getById("accept-button") as HTMLInputElement;
+
+  acceptButton.onclick = async function () {
+    const wallet_file = getById("select-file-input") as HTMLInputElement;
+
+    const getKey = async (key: any) => {
+      const price = props.price;
+      if (price !== "NONE") {
+        const winston = toWinston(price);
+        await acceptAndPayContract({
+          props,
+          winston,
+          key,
+        });
+      } else {
+        await acceptContract(props.arweave, key);
+      }
+    };
+    readFile(wallet_file.files, getKey);
+  };
 }
