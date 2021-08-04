226b2b0ae96c0972104fed10acc37000dc6a5e5d
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-04 14:46:28 +0100

    update, templating changes

--- lib/view/templates/acceptablePage.ts
+++ lib/view/templates/acceptablePage.ts
@@ -1,4 +1,5 @@
 import { html } from "lit-html";
+import { FEE } from "../../arweave/arweave";
 import { AcceptablePageProps } from "../../types";
 import { arweaveDep, communityJsDep, mainDep } from "./dependencies";
 import { loadingIndicator } from "./loadingIndicator";
@@ -35,6 +36,10 @@ export const acceptablePageLayout = (props: AcceptablePageProps) => html`
       width: 100px;
     }
 
+    .width-200 {
+      width: 200px;
+    }
+
     .paddingLeft-50 {
       padding-left: 50px;
     }
@@ -49,27 +54,71 @@ export const acceptablePageLayout = (props: AcceptablePageProps) => html`
   </body>
 `;
 
-const acceptablePage = (props: AcceptablePageProps) => html`
+const getPrice = (price: string) => {
+  if (price !== "NONE") {
+    const formattedPrice = price === "NONE" ? "" : `${price} Ar`;
+    return html`
+      <div class="row center">
+        <div class="column width-100">
+          <label for="center">Price </label>
+        </div>
+        <div class="column width-100">
+          <div class="center">
+            <span>${formattedPrice}</span>
+          </div>
+        </div>
+      </div>
+      <div class="row center">
+        <div class="column width-100">
+          <label for="center">Fee: </label>
+        </div>
+        <div class="column width-100">
+          <div class="center">
+            <span>${FEE}</span>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+};
 
-      <div data-creatorAddress="${props.creatorWallet}" data-arweavedep="${
-  props.arweaveDeps.src
-}"  data-communityjsdep="${props.communityJsDep.src}"  data-maindep="${
-  props.mainDep.src
-}" data-redirect="${props.redirect}" data-contracttype="acceptable" id="page">
-            <hr>  
+const createdDate = (date: string) => {
+  return html` <div class="center">${date}</div> `;
+};
+
+const creator = (address: string) => {
+  return html`
+    <label class="center">Issuer</label>
+    <div class="center"><span>${address}</span></div>
+  `;
+};
+
+const acceptablePage = (props: AcceptablePageProps) => html`
+<style>
+.address{
+  overflow: scroll;
+}
+
+</style>
+      <div data-price="${props.price}"  data-creatorAddress="${
+  props.creatorAddress
+}" data-arweavedep="${props.arweaveDeps.src}"  data-communityjsdep="${
+  props.communityJsDep.src
+}"  data-maindep="${props.mainDep.src}" data-redirect="${
+  props.redirect
+}" data-contracttype="acceptable" id="page">
+      <hr>  
       <h1 class="center title">Ricardian Fabric</h1>
       <hr> 
       <h5 class="center">Carefully read the contract bellow</h5>
         <div class="center" disabled id="contract-display"></div>
-        <hr />
-        <div class="center"><span>${props.createdDate}</span></div>
-        <div class="center">Price: <span>${props.price}</span>${
-  props.price === "NONE" ? "" : " Ar"
-}</div>
-        <hr />
+        ${creator(props.creatorAddress)}
+        ${createdDate(props.createdDate)}
+        <hr/>
+        ${getPrice(props.price)}
+         <hr/>
         <div id="action-container" class="center">
-          ${loadingIndicator}
+        ${loadingIndicator}
         </div>
       </div>
-    
   </html> `;
