d7a00af772f04c682aa8372fc0fdfa9bbc97297a
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-04 14:45:24 +0100

    remove, moved!

--- lib/view/actions/renderBalance.ts
+++
@@ -1,8 +0,0 @@
-import { render } from "lit-html";
-import { balanceTemplate } from "../templates/balance";
-import { getById } from "../utils";
-
-export async function renderbalance(balance: number) {
-  const balanceEl = getById("balance");
-  render(balanceTemplate(balance), balanceEl);
-}
