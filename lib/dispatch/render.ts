bd96143902d8f957a313113bea330444e509bcfb
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-04 14:39:53 +0100

    add, render dispatches added during refactoring

--- lib/dispatch/render.ts
+++ lib/dispatch/render.ts
@@ -16,3 +16,51 @@ export async function dispatch_renderAcceptButton(props: State) {
 export async function dispatch_renderBalance(props: State) {
   dispatch(Events.render, { type: RenderType.balance, props });
 }
+
+export async function dispatch_renderLoadingIndicator(to: string) {
+  dispatch(Events.render, {
+    type: RenderType.addLoadingIndicator,
+    props: { to },
+  });
+}
+
+export async function dispatch_removeLoadingIndicator(from: string) {
+  dispatch(Events.render, {
+    type: RenderType.removeLoadingIndicator,
+    props: { from },
+  });
+}
+
+export async function dispatch_renderTransaction(url: string) {
+  dispatch(Events.render, {
+    type: RenderType.transaction,
+    props: { url },
+  });
+}
+
+export async function dispatch_removeError() {
+  dispatch(Events.render, {
+    type: RenderType.removeError,
+  });
+}
+
+export async function dispatch_enableButton(props: State) {
+  dispatch(Events.render, {
+    type: RenderType.enableButton,
+    props,
+  });
+}
+
+export async function dispatch_disableButton(props: State) {
+  dispatch(Events.render, {
+    type: RenderType.disableButton,
+    props,
+  });
+}
+
+export async function dispatch_renderError(message: string) {
+  dispatch(Events.render, {
+    type: RenderType.renderError,
+    props: { message },
+  });
+}
