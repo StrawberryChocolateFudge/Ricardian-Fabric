fa058daf926a9c72e5974462d0cbc82b621accff
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-05 23:08:25 +0100

    add, acceptContract, update refactoring

--- lib/business/bloc.ts
+++ lib/business/bloc.ts
@@ -1,4 +1,6 @@
 import {
+  acceptTransactionFree,
+  acceptTransactionPay,
   createTransactionSend,
   FEE,
   getArweaveCall,
@@ -19,7 +21,10 @@ import {
 import { AcceptablePageProps, State } from "../types";
 
 import { getAcceptableContract, getFromUrl } from "../view/utils";
-import { getAcceptablePage, getFulfilledPage } from "../view/vDom";
+import {
+  getAcceptablePageFromVDOM,
+  getFulfilledPagefromVDOM,
+} from "../view/vDom";
 
 export async function getArweave() {
   const arweave = await getArweaveCall();
@@ -42,7 +47,7 @@ export async function createAcceptableContract(args: {
   data: AcceptablePageProps;
 }) {
   dispatch_renderLoadingIndicator("transaction-display");
-  const page = await getAcceptablePage({
+  const page = await getAcceptablePageFromVDOM({
     ...args.data,
     arweaveDeps: {
       src: args.props.arweaveDependencyUrl,
@@ -74,28 +79,68 @@ export async function createAcceptableContract(args: {
 
 export async function acceptAndPayContract(data: {
   props: State;
-  winston: number;
+  ar: number;
   key: any;
 }) {
-  dispatch_renderLoadingIndicator("transaction-display");
-  dispatch_disableButton(data.props);
-  const page = await getFulfilledPage({
-    legalContract: getAcceptableContract(),
-    creator: data.props.creatorAddress,
-    parentUrl: getFromUrl(),
-    fee: FEE,
-    paidAmount: data.winston,
-    paidTo: data.props.creatorAddress,
-    paidFrom: await getWalletAddr(data.props.arweave, data.key),
-    createdDate: new Date().toISOString(),
-    domParser: data.props.domParser,
-  });
-  //TODO: style page!
-  console.log(page);
-  //TODO:
-  // const result = await acceptTransactionPay({});
+  const props = data.props;
 
+  dispatch_disableButton(props);
+  dispatch_renderLoadingIndicator("transaction-display");
+  const page = await fulfilledPage(data);
+  const result = await acceptTransactionPay({
+    arweave: data.props.arweave,
+    key: data.key,
+    page,
+    target: props.creatorAddress,
+    quantity: data.ar,
+  });
+
+  if (result.statusCode !== 200) {
+    dispatch_removeLoadingIndicator("transaction-display");
+    dispatch_renderError("An error occured when sending the transaction!");
+  } else {
+    dispatch_renderTransaction(result.path);
+  }
   //then I can redirect when I got the transaction id in the result
 }
 
-export async function acceptContract(arweave: any, key: any) {}
+export async function acceptContract(props: State, key: any) {
+  dispatch_renderLoadingIndicator("transaction-display");
+  dispatch_disableButton(props);
+  const page = await fulfilledPage({ props, ar: 0, key });
+
+  const result = await acceptTransactionFree({
+    arweave: props.arweave,
+    key,
+    page,
+  });
+
+  if (result.statusCode !== 200) {
+    dispatch_removeLoadingIndicator("transaction-display");
+    dispatch_renderError("An error occured when sending the transaction!");
+  } else {
+    dispatch_renderTransaction(result.path);
+  }
+
+  if (props.webhook) {
+    //I hit the webhook here
+  } else if (props.redirect) {
+   // I show a coundback and redirect
+  }
+}
+
+async function fulfilledPage(data: { props: State; ar: number; key: any }) {
+  const props = data.props;
+
+  return await getFulfilledPagefromVDOM({
+    legalContract: getAcceptableContract(),
+    creator: props.creatorAddress,
+    parentUrl: getFromUrl(),
+    fee: FEE,
+    paidAmount: data.ar,
+    paidTo: props.creatorAddress,
+    paidFrom: await getWalletAddr(props.arweave, data.key),
+    createdDate: new Date().toISOString(),
+    domParser: props.domParser,
+  });
+}
