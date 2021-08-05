ddcdb30df26958eca7c107e6a2de599592c875ae
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-05 23:08:55 +0100

    add,webhook request imp started WIP!

--- lib/fetch/index.ts
+++ lib/fetch/index.ts
@@ -1,5 +1,27 @@
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
+
 export async function fetchDependency(url: string) {
   const response = await fetch(url);
   const data = await response.text();
   return data;
+}
+
+export async function hitWebhook(url: string, secret: string) {
+  try {
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        secret,
+      }),
+    });
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
 }
