0d80e144b96a29396ad044c3d5f39dcc4593d4f7
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-06 15:15:10 +0100

    update, fetch headers cors

--- lib/fetch/index.ts
+++ lib/fetch/index.ts
@@ -7,11 +7,14 @@ export async function fetchDependency(url: string) {
 }
 
 export async function hitWebhook(url: string, secret: string) {
+  // The backend must handle cross origin requests for this to work!
   try {
     const response = await fetch(url, {
       method: "post",
+      mode: "cors",
+      cache: "no-cache",
       headers: {
-        Accept: "application/json",
+        "Accept": "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
