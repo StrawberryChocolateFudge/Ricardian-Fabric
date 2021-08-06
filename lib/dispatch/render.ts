60092fec981767c8a7d268affeaf41ec21b18ff7
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-06 14:18:31 +0100

    add, render version event

--- lib/dispatch/render.ts
+++ lib/dispatch/render.ts
@@ -2,65 +2,72 @@ import { RenderType, State } from "../types";
 import { dispatch } from "./dispatch";
 import { Events } from "../types";
 
-export async function dispatch_renderCreateButton(props: State) {
+export function dispatch_renderCreateButton(props: State) {
   dispatch(Events.render, {
     type: RenderType.createButton,
     props,
   });
 }
 
-export async function dispatch_renderAcceptButton(props: State) {
+export function dispatch_renderAcceptButton(props: State) {
   dispatch(Events.render, { type: RenderType.acceptButton, props });
 }
 
-export async function dispatch_renderBalance(props: State) {
+export function dispatch_renderBalance(props: State) {
   dispatch(Events.render, { type: RenderType.balance, props });
 }
 
-export async function dispatch_renderLoadingIndicator(to: string) {
+export function dispatch_renderLoadingIndicator(to: string) {
   dispatch(Events.render, {
     type: RenderType.addLoadingIndicator,
     props: { to },
   });
 }
 
-export async function dispatch_removeLoadingIndicator(from: string) {
+export function dispatch_removeLoadingIndicator(from: string) {
   dispatch(Events.render, {
     type: RenderType.removeLoadingIndicator,
     props: { from },
   });
 }
 
-export async function dispatch_renderTransaction(url: string) {
+export function dispatch_renderTransaction(url: string) {
   dispatch(Events.render, {
     type: RenderType.transaction,
     props: { url },
   });
 }
 
-export async function dispatch_removeError() {
+export function dispatch_removeError() {
   dispatch(Events.render, {
     type: RenderType.removeError,
   });
 }
 
-export async function dispatch_enableButton(props: State) {
+export function dispatch_enableButton(props: State) {
   dispatch(Events.render, {
     type: RenderType.enableButton,
     props,
   });
 }
 
-export async function dispatch_disableButton(props: State) {
+export function dispatch_disableButton(props: State) {
   dispatch(Events.render, {
     type: RenderType.disableButton,
     props,
   });
 }
 
-export async function dispatch_renderError(message: string) {
+export function dispatch_renderError(message: string) {
   dispatch(Events.render, {
     type: RenderType.renderError,
     props: { message },
+  });
+}
+
+export function dispatch_renderVersion(version: string) {
+  dispatch(Events.render, {
+    type: RenderType.version,
+    props: { version },
   });
 }
