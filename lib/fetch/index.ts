8577a0cf9b6f63f6743e638249c414b1fb0e848e
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-07 13:42:04 +0100

    add WIP I need an automatic validity verification

--- lib/fetch/index.ts
+++ lib/fetch/index.ts
@@ -1,11 +1,3 @@
-import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
-
-export async function fetchDependency(url: string) {
-  const response = await fetch(url);
-  const data = await response.text();
-  return data;
-}
-
 export async function hitWebhook(url: string, secret: string) {
   // The backend must handle cross origin requests for this to work!
   try {
@@ -14,7 +6,7 @@ export async function hitWebhook(url: string, secret: string) {
       mode: "cors",
       cache: "no-cache",
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -27,4 +19,14 @@ export async function hitWebhook(url: string, secret: string) {
   } catch (error) {
     console.log(error);
   }
+}
+
+export async function fetchTransactionData() {
+  //TODO: This is probably a graph QL query!!
+  // I want to fetch the transaction URL of the current page
+  // to determine the on the acceptable contract page
+  // that the issuer is the same in the website as in the transaction!
+  // I probably call the graphQL endpoint so I can extract the tags added to the transaction
+  // If I compare the tags of the transaction with the current id, I can determine if somebody manipulated the data props in HTML!
+  // I might just migrate all the data props to tags if this works, but I prefer to keep them so the data is extractable without running the javascript by a third party client
 }
