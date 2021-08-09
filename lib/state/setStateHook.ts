f9a4d71d660d408e6ea96f82a23ae213595b1feb
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-09 15:57:06 +0100

    add selectedDate, created cloneState abstraction

--- lib/state/setStateHook.ts
+++ lib/state/setStateHook.ts
@@ -1,25 +1,32 @@
 import {
+  dispatch_attachDateClickListener,
   dispatch_renderAcceptButton,
   dispatch_renderBalance,
   dispatch_renderCreateButton,
   dispatch_renderVersion,
 } from "../dispatch/render";
 import { ContractTypes, SetHookArgs, State, StateProperties } from "../types";
-// import { getCurrentPageDataProp } from "../view/utils";
 
 export const setStateHook = {
   [StateProperties.arweave]: (args: SetHookArgs) => {
     const currentPage = args.obj.contracttype;
     if (currentPage === ContractTypes.create) {
-      dispatch_renderCreateButton(Object.assign({}, args.obj));
+      dispatch_renderCreateButton(cloneState(args.obj));
       dispatch_renderVersion(args.obj.version);
     } else if (currentPage === ContractTypes.acceptable) {
-      dispatch_renderAcceptButton(Object.assign({}, args.obj));
+      dispatch_renderAcceptButton(cloneState(args.obj));
     }
   },
   [StateProperties.editor]: (args: SetHookArgs) => {},
   [StateProperties.balance]: (args: SetHookArgs) => {
-    dispatch_renderBalance(Object.assign({}, args.obj));
+    dispatch_renderBalance(cloneState(args.obj));
   },
   [StateProperties.address]: (args: SetHookArgs) => {},
+  [StateProperties.selectedDate]: (args: SetHookArgs) => {
+    dispatch_attachDateClickListener(cloneState(args.obj));
+  },
 };
+
+function cloneState(state: State) {
+  return Object.assign({}, state);
+}
