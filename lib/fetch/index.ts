f1413557f5a246ff13b7b2a4cc4e3ecfaa2d4af8
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-09 17:37:19 +0100

    delete, redundant idea, don't work liek that

--- lib/fetch/index.ts
+++ lib/fetch/index.ts
@@ -21,12 +21,3 @@ export async function hitWebhook(url: string, secret: string) {
   }
 }
 
-export async function fetchTransactionData() {
-  //TODO: This is probably a graph QL query!!
-  // I want to fetch the transaction URL of the current page
-  // to determine the on the acceptable contract page
-  // that the issuer is the same in the website as in the transaction!
-  // I probably call the graphQL endpoint so I can extract the tags added to the transaction
-  // If I compare the tags of the transaction with the current id, I can determine if somebody manipulated the data props in HTML!
-  // I might just migrate all the data props to tags if this works, but I prefer to keep them so the data is extractable without running the javascript by a third party client
-}
