33a0e6040602a9bfac5ae28c5f62308623df2d21
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-10 19:37:38 +0100

    update, added css to page and moved balance

--- lib/view/templates/acceptablePage.ts
+++ lib/view/templates/acceptablePage.ts
@@ -11,14 +11,49 @@ import { loadingIndicator } from "./loadingIndicator";
 
 export const acceptablePageLayout = (props: AcceptablePageProps) => html`
   <style>
+    body {
+      background: radial-gradient(lightgrey 3px, transparent 4px),
+        radial-gradient(lightgrey 3px, transparent 4px),
+        linear-gradient(#fff 4px, transparent 0),
+        linear-gradient(
+          45deg,
+          transparent 74px,
+          transparent 75px,
+          #a4a4a4 75px,
+          #a4a4a4 76px,
+          transparent 77px,
+          transparent 109px
+        ),
+        linear-gradient(
+          -45deg,
+          transparent 75px,
+          transparent 76px,
+          #a4a4a4 76px,
+          #a4a4a4 77px,
+          transparent 78px,
+          transparent 109px
+        ),
+        #fff;
+      background-size: 109px 109px, 109px 109px, 100% 6px, 109px 109px,
+        109px 109px;
+      background-position: 54px 55px, 0px 0px, 0px 0px, 0px 0px, 0px 0px;
+    }
     .center {
       margin: 0 auto;
     }
     #page {
+      margin: 0 auto;
       display: flex;
-      width: 100%;
+      max-width: 800px;
+      min-width: 350px;
       height: 100%;
       flex-direction: column;
+      box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
+        rgba(0, 0, 0, 0.23) 0px 6px 6px;
+      padding-bottom: 20px;
+      padding-left: 20px;
+      padding-right: 20px;
+      background-color: white;
     }
     #contract-display {
       max-width: 800px;
@@ -48,8 +83,6 @@ const acceptablePage = (props: AcceptablePageProps) => html`
     id="page"
   >
     <hr />
-    <h1 class="center title">Ricardian Fabric</h1>
-    <hr />
     <h5 class="center">Carefully read the contract bellow</h5>
     <div class="center" id="contract-display"></div>
     <table class="center">
@@ -60,6 +93,7 @@ const acceptablePage = (props: AcceptablePageProps) => html`
       ${issuerTemplate(props.creatorAddress)}
       ${createdDateTemplate(props.createdDate)} ${expiryTemplate(props.expires)}
       ${getPriceTemplate(props.price, props.fee)}
+      <tr id="balance" class="center"></tr>
     </table>
     <hr />
     <div id="action-container" class="center">${loadingIndicator}</div>
