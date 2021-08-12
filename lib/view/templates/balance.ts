a2c5ef03dd59b1675d8ef8e8afd0acfda417205e
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-12 14:57:14 +0100

    add height to balance

--- lib/view/templates/balance.ts
+++ lib/view/templates/balance.ts
@@ -1,6 +1,11 @@
 import { html } from "lit-html";
 
 export const balanceTemplate = (balance: number) => html`
+  <style>
+    #balance{
+      height: 50px;
+    }
+  </style>
   <td aria-label="your balance">Your Balance:</td>
   <td aria-label="Balance">${balance} Ar</td>
 `;
