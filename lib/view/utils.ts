417ad0f8d2c0040fb8e841e290d2dd381a4015e2
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-20 18:08:36 +0100

    add more utils and refactoring!

--- lib/view/utils.ts
+++ lib/view/utils.ts
@@ -117,11 +117,154 @@ export function getExpires(): string {
   return new Date(acceptableTill.value).toISOString();
 }
 
+export function setOnlySignerToDOM(onlySigner: string) {
+  const onlySignerEl = getById("onlysigner-input") as HTMLInputElement;
+  if (onlySigner !== "NONE") {
+    onlySignerEl.value = onlySigner;
+  }
+}
+
+export function setPriceToDOM(price: string) {
+  const priceEl = getById("price-input") as HTMLInputElement;
+  if (price !== "NONE") {
+    priceEl.value = price;
+  }
+}
+
+export function setExpiresDateToDOM(date: string) {
+  const dateEl = getById("expires-input") as HTMLInputElement;
+  console.log("TYPEOF DATE" + typeof date);
+  if (date !== "NEVER") {
+    dateEl.valueAsDate = new Date(date);
+  }
+}
+
+export function setPDFtoDOM(fileList: FileList | string) {
+  const pdfEl = getById("pdf-input") as HTMLInputElement;
+  if (typeof fileList !== "string") {
+    pdfEl.files = fileList;
+  }
+}
+
+export function getPDF(): FileList {
+  const pdf = getById("pdf-input") as HTMLInputElement;
+  if (pdf.files.length === 1 && pdf.files[0].type === "application/pdf") {
+    return pdf.files;
+  } else {
+    return null;
+  }
+}
+
+export function getWallet(): FileList {
+  const wallet = getById("wallet-input") as HTMLInputElement;
+
+  if (
+    wallet.files.length === 1 &&
+    wallet.files[0].type === "application/json"
+  ) {
+    return wallet.files;
+  } else {
+    return null;
+  }
+}
+
 export function getSecret(): string {
   const secret = getById("secret-input") as HTMLInputElement;
   return secret.value;
 }
 
+export function setSmartContractInputFields(checkboxState: boolean) {
+  const pstContractInput = getPSTContractEl();
+  const nameEl = instrumentNameEl();
+  const tickerEl = instrumentTickerEl();
+  const supplyEl = instrumentSupplyEl();
+  const canDeriveEl = instrumentDeriveEl();
+  if (checkboxState) {
+    pstContractInput.disabled = true;
+    nameEl.disabled = false;
+    tickerEl.disabled = false;
+    supplyEl.disabled = false;
+    canDeriveEl.disabled = false;
+  } else {
+    pstContractInput.disabled = false;
+    nameEl.disabled = true;
+    tickerEl.disabled = true;
+    supplyEl.disabled = true;
+    canDeriveEl.disabled = true;
+  }
+}
+
+export function setProfitSharingContractIdToDOM(id: string) {
+  getPSTContractEl().value = id;
+}
+
+export function setIsIntrumentToDOM(isIns: boolean) {
+  isInstrumentEl().checked = isIns;
+}
+
+export function setInstrumentNameToDOM(name: string) {
+  instrumentNameEl().value = name;
+}
+
+export function setInstrumentTickerToDOM(ticker: string) {
+  instrumentTickerEl().value = ticker;
+}
+
+export function setInstrumentSupplyToDOM(supply: number) {
+  instrumentSupplyEl().valueAsNumber = supply;
+}
+
+export function setInstrumentCanDeriveToDOM(canDerive: number) {
+  instrumentDeriveEl().valueAsNumber = canDerive;
+}
+
+export function getProfitSharingContractId(): string {
+  return getPSTContractEl().value;
+}
+
+export function getIsInstrument(): boolean {
+  return isInstrumentEl().checked;
+}
+
+export function getInstrumentName(): string {
+  return instrumentNameEl().value;
+}
+
+export function getInstrumentTicker(): string {
+  return instrumentTickerEl().value;
+}
+
+export function getInstrumentSupply(): string {
+  return instrumentSupplyEl().value;
+}
+
+export function getInstrumentCanDerive(): string {
+  return instrumentDeriveEl().value;
+}
+
+export function getPSTContractEl(): HTMLInputElement {
+  return getById("pstContractId") as HTMLInputElement;
+}
+
+export function isInstrumentEl(): HTMLInputElement {
+  return getById("is-crypto-instrument") as HTMLInputElement;
+}
+
+export function instrumentNameEl(): HTMLInputElement {
+  return getById("instrument-name-input") as HTMLInputElement;
+}
+
+export function instrumentTickerEl(): HTMLInputElement {
+  return getById("instrument-ticker-input") as HTMLInputElement;
+}
+
+export function instrumentSupplyEl(): HTMLInputElement {
+  return getById("instrument-supply-input") as HTMLInputElement;
+}
+
+export function instrumentDeriveEl(): HTMLInputElement {
+  return getById("instrument-derive-input") as HTMLInputElement;
+}
 export function redirect(url: string) {
   window.location.replace(url);
 }
@@ -158,4 +301,16 @@ export function getTermsAccepted(): boolean {
   } else {
     return parsed.termsAccepted;
   }
+}
+
+export function updatePromptSuccess(file) {
+  const prompt = getById("drop-prompt");
+  prompt.style.color = "black";
+  prompt.textContent = file.name;
+}
+
+export function updatePromptError(message: string) {
+  const prompt = getById("drop-prompt");
+  prompt.textContent = message;
+  prompt.style.color = "red";
 }
