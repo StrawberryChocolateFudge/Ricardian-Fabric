f9c1531ab784902f69e9ef29151f2c6133574cc9
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-20 18:09:11 +0100

    update, added page support

--- lib/view/actions/attachExpiryClickAndListener.ts
+++ lib/view/actions/attachExpiryClickAndListener.ts
@@ -5,37 +5,39 @@ import {
   dispatch_renderError,
 } from "../../dispatch/render";
 import { dispatch_setSelectedDate } from "../../dispatch/stateChange";
-import { State } from "../../types";
+import { CreatePages, State } from "../../types";
 import { didExpire, getById, getExpires } from "../utils";
 
 export const attachExpiryClickAndListener = (props: State) => {
-  const reset = getById("expires-reset");
-  const date = getById("expires-input") as HTMLInputElement;
-  if (props.selectedDate === "") {
-    date.valueAsDate = new Date();
-  } else {
-    handleExpiry();
-  }
-
-  reset.onclick = function () {
-    date.value = "";
-    dispatch_setSelectedDate(getExpires());
-  };
-
-  date.onchange = function () {
-    dispatch_setSelectedDate(getExpires());
-  };
-
-  function handleExpiry() {
-    const expires = getExpires();
-    const expired = didExpire(expires);
-    if (expired) {
-      dispatch_renderError("Date expired!");
-      dispatch_disableButton(props);
+  if (props.createPages === CreatePages.PDF) {
+    const reset = getById("expires-reset");
+    const date = getById("expires-input") as HTMLInputElement;
+    if (props.pdfPage.selectedDate === "") {
+      date.valueAsDate = new Date();
     } else {
-      dispatch_removeError();
-      if (props.address !== "") {
-        dispatch_enableButton(props);
+      handleExpiry();
+    }
+
+    reset.onclick = function () {
+      date.value = "";
+      dispatch_setSelectedDate(getExpires());
+    };
+
+    date.onchange = function () {
+      dispatch_setSelectedDate(getExpires());
+    };
+
+    function handleExpiry() {
+      const expires = getExpires();
+      const expired = didExpire(expires);
+      if (expired) {
+        dispatch_renderError("Date expired!");
+        dispatch_disableButton(props);
+      } else {
+        dispatch_removeError();
+        if (props.address !== "") {
+          dispatch_enableButton(props);
+        }
       }
     }
   }
