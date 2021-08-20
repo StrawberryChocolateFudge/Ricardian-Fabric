c69eeebaf36ca0392ede670f660931d17029282a
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-20 18:08:43 +0100

    add, more state added

--- lib/state/setStateHook.ts
+++ lib/state/setStateHook.ts
@@ -1,8 +1,9 @@
 import {
   dispatch_attachDateClickListener,
+  dispatch_createPage,
+  dispatch_instrumentsSetRerender,
   dispatch_renderAcceptButton,
   dispatch_renderBalance,
-  dispatch_renderCreateButton,
   dispatch_renderVersion,
 } from "../dispatch/render";
 import { ContractTypes, SetHookArgs, State, StateProperties } from "../types";
@@ -11,7 +12,8 @@ export const setStateHook = {
   [StateProperties.arweave]: (args: SetHookArgs) => {
     const currentPage = args.obj.contracttype;
     if (currentPage === ContractTypes.create) {
-      dispatch_renderCreateButton(cloneState(args.obj));
+      dispatch_createPage(cloneState(args.obj));
+      // dispatch_renderCreateButton(cloneState(args.obj));
       dispatch_renderVersion(args.obj.version);
     } else if (currentPage === ContractTypes.acceptable) {
       dispatch_renderAcceptButton(cloneState(args.obj));
@@ -22,8 +24,16 @@ export const setStateHook = {
     dispatch_renderBalance(cloneState(args.obj));
   },
   [StateProperties.address]: (args: SetHookArgs) => {},
-  [StateProperties.selectedDate]: (args: SetHookArgs) => {
-    dispatch_attachDateClickListener(cloneState(args.obj));
+  [StateProperties.pdfPage]: (args: SetHookArgs) => {
+    if (args.value.PDF === "") {
+      console.log("RUNS");
+      //There is a case when I only set the date
+      dispatch_attachDateClickListener(cloneState(args.obj));
+    }
+  },
+  [StateProperties.instrumentPageData]: (args: SetHookArgs) => {},
+  [StateProperties.createPages]: (args: SetHookArgs) => {
+    dispatch_createPage(cloneState(args.obj));
   },
 };
 
