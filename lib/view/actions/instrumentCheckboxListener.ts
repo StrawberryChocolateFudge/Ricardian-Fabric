9e67e73359093118e1758fa816d5c5ae76d424f7
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-21 21:04:53 +0100

    add , smart contract checkbox logic change

--- lib/view/actions/instrumentCheckboxListener.ts
+++ lib/view/actions/instrumentCheckboxListener.ts
@@ -1,10 +1,21 @@
 import { State } from "../../types";
-import { isInstrumentEl, setSmartContractInputFields } from "../utils";
+import { setSmartContractInputFields } from "../render";
+import { getPSTCheckboxEl, isInstrumentEl } from "../utils";
 
 export const instrumentCheckboxListener = (props: State) => {
   const instrumentcheckbox = isInstrumentEl();
-
+  const pstCheckbox = getPSTCheckboxEl();
   instrumentcheckbox.onchange = function () {
-    setSmartContractInputFields(instrumentcheckbox.checked);
+    setSmartContractInputFields(
+      pstCheckbox.checked,
+      instrumentcheckbox.checked
+    );
+  };
+
+  pstCheckbox.onchange = function () {
+    setSmartContractInputFields(
+      pstCheckbox.checked,
+      instrumentcheckbox.checked
+    );
   };
 };
