ac8bf319432100820216b2ea4a92bd122bc7ed3a
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-22 20:14:47 +0100

    add,  inlining script dependency ,WIP|

--- lib/view/vDom.ts
+++ lib/view/vDom.ts
@@ -11,8 +11,18 @@ export async function getAcceptablePageFromVDOM(
   render(acceptablePageLayout(pageProps), doc.body);
   // The legal contract HTML is sanitized by the editor
   doc.getElementById("contract-display").innerHTML = pageProps.legalContract;
-  //TODO: I need to inline the script dependency here!!
-  //with document.createTextNode?
+  // I need to inline the script dependency here!zzs
+  // TODO: UNTESTED!
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  const code = pageProps.mainDep.code;
+  try {
+    script.appendChild(document.createTextNode(code));
+    doc.body.appendChild(script);
+  } catch (e) {
+    script.text = code;
+    doc.body.appendChild(script);
+  }
 
   return serialize(doc);
 }
