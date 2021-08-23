738176fb20f9c41bbd5a1272f9746f6387d404e2
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-23 19:07:31 +0100

    add, buttons for create are you sure

---
+++ lib/view/actions/areYouSureButtons.ts
@@ -0,0 +1,21 @@
+import { postTransactionFromCreatePage } from "../../business/bloc";
+import {
+  dispatch_noButtonPressed,
+  dispatch_yesButtonPressed,
+} from "../../dispatch/render";
+import { State } from "../../types";
+import { getById } from "../utils";
+
+export function areYouSureButtons(props: any) {
+  const noButton = getById("no-button");
+  const yesButton = getById("yes-button");
+
+  noButton.onclick = function () {
+    dispatch_noButtonPressed(props);
+  };
+
+  yesButton.onclick = function () {
+    postTransactionFromCreatePage(props as State, props.tx, props.key);
+    dispatch_yesButtonPressed(props);
+  };
+}
