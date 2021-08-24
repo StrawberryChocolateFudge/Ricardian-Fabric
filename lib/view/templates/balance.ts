ddae808988e2671ea53a6f544965ebc311df5f9f
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-24 18:29:05 +0100

    add address template

--- lib/view/templates/balance.ts
+++ lib/view/templates/balance.ts
@@ -1,4 +1,5 @@
 import { html } from "lit-html";
+import { helperTooltips } from "./helperTooltips";
 
 export const balanceTemplate = (balance: number) => html`
   <style>
@@ -6,6 +7,22 @@ export const balanceTemplate = (balance: number) => html`
       height: 50px;
     }
   </style>
-  <td aria-label="your balance">Wallet Balance:</td>
-  <td aria-label="Balance">${balance} Ar</td>
+  <tr>
+    <td aria-label="your balance"></td>
+    <td aria-label="Balance">${balance} Ar</td>
+    <td>${helperTooltips("Your wallet's current balance.")}</td>
+  </tr>
+`;
+
+export const addressTemplate = (address: string) => html`
+  <style>
+    #address {
+      height: 50px;
+    }
+  </style>
+  <tr>
+    <td aria-label="wallet address"></td>
+    <td aria-label="Address">${address}</td>
+    <td>${helperTooltips("Your wallet's public address")}</td>
+  </tr>
 `;
