ca4f5b8b8e25cc19ea3f768e275e80ed41ee5c8d
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-24 18:26:21 +0100

    add tooltip, prompt and other renderers

--- lib/view/render.ts
+++ lib/view/render.ts
@@ -1,24 +1,39 @@
 import { html, render } from "lit-html";
 import { ContractTypes, State } from "../types";
-import { acceptButton } from "./templates/acceptButton";
-import { balanceTemplate } from "./templates/balance";
+import { AcceptButton, acceptTools } from "./templates/acceptTools";
+import { addressTemplate, balanceTemplate } from "./templates/balance";
 import { createButton } from "./templates/createButton";
 import { CreateSummary } from "./templates/createSummary";
+import { helperTooltips } from "./templates/helperTooltips";
 import { loadingIndicator } from "./templates/loadingIndicator";
 import { redirectCounter } from "./templates/redirectCounter";
 import { termsLayout } from "./templates/terms";
 import { transactionUrl } from "./templates/transaction";
-import { copyStringToClipboard, getById, setBannerDisplayBlock } from "./utils";
+import {
+  copyStringToClipboard,
+  getById,
+  getPromptEl,
+  setBannerDisplayBlock,
+} from "./utils";
 
 export async function renderbalance(balance: number) {
   const balanceEl = getById("balance");
   render(balanceTemplate(balance), balanceEl);
 }
+export async function renderAddress(address: string) {
+  const addressEl = getById("address");
+  render(addressTemplate(address), addressEl);
+}
 
-export function renderAcceptButton(props: State) {
+export function renderAcceptTools(props: State) {
   const actionContainer = getById("action-container");
   actionContainer.innerHTML = "";
-  render(acceptButton(props), actionContainer);
+  render(acceptTools(props), actionContainer);
+}
+
+export function renderAcceptButton(props: State) {
+  const buttonSlot = getById("button-slot");
+  render(AcceptButton(), buttonSlot);
 }
 
 export async function renderLoadingIndicator(to: string) {
@@ -102,4 +117,52 @@ export function removeButtons() {
 
 export function removeAcceptedButton() {
   getById("accept-button").innerHTML = "";
+}
+
+export function revertPrompt() {
+  const prompt = getPromptEl();
+  prompt.textContent = "Drop PDF here or click to upload";
+  prompt.style.color = "black";
+}
+
+export function updatePromptSuccess(file: File) {
+  const prompt = getPromptEl();
+  prompt.style.color = "black";
+  prompt.textContent = file.name;
+}
+
+export function updatePromptError(message: string) {
+  const prompt = getPromptEl();
+  prompt.textContent = message;
+  prompt.style.color = "red";
+}
+
+export function renderTooltips() {
+  const onlySigner = getById("onlysigner-tooltip");
+  const expires = getById("expires-tooltip");
+  const postto = getById("postto-tooltip");
+  const webhook = getById("webhook-tooltip");
+  const redirect = getById("redirect-tooltip");
+
+  render(
+    helperTooltips("The only arweave address that can sign this contract."),
+    onlySigner
+  );
+  render(helperTooltips("The contract expires always at midnight"), expires);
+  render(
+    helperTooltips(
+      "Posts to the url with path /{id}. Choose bellow the preferred method."
+    ),
+    postto
+  );
+  render(
+    helperTooltips(
+      "The webhook option offers an extra field on the contract for a pre-shared secret. It will be posted as {secret : string} to /{id}"
+    ),
+    webhook
+  );
+  render(
+    helperTooltips("The contract redirects in 5 seconds to path /{id}."),
+    redirect
+  );
 }
