767aa7ad06fc9c41a593153ffb53f411bde3515f
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-24 18:25:49 +0100

    add arweave props setting flow changes

--- lib/state/setStateHook.ts
+++ lib/state/setStateHook.ts
@@ -1,6 +1,7 @@
 import {
   dispatch_attachDateClickListener,
   dispatch_renderAcceptButton,
+  dispatch_renderAddress,
   dispatch_renderBalance,
   dispatch_renderCreateButton,
   dispatch_renderVersion,
@@ -11,8 +12,9 @@ export const setStateHook = {
   [StateProperties.arweave]: (args: SetHookArgs) => {
     const currentPage = args.obj.contracttype;
     if (currentPage === ContractTypes.create) {
-      dispatch_renderCreateButton(cloneState(args.obj));
-      dispatch_renderVersion(args.obj.version);
+      const clone = cloneState(args.obj);
+      dispatch_renderCreateButton(clone);
+      dispatch_renderVersion(clone.version);
     } else if (currentPage === ContractTypes.acceptable) {
       dispatch_renderAcceptButton(cloneState(args.obj));
     }
@@ -21,7 +23,9 @@ export const setStateHook = {
   [StateProperties.balance]: (args: SetHookArgs) => {
     dispatch_renderBalance(cloneState(args.obj));
   },
-  [StateProperties.address]: (args: SetHookArgs) => {},
+  [StateProperties.address]: (args: SetHookArgs) => {
+    dispatch_renderAddress(cloneState(args.obj));
+  },
   [StateProperties.selectedDate]: (args: SetHookArgs) => {
     dispatch_attachDateClickListener(cloneState(args.obj));
   },
