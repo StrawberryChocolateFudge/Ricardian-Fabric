4d276e14b0290da5c6d666e5fbbde0fe40e64e11
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-24 18:28:19 +0100

    update renderOnAcceptClick flow

--- lib/view/actions/renderAcceptButton.ts
+++ lib/view/actions/renderAcceptButton.ts
@@ -1,38 +1,29 @@
-import {
-  acceptAndPayContract,
-  acceptContract,
-  isOnlySigner,
-} from "../../business/bloc";
+import { createFulfilledContractTx, isOnlySigner } from "../../business/bloc";
 import {
   dispatch_removeError,
   dispatch_renderError,
+  dispatch_renderFee,
 } from "../../dispatch/render";
-import { State } from "../../types";
+import { FileType, State } from "../../types";
 import { getById, readFile } from "../utils";
 
 export function renderAcceptOnCLick(props: State) {
   const acceptButton = getById("accept-button") as HTMLInputElement;
 
   acceptButton.onclick = async function () {
-    const wallet_file = getById("select-file-input") as HTMLInputElement;
+    const wallet_file = getById("wallet-input") as HTMLInputElement;
     const getKey = async (key: any) => {
       dispatch_removeError();
       const validSigner = await isOnlySigner(props, key);
       if (validSigner) {
-        const price = props.price;
-        if (price !== "NONE") {
-          await acceptAndPayContract({
-            props,
-            ar: price,
-            key,
-          });
-        } else {
-          await acceptContract(props, key);
-        }
+        const tx = await createFulfilledContractTx(props, key);
+        const txfee = props.arweave.ar.winstonToAr(tx.reward);
+
+        dispatch_renderFee(txfee, props, tx, key);
       } else {
         dispatch_renderError("You are not allowed to sign this contract");
       }
     };
-    readFile(wallet_file.files, getKey);
+    readFile(wallet_file.files, getKey, FileType.key);
   };
 }
