79f591368b5b2bd294251a62063ba573c49dddcf
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-08-26 14:16:13 +0100

    add decomissioning

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 import "./lib/state";
 import "./lib/view";
-import { getArweave, showBanner } from "./lib/business/bloc";
+import { decomissioned, getArweave, showBanner } from "./lib/business/bloc";
 import "./lib/view/templates/acceptablePage";
-import { getById } from "./lib/view/utils";
 (async function init() {
-  console.log(getById("main-script"));
-
   showBanner();
+  decomissioned();
   await getArweave();
 })();
