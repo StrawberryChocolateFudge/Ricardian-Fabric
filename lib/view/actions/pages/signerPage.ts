d4a74e9139e0aa83fc0f3f7b1076304fb5af3e34
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-26 16:39:29 +0100

    add signerPage

---
+++ lib/view/actions/pages/signerPage.ts
@@ -0,0 +1,18 @@
+import { goToCreateRoutes } from "../../../dispatch/dispatch";
+import { State } from "../../../types";
+import { getById } from "../../utils";
+
+export function signerPage(props: State) {
+  const cancelButton = getById("signer-cancel");
+  const nextButton = getById("signer-save");
+  const addSelectedCountry = getById("add-country");
+
+  cancelButton.onclick = function () {
+    goToCreateRoutes();
+  };
+
+  nextButton.onclick = function () {
+    goToCreateRoutes();
+  };
+
+}
