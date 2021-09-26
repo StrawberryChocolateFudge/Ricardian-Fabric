dfcd9c8cf0430588e6c6ef58041ac491a67edf7d
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-26 16:39:19 +0100

    add new page

---
+++ lib/view/actions/pages/semanticsPage.ts
@@ -0,0 +1,48 @@
+import { goToCreateRoutes } from "../../../dispatch/dispatch";
+import createNewEditor from "../../../state/editor";
+import { State } from "../../../types";
+import { getById } from "../../utils";
+
+export function semanticsPage(props: State) {
+  console.log("semantics page runs");
+  const editor = createNewEditor();
+
+  //Dispatch init like agreementPage
+
+  //GET the CHECKBOXES
+
+  const docx = getById("docX-checkbox") as HTMLInputElement;
+  const type = getById("type-checkbox") as HTMLInputElement;
+
+  docx.onchange = function () {
+    if (docx.checked) {
+      type.checked = false;
+    }
+  };
+
+  type.onchange = function () {
+    if (type.checked) {
+      docx.checked = false;
+    }
+  };
+
+  const cancelButton = getById("semantics-previous");
+  const saveButton = getById("semantics-save");
+
+  cancelButton.onclick = function () {
+    goToCreateRoutes();
+  };
+
+  saveButton.onclick = function () {
+    // let content =
+
+    // if(docx )
+
+    const content = editor.getContent();
+
+    //Dispatch the content!!
+
+    //TODO: Do the Save
+    goToCreateRoutes();
+  };
+}
