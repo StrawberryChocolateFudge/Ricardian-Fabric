ec4daf59142f075661b07b5b6be8273dec57d26c
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-26 16:33:13 +0100

    add goToCreateRoutes

--- lib/dispatch/dispatch.ts
+++ lib/dispatch/dispatch.ts
@@ -1,4 +1,12 @@
+import { CreatePages } from "../types";
+import { dispatch_setCreatePages } from "./stateChange";
+
 //Dispatch an event to trigger something
 export function dispatch(to :any, detail : any) {
   document.body.dispatchEvent(new CustomEvent(to, { detail }));
+}
+
+
+export function goToCreateRoutes(){
+  dispatch_setCreatePages(CreatePages.Routes);
 }
