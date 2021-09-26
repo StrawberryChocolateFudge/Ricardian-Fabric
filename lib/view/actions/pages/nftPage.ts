37d19ebba98983151723ae5e3b061b80608a53e6
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-26 16:38:49 +0100

    add nftPage WIP

---
+++ lib/view/actions/pages/nftPage.ts
@@ -0,0 +1,18 @@
+import { goToCreateRoutes } from "../../../dispatch/dispatch";
+import { State } from "../../../types";
+import { getById } from "../../utils";
+
+export function nftPage(props: State) {
+  //Dispatch the initialization
+
+  const cancelButton = getById("nftpage-cancel");
+  const saveButton = getById("nftpage-save");
+
+  cancelButton.onclick = function (e: Event) {
+    goToCreateRoutes();
+  };
+
+  saveButton.onclick = function (e: Event) {
+    goToCreateRoutes();
+  };
+}
