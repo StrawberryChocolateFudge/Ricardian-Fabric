8d01f1e78ad05881e6e5d3c844335be20781dbd4
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-26 16:37:55 +0100

    add inputs page

---
+++ lib/view/actions/pages/inputsPage.ts
@@ -0,0 +1,17 @@
+import { goToCreateRoutes } from "../../../dispatch/dispatch";
+import { State } from "../../../types";
+import { getById } from "../../utils";
+
+export function inputsPage(props: State) {
+  // ToDo: init
+
+  const cancelButton = getById("input-cancel");
+  const saveButton = getById("input-save");
+
+  cancelButton.onclick = function () {
+    goToCreateRoutes();
+  };
+  saveButton.onclick = function () {
+    goToCreateRoutes();
+  };
+}
