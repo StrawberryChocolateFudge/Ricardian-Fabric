04d6ba9402d9586b7ff37ac0f1b0bf0d4cd650a7
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-26 16:38:28 +0100

    update, networking changed

--- lib/view/actions/pages/networkingPage.ts
+++ lib/view/actions/pages/networkingPage.ts
@@ -1,31 +1,40 @@
-import { dispatch_initNetworkingPage, dispatch_removeError, dispatch_renderError } from "../../../dispatch/render";
-import { dispatch_setCreatePages, dispatch_setNetworkingPage } from "../../../dispatch/stateChange";
-import { CreatePages, State } from "../../../types";
-import { getById, getPostTo, getRedirectCheckbox, getWebhookCheckbox } from "../../utils";
+import { goToCreateRoutes } from "../../../dispatch/dispatch";
+import {
+  dispatch_initNetworkingPage,
+  dispatch_removeError,
+  dispatch_renderError,
+} from "../../../dispatch/render";
+import { dispatch_setNetworkingPage } from "../../../dispatch/stateChange";
+import { State } from "../../../types";
+import {
+  getById,
+  getPostTo,
+  getRedirectCheckbox,
+  getWebhookCheckbox,
+} from "../../utils";
 
-export function networkingPage(props: State){
- dispatch_initNetworkingPage(props);
-    const prevButton = getById("NetworkingPage-previous");
-    const nextButton = getById("NetworkingPage-next");
+export function networkingPage(props: State) {
+  dispatch_initNetworkingPage(props);
+  const prevButton = getById("NetworkingPage-previous");
+  const nextButton = getById("NetworkingPage-next");
 
-    prevButton.onclick = function (e: Event) {
-      dispatch_setCreatePages(CreatePages.SmartContract);
-    };
+  prevButton.onclick = function (e: Event) {
+    goToCreateRoutes();
+  };
 
-    nextButton.onclick = function (e: Event) {
-      dispatch_removeError();
-      const postto = getPostTo();
-      const webhook = getWebhookCheckbox();
-      const redirect = getRedirectCheckbox();
-
-      if (webhook || redirect) {
-        if (postto === "NONE") {
-          dispatch_renderError("Post to, where?");
-          return;
-        }
+  nextButton.onclick = function (e: Event) {
+    dispatch_removeError();
+    const postto = getPostTo();
+    const webhook = getWebhookCheckbox();
+    const redirect = getRedirectCheckbox();
+    if (webhook || redirect) {
+      if (postto === "NONE") {
+        dispatch_renderError("Post to, where?");
+        return;
       }
+    }
 
-      dispatch_setNetworkingPage({ postto, webhook, redirect });
-      dispatch_setCreatePages(CreatePages.SummaryPage);
-    };	
+     dispatch_setNetworkingPage({ postto, webhook, redirect, });
+    goToCreateRoutes();
+  };
 }
