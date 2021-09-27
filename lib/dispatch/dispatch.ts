aafd33536d19bd4e22f0ba2a9904846e9c87f144
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-27 14:18:56 +0100

    add new dispatch abstraction

--- lib/dispatch/dispatch.ts
+++ lib/dispatch/dispatch.ts
@@ -1,12 +1,19 @@
-import { CreatePages } from "../types";
-import { dispatch_setCreatePages } from "./stateChange";
+import { AccountantPage, CreatePages, ManagerPages } from "../types";
+import {
+  dispatch_setAccountantPages,
+  dispatch_setCreatePages,
+  dispatch_setManagerPages,
+} from "./stateChange";
 
 //Dispatch an event to trigger something
-export function dispatch(to :any, detail : any) {
+export function dispatch(to: any, detail: any) {
   document.body.dispatchEvent(new CustomEvent(to, { detail }));
 }
 
-
-export function goToCreateRoutes(){
+export function goToCreateRoutes() {
   dispatch_setCreatePages(CreatePages.Routes);
+}
+
+export function gotToManagerRoutes() {
+  dispatch_setManagerPages(ManagerPages.Routes);
 }
