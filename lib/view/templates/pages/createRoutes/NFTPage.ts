17bfaade3db532652a1746d93568579bbee73137
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-29 14:33:19 +0100

    update NFT page

--- lib/view/templates/pages/createRoutes/NFTPage.ts
+++ lib/view/templates/pages/createRoutes/NFTPage.ts
@@ -3,6 +3,9 @@ import { html } from "lit-html";
 export const NFTPage = () => html`
   <h2 class="center">NFT</h2>
   <hr />
+  <h5 class="center">
+    Allow your customers to create a non-fungible token from the agreement
+  </h5>
   <table aria-label="nft field table" class="center">
     <tr>
       <th></th>
@@ -11,13 +14,15 @@ export const NFTPage = () => html`
     </tr>
     <tr>
       <td>
-        <label aria-labelledby="Allow nft minting">Allow NFT minting</label>
+        <label for="allowNFT-checkbox" aria-labelledby="Allow nft minting"
+          >Allow NFT minting</label
+        >
       </td>
       <td>
         <input
           aria-label="allow minting of NFT"
           name="allowNFT"
-          id="allowNFT-Checkbox"
+          id="allowNFT-checkbox"
           type="checkbox"
         />
       </td>
@@ -26,27 +31,42 @@ export const NFTPage = () => html`
       </td>
     </tr>
     <tr>
-      <td><label aria-labelledby="Nft name input">Title:</label></td>
-      <td><input aria-label="nft name input" id="nft-name" type="text" /></td>
+      <td>
+        <label for="nft-title" aria-labelledby="Nft name input">Title:</label>
+      </td>
+      <td><input aria-label="nft name input" id="nft-title" type="text"/></td>
       <td><span id="nft-name-tooltip"></span></td>
     </tr>
     <tr>
-      <td><label aria-labelledby="Nft name input">Name:</label></td>
+      <td>
+        <label for="nft-name" aria-labelledby="Nft name input">Name:</label>
+      </td>
       <td><input aria-label="nft name input" id="nft-name" type="text" /></td>
       <td><span id="nft-name-tooltip"></span></td>
     </tr>
-    <tr>
-      <td><label aria-labelledby="Nft name input">Description:</label></td>
-      <td><input aria-label="nft name input" id="nft-name" type="text" /></td>
-      <td><span id="nft-name-tooltip"></span></td>
-    </tr>
-    <tr>
-      <td><label aria-labelledby="Nft ticker input">Ticker:</label></td>
+     <tr>
+      <td>
+        <label for="nft-ticker" aria-labelledby="Nft ticker input"
+          >Ticker:</label
+        >
+      </td>
       <td>
         <input aria-label="nft ticker input" id="nft-ticker" type="text" />
       </td>
       <td><span id="nft-ticker-tooltip"></span></td>
     </tr>
+    <tr>
+      <td>
+        <label for="nft-description" aria-labelledby="Nft name input"
+          >Description:</label
+        >
+      </td>
+      <td>
+        <textarea aria-label="nft name input" id="nft-description" type="text" ></textarea>
+      </td>
+      <td><span id="nft-name-tooltip"></span></td>
+    </tr>
+   
   </table>
   <div
     aria-label="error-display-slot"
@@ -54,7 +74,9 @@ export const NFTPage = () => html`
     id="error-display"
   ></div>
   <div class="button-row">
-    <button class="marginRight-20 cancel-button" id="nftpage-cancel">Cancel</button>
+    <button class="marginRight-20 cancel-button" id="nftpage-cancel">
+      Cancel
+    </button>
     <button class="marginLeft-20 save-button" id="nftpage-save">Save</button>
   </div>
 `;
