e2f0499252a677942dcc8aa88232d92ed8471482
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-29 15:40:28 +0100

    add inputs page implementation

--- lib/view/actions/pages/createRoutes/inputsPage.ts
+++ lib/view/actions/pages/createRoutes/inputsPage.ts
@@ -1,17 +1,61 @@
 import { goToCreateRoutes } from "../../../../dispatch/dispatch";
+import {
+  dispatch_initInputsPage,
+  dispatch_renderInputs,
+} from "../../../../dispatch/render";
+import { dispatch_setInputsPageData } from "../../../../dispatch/stateChange";
 import { State } from "../../../../types";
-import { getById } from "../../../utils";
+import { getById, getRequiredInputs } from "../../../utils";
 
 export function inputsPage(props: State) {
-  // ToDo: init
+  dispatch_initInputsPage(props);
 
-  const cancelButton = getById("input-cancel");
-  const saveButton = getById("input-save");
+  const cancelButtonEl = getById("input-cancel");
+  const saveButtonEl = getById("input-save");
 
-  cancelButton.onclick = function () {
+  const addNewInputEl = getById("add-input-item") as HTMLButtonElement;
+  const newInputEl = getById("new-input-name") as HTMLInputElement;
+
+  const inputs: Array<string> = props.inputsPage.requiredInputs.slice();
+
+  addNewInputEl.onclick = function () {
+    const input = newInputEl.value;
+
+    if (input === "") {
+      return "";
+    }
+    if (inputs.includes(input)) {
+      return;
+    }
+
+    inputs.push(input);
+    newInputEl.value = "";
+    dispatchInputListRenders(inputs);
+  };
+
+  cancelButtonEl.onclick = function () {
     goToCreateRoutes();
   };
-  saveButton.onclick = function () {
+  saveButtonEl.onclick = function () {
+    const requiredInputs = getRequiredInputs();
+    dispatch_setInputsPageData({ requiredInputs });
     goToCreateRoutes();
   };
+}
+
+export function attachListenersToRemoveInputButtons(inputs: Array<string>) {
+  inputs.forEach((input) => {
+    const currentEl = getById(`remove-input-${input}`);
+    currentEl.onclick = function (ev: any) {
+      const index = inputs.indexOf(ev.target.name);
+      inputs.splice(index, 1);
+      dispatchInputListRenders(inputs);
+      console.log(inputs);
+    };
+  });
+}
+
+export function dispatchInputListRenders(inputs: Array<string>) {
+  dispatch_renderInputs(inputs);
+  attachListenersToRemoveInputButtons(inputs);
 }
