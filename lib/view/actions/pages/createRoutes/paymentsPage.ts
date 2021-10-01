0455243fea3f8745a161bb349011673445046ca1
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-01 18:16:35 +0100

    add stock and validation

--- lib/view/actions/pages/createRoutes/paymentsPage.ts
+++ lib/view/actions/pages/createRoutes/paymentsPage.ts
@@ -5,7 +5,7 @@ import {
 } from "../../../../dispatch/render";
 import { dispatch_setPaymentsPageData } from "../../../../dispatch/stateChange";
 import { State } from "../../../../types";
-import { getById, getPrice } from "../../../utils";
+import { getById, getPrice, getStock } from "../../../utils";
 
 export function paymentsPage(props: State) {
   dispatch_initPaymentsPage(props);
@@ -40,20 +40,50 @@ export function paymentsPage(props: State) {
       return;
     }
 
-    const percentage = percentageEl.value;
+    const stock = getStock();
 
-    if (parseFloat(percentage) < 0) {
-      dispatch_renderError("Percentage can't be negative!");
+    if (parseFloat(stock) < 0) {
+      dispatch_renderError("Stock can't be negative!");
       return;
     }
+
+    const percentage = percentageEl.value;
 
     const willProfitShare = isProfitSharingEl.checked;
     const pstContractId = pscEl.value;
     const accountantContractId = accountantContractEl.value;
     const needsKYC = needsKYCEl.checked;
 
+    if (willProfitShare) {
+      if (parseFloat(percentage) < 0) {
+        dispatch_renderError("Percentage can't be negative!");
+        return;
+      }
+
+      if (parseFloat(percentage) === NaN) {
+        dispatch_renderError("Percentage needs to be a number");
+        return;
+      }
+
+      if (parseFloat(percentage) > 99.5) {
+        dispatch_renderError("Maximum fee you can add is 99.5%");
+        return;
+      }
+
+      // if (pstContractId === "") {
+      //   dispatch_renderError("The contract id must be set");
+      //   return;
+      // }
+
+      if (accountantContractId === "") {
+        dispatch_renderError("The accountant must be set");
+        return;
+      }
+    }
+
     dispatch_setPaymentsPageData({
       price,
+      stock,
       willProfitShare,
       percentage,
       pstContractId,
