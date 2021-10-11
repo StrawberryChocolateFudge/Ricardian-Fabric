82d660894aa335433a7602161b9dd6dd412ddffc
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-11 18:10:32 +0100

    WIP

--- lib/view/actions/renderAcceptButton.ts
+++ lib/view/actions/renderAcceptButton.ts
@@ -1,30 +1,27 @@
-import { createFulfilledContractTx, isOnlySigner } from "../../business/bloc";
+import { isOnlySigner } from "../../business/bloc";
 import {
-  dispatch_disableAcceptableInputs,
   dispatch_removeError,
-  dispatch_renderError,
-  dispatch_renderFee,
 } from "../../dispatch/render";
-import { FileType, State } from "../../types";
-import { getById, readFile } from "../utils";
+import { State } from "../../types";
+import { getById } from "../utils";
 
 export function renderAcceptOnCLick(props: State) {
   const acceptButton = getById("accept-button") as HTMLInputElement;
 
   acceptButton.onclick = async function () {
-    const wallet_file = getById("wallet-input") as HTMLInputElement;
-    const getKey = async (key: any) => {
+    // const wallet_file = getById("wallet-input") as HTMLInputElement;
+    // const getKey = async (key: any) => {
       dispatch_removeError();
-      const validSigner = await isOnlySigner(props, key);
-      if (validSigner) {
-        const tx = await createFulfilledContractTx(props, key);
-        const txfee = props.arweave.ar.winstonToAr(tx.reward);
-        dispatch_disableAcceptableInputs();
-        dispatch_renderFee(txfee, props, tx, key);
-      } else {
-        dispatch_renderError("You are not allowed to sign this contract");
-      }
+      // const validSigner = await isOnlySigner(props, key);
+      // if (validSigner) {
+      //   // const tx = await createFulfilledContractTx(props, key);
+      //   // const txfee = props.arweave.ar.winstonToAr(tx.reward);
+      //   dispatch_disableAcceptableInputs();
+      //   // dispatch_renderFee(txfee, props, tx, key);
+      // } else {
+      //   dispatch_renderError("You are not allowed to sign this contract");
+      // }
     };
-    readFile(wallet_file.files, getKey, FileType.key);
+    // readFile(wallet_file.files, getKey, FileType.key);
   };
 }
