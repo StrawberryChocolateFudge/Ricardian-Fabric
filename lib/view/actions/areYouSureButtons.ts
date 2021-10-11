c765d75ebc0deca154ef1afa727e043d64d09cce
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-11 18:07:31 +0100

    add ipfs

--- lib/view/actions/areYouSureButtons.ts
+++ lib/view/actions/areYouSureButtons.ts
@@ -1,14 +1,15 @@
-import { postTransactionFromPage } from "../../business/bloc";
 import {
   dispatch_enableAcceptableInputs,
   dispatch_enableCreateInputs,
   dispatch_noButtonPressed,
+  dispatch_renderTransaction,
   dispatch_yesButtonPressed,
 } from "../../dispatch/render";
+import { IPFS_Add } from "../../ipfs/add";
 import { State } from "../../types";
 import { getById } from "../utils";
 
-export function areYouSureButtons(props: any) {
+export function areYouSureButtons(props: State) {
   const noButton = getById("no-button");
   const yesButton = getById("yes-button");
   noButton.onclick = function () {
@@ -19,6 +20,9 @@ export function areYouSureButtons(props: any) {
 
   yesButton.onclick = async function () {
     dispatch_yesButtonPressed(props);
-    await postTransactionFromPage(props as State, props.tx, props.key);
+
+    const CID = await IPFS_Add(props.stashedPage, props.ipfs);
+
+    dispatch_renderTransaction(`https://ipfs.infura.io/ipfs/${CID.toString()}`);
   };
 }
