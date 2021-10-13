513741ba88232a4ad4dd1637243984c872bc3d42
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-13 16:41:10 +0100

    add permapining!

--- lib/view/actions/areYouSureButtons.ts
+++ lib/view/actions/areYouSureButtons.ts
@@ -4,12 +4,14 @@ import {
   dispatch_enableAcceptableInputs,
   dispatch_enableCreateInputs,
   dispatch_noButtonPressed,
+  dispatch_renderError,
   dispatch_renderLoadingIndicator,
   dispatch_renderTransaction,
   dispatch_yesButtonPressed,
 } from "../../dispatch/render";
+import { permapin } from "../../fetch";
 import { IPFS_Add } from "../../ipfs/add";
-import { ContractTypes, State } from "../../types";
+import { ContractTypes, PinOptions, PinStatus, State } from "../../types";
 import { getById } from "../utils";
 
 export function areYouSureButtons(props: State) {
@@ -26,19 +28,23 @@ export function areYouSureButtons(props: State) {
     dispatch_yesButtonPressed(props);
 
     const CID = await IPFS_Add(props.stashedPage, props.ipfs);
-    dispatch_renderTransaction(
-      props,
-      `http://localhost:8080/ipfs/${CID.toString()}`
-    );
+    const id = `${CID.toString()}`;
+    dispatch_renderTransaction(props, `http://localhost:8080/ipfs/${id}`);
 
-    //TODO: Permapin!
 
-    if (props.contracttype === ContractTypes.create) {
-      dispatch_deployAgain(props);
-    } else {
-      
-      handlePost(props, `${CID.toString()}`);
-      
-    }
+    await permapin(id, props.ipfsArweaveBridge).then(async (res : PinOptions) => {
+
+      if(res.status === PinStatus.Failure){
+        dispatch_renderError(res.error);
+      } else {
+        
+      }
+
+      if (props.contracttype === ContractTypes.create) {
+        dispatch_deployAgain(props);
+      } else {
+        await handlePost(props, `${CID.toString()}`);
+      }
+    });
   };
 }
