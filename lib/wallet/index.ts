9b36a248cc906ccc600913f9ceefd76aa00de210
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-18 15:54:59 +0100

    add setTerms, accept,canAgree ...

--- lib/wallet/index.ts
+++ lib/wallet/index.ts
@@ -1,12 +1,14 @@
 import Web3 from "web3";
-import { recoverTypedSignatureLegacy } from "eth-sig-util";
-import { toChecksumAddress } from "ethereumjs-util";
 
+import { recoverTypedSignature } from "eth-sig-util";
+import { ECDSASignature, fromRpcSig, toChecksumAddress } from "ethereumjs-util";
+import { getAgreementAbi } from "./abi/Agreement";
+import { Options, Status } from "../types";
 export async function requestAccounts() {
   //TODO: refactor to request
   await window.ethereum.send("eth_requestAccounts");
 }
-
+//TODO: wallet_switchEthereumChain
 export async function switchToHarmony() {
   await window.ethereum.request({
     method: "wallet_addEthereumChain",
@@ -26,38 +28,63 @@ export async function switchToHarmony() {
   });
 }
 
-export async function getAddress(web3: Web3): Promise<string> {
+export async function getAddress(): Promise<string> {
+  const web3 = new Web3(window.ethereum);
   const accounts = await web3.eth.getAccounts();
 
   return accounts[0];
 }
 
-export async function getNetwork(web3: Web3): Promise<number> {
-  return await web3.eth.getChainId();
+export async function getNetwork(): Promise<number> {
+  const hex = window.ethereum.chainId;
+  return Number(hex);
 }
 
 export async function signHash(
   hash: string,
-  web3: Web3,
-  signer: string,
+  from: string,
+  networkId: string,
+  smartContract: string,
   onSuccess: CallableFunction,
   onError: CallableFunction
 ) {
-  const msgParams = [{ type: "string", name: "Document Hash", value: hash }];
-
-  //@ts-ignore
-  await web3.currentProvider.sendAsync(
+  // const msgParams = [{ type: "string", name: "Document Hash", value: hash }];
+  const msgParams = {
+    domain: {
+      chainId: networkId,
+      name: "Ricardian Fabric",
+      verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+      version: "1",
+    },
+    types: {
+      EIP712Domain: [
+        { name: "name", type: "string" },
+        { name: "version", type: "string" },
+        { name: "chainId", type: "uint256" },
+        { name: "verifyingContract", type: "address" },
+      ],
+      doc: [{ name: "value", type: "string" }],
+    },
+    primaryType: "doc",
+    message: {
+      value: hash,
+    },
+  };
+  console.log(msgParams);
+  await window.ethereum.sendAsync(
     {
-      method: "eth_signTypedData",
-      params: [msgParams, signer],
-      from: signer,
+      method: "eth_signTypedData_v3",
+      params: [from, JSON.stringify(msgParams)]
     },
     async function (err, result) {
       if (result.error) {
         onError(result.error.message);
       } else {
-        const recovered = await recoverTypedSignature(msgParams, result.result);
-        if (compareAddresses(signer, recovered)) {
+        const recovered = await recoverTypedSignatures(msgParams, result.result);
+        console.log(recovered + "       recovered");
+        if (compareAddresses(from, recovered)) {
+          console.log(getSigParams(result.result));
+
           await onSuccess(result.result);
         } else {
           await onError("Signature verification failed.");
@@ -67,8 +94,8 @@ export async function signHash(
   );
 }
 
-export async function recoverTypedSignature(msgParams, signature) {
-  const recovered = await recoverTypedSignatureLegacy({
+export async function recoverTypedSignatures(msgParams, signature) {
+  const recovered = await recoverTypedSignature({
     data: msgParams,
     sig: signature,
   });
@@ -92,13 +119,125 @@ export function web3Injected(): boolean {
   }
 }
 
+export function getSigParams(sig: string): ECDSASignature {
+  return fromRpcSig(sig);
+}
+
+// I'm using web3 in the below function because it throws errors nice for this validation.
+export async function canUseContract(
+  address: string,
+  issuer: string
+): Promise<boolean> {
+  let success = true;
+  const web3 = new Web3(window.ethereum);
+  try {
+    const agreementContract = await new web3.eth.Contract(
+      getAgreementAbi(),
+      address
+    );
+
+    //If accepted terms is false, means I can use
+    const resul = await agreementContract.methods.acceptedTerms(issuer).call();
+  } catch (err) {
+    success = false;
+  }
+
+  return success;
+}
+
+// A function to get a boolean, if I can agree to the contract or not
+export async function canAgree(
+  contractAddress: string,
+  signerAddress: string
+): Promise<boolean> {
+  try {
+    const web3 = new Web3(window.ethereum);
+
+    const contract = new web3.eth.Contract(getAgreementAbi(), contractAddress);
+
+    const alreadyAccepted = await contract.methods
+      .acceptedTerms(signerAddress)
+      .call();
+
+    return !alreadyAccepted;
+  } catch (err) {
+    console.log(err);
+    console.log("error occured");
+  }
+  return false;
+}
+
 // I need a function to add a new agreement to a compatible smart contract
 // Pass in the document hash,issuer,signature,network
-export function newAgreement() {}
+export async function setTerms(arg: {
+  url: string;
+  hash: string;
+  contractAddress: string;
+  signerAddress: string;
+}): Promise<Options> {
+  const options: Options = {
+    error: "",
+    status: Status.Success,
+    data: {},
+  };
+  try {
+    const web3 = new Web3(window.ethereum);
+
+    const contract = new web3.eth.Contract(
+      getAgreementAbi(),
+      arg.contractAddress
+    );
+
+    const resp = await contract.methods
+      .setTerms(arg.url, arg.hash)
+      .send({ from: arg.signerAddress });
+
+    options.data = resp;
+  } catch (err) {
+    options.status = Status.Failure;
+    options.error = err.message;
+  }
+
+  return options;
+}
 
 //I need to call an Accept function on a smart contract
 // Pass in the document,
-export function acceptAgreement() {}
+export async function acceptAgreement(arg: {
+  url: string;
+  hash: string;
+  contractAddress: string;
+  signature: string;
+  signerAddress: string;
+}): Promise<Options> {
+  const options: Options = {
+    error: "",
+    status: Status.Success,
+    data: {},
+  };
+  try {
+    const web3 = new Web3(window.ethereum);
 
-// A function to get a boolean, if I can agree to the contract or not
-export function canAgree() {}
+    const contract = new web3.eth.Contract(
+      getAgreementAbi(),
+      arg.contractAddress
+    );
+
+    const sigParams = getSigParams(arg.signature);
+
+    const result = await contract.methods
+      .recoverSignature(arg.hash, sigParams.v, sigParams.r, sigParams.s)
+      .call();
+  
+    const sifg = await recoverTypedSignatures(arg.hash, arg.signature);
+    const resp = await contract.methods
+      .accept(arg.url, arg.hash, sigParams.v, sigParams.r, sigParams.s)
+      .send({ from: arg.signerAddress });
+
+  } catch (err) {
+    options.status = Status.Failure;
+    options.error = err.message;
+  }
+
+  return options;
+}
