287426111f6f21aa1927bb50f4f1421c4a4d6006
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-19 19:35:03 +0100

    removed unused function

--- lib/fetch/index.ts
+++ lib/fetch/index.ts
@@ -1,26 +1,5 @@
 import { PinOptions, Status } from "../types";
 
-export async function hitWebhook(url: string, secret: string) {
-  // The backend must handle cross origin requests for this to work!
-  try {
-    const response = await fetch(url, {
-      method: "post",
-      mode: "cors",
-      cache: "no-cache",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        secret,
-      }),
-    });
-    // I don't need the response right now, just want to trigger a webhook
-    // const json = await response.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 export async function permapin(CID: string, url: string): Promise<PinOptions> {
   try {
