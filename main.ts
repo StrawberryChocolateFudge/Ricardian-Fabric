acf81a01b92828097118d139118f54e18e9a97a7
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-19 19:34:52 +0100

    removed unused dependency

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import "./lib/state";
 import "./lib/view";
 import { showBanner } from "./lib/business/bloc";
 import "./lib/view/templates/acceptablePage";
-import { dispatch_setInit, dispatch_setIPFS } from "./lib/dispatch/stateChange";
+import { dispatch_setInit } from "./lib/dispatch/stateChange";
 (async function init() {
   showBanner();
   dispatch_setInit();
