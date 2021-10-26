4a538c7301ad4d07829c9f02b6a624255ac3fb8d
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-26 14:53:13 +0100

    added checked

--- lib/view/templates/sanctionsDropdown.ts
+++ lib/view/templates/sanctionsDropdown.ts
@@ -14,6 +14,7 @@ export const SanctionsDropdown = (expand: boolean) => html`
         aria-label="ofec checkbox"
         id="ofec_checkbox"
         type="checkbox"
+        checked
       /><span class="sanctions-span">OFEC sanctioned countries</span>
     </li>
     <li>
@@ -21,6 +22,7 @@ export const SanctionsDropdown = (expand: boolean) => html`
         aria-label="eu sanctions checkbox"
         id="eu-checkbox"
         type="checkbox"
+        checked
       /><span class="sanctions-span">EU sanctioned countries</span>
     </li>
     <li>
@@ -28,6 +30,7 @@ export const SanctionsDropdown = (expand: boolean) => html`
         aria-label="UN sanctions checkbox"
         id="un-checkbox"
         type="checkbox"
+        checked
       /><span class="sanctions-span">UN sanctioned countries</span>
     </li>
     <li>
