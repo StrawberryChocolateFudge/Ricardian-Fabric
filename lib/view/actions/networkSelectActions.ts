398947f0c36bf6830a743ff8fdbf49242238ba00
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-26 14:51:51 +0100

    add network switching

--- lib/view/actions/networkSelectActions.ts
+++ lib/view/actions/networkSelectActions.ts
@@ -1,8 +1,11 @@
-import { dispatch_renderError } from "../../dispatch/render";
+import {
+  dispatch_deploySCIntent,
+  dispatch_renderError,
+} from "../../dispatch/render";
 import { switchNetwork, web3Injected } from "../../wallet";
 import { getById } from "../utils";
 import MetaMaskOnboarding from "@metamask/onboarding";
-import { State } from "../../types";
+import { Chains, State } from "../../types";
 
 export function networkSelectActions() {
   const switchnetworkToggle = getById(
@@ -54,10 +57,35 @@ export function networkSelectActions() {
   };
 }
 
-export function addChainButtonListener(props :State){
+export function addChainButtonListener(props: State) {
   const addChainButton = getById("addChainButton");
-  addChainButton.onclick = async function(){
-       await switchNetwork("Harmony", 0, "Testnet");
+  addChainButton.onclick = async function () {
+    const chains = {
+      [Chains.HmnyMainnetShard0]: async () =>
+        await switchNetwork("Harmony", 0, "Mainnet"),
+      [Chains.HmnyMainnetShard1]: async () =>
+        await switchNetwork("Harmony", 1, "Mainnet"),
+      [Chains.HmnyMainnetShard2]: async () =>
+        await switchNetwork("Harmony", 2, "Mainnet"),
+      [Chains.HmnyMainnetShard3]: async () =>
+        await switchNetwork("Harmony", 3, "Mainnet"),
+      [Chains.HmnyTestnetShard0]: async () =>
+        await switchNetwork("Harmony", 0, "Testnet"),
+      [Chains.HmnyTestnetShard1]: async () =>
+        await switchNetwork("Harmony", 1, "Testnet"),
+      [Chains.HmnyTestnetShard2]: async () =>
+        await switchNetwork("Harmony", 2, "Testnet"),
+      [Chains.HmnyTestnetShard3]: async () =>
+        await switchNetwork("Harmony", 3, "Testnet"),
+    };
+    await chains[props.network]();
+  };
+}
 
-  }
+export function addDeployButtonListener(props: State) {
+  const deployButton = getById("deploy-sc-button");
+
+  deployButton.onclick = function () {
+    dispatch_deploySCIntent(props);
+  };
 }
