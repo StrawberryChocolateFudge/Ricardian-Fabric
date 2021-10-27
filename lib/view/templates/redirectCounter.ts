919db5587ca74a1faddca37cbd8991a9768d2d5e
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-27 15:43:10 +0100

    add redirect button

--- lib/view/templates/redirectCounter.ts
+++ lib/view/templates/redirectCounter.ts
@@ -1,5 +1,12 @@
 import { html } from "lit-html";
 
 export const redirectCounter = (count: number) =>
-  html`
-  <p>Redirecting in ${count}</p> `;
+  html` <p>Redirecting in ${count}</p> `;
+
+export const redirectButton = html`<button
+    class="NextButton"
+    id="redirect-button"
+  >
+    Click here to redirect.
+  </button>
+  <hr />`;
