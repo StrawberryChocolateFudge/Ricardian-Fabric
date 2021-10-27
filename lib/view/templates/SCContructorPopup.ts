8842b54e2d8905a8586f490b9b716cc610ef74d4
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-27 15:43:29 +0100

    update , custom ui

--- lib/view/templates/SCContructorPopup.ts
+++ lib/view/templates/SCContructorPopup.ts
@@ -2,6 +2,7 @@ import { html } from "lit-html";
 import { DeploySC } from "../../types";
 import { findConstructorParameters } from "../../wallet";
 import { getHRC20Abi } from "../../wallet/abi/HRC20";
+import { helperTooltips } from "./helperTooltips";
 
 export function SCConstructorPopup(selected: DeploySC) {
   let constructorParams;
@@ -9,16 +10,77 @@ export function SCConstructorPopup(selected: DeploySC) {
     constructorParams = findConstructorParameters(getHRC20Abi());
   }
 
+  const getLabelFromParamName = (name: string) => {
+    switch (name) {
+      case "tokenName":
+        return "Token name:";
+        break;
+      case "tokenSymbol":
+        return "Token Symbol:";
+        break;
+      case "initialSupply":
+        return "Initial Supply:";
+        break;
+      case "_decimals":
+        return "Decimals:";
+        break;
+      default:
+        break;
+    }
+  };
+
+  const getTypeFromName = (name: string) => {
+    switch (name) {
+      case "tokenName":
+        return "text";
+        break;
+      case "tokenSymbol":
+        return "text";
+        break;
+      case "initialSupply":
+        return "number";
+        break;
+      case "_decimals":
+        return "number";
+        break;
+      default:
+        break;
+    }
+  };
+
+  const getTooltipFromName = (name: string) => {
+    switch (name) {
+      case "tokenName":
+        return "The name of the token. ";
+        break;
+      case "tokenSymbol":
+        return "The symbol of the token.";
+        break;
+      case "initialSupply":
+        return "The amount of tokens minted to the creator's address on contract creation.";
+        break;
+      case "_decimals":
+        return "Token decimals. Recommended is 18.";
+        break;
+      default:
+        break;
+    }
+  };
+
   const list = constructorParams.map((params) => {
     return html`<tr>
       <td>
         <label for="${params.name}-input"
-          >${params.name} (${params.type})</label
+          >${getLabelFromParamName(params.name)}</label
         >
       </td>
       <td>
-        <input id="${params.name}-input" type="$1" />
+        <input
+          id="${params.name}-input"
+          type="${getTypeFromName(params.name)}"
+        />
       </td>
+      <td>${helperTooltips(getTooltipFromName(params.name))}</td>
     </tr>`;
   });
 
@@ -30,16 +92,15 @@ export function SCConstructorPopup(selected: DeploySC) {
         <tr>
           <th></th>
           <th></th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
         ${list}
       </tbody>
     </table>
-    <small>Make sure you enter the correct details.</small>
     <small
-      >Variables with uint256 type will be converted to Big Number
-      automaticly.</small
+      >Make sure you enter the correct details.This is non-reversible.</small
     >
     <hr />
     <table>
@@ -51,12 +112,25 @@ export function SCConstructorPopup(selected: DeploySC) {
       </thead>
       <tbody>
         <tr>
-          <td><label for="logo-url-input">Logo url: (not required)</label></td>
-          <td><input id="logo-url-input" /></td>
+          <td><label for="logo-url-input">Logo url:</label></td>
+          <td><input id="logo-url-input" type="text" /></td>
+          <td>
+            ${helperTooltips(
+              "Not required. The url of the logo will be used when it adds the tokens to the wallet."
+            )}
+          </td>
         </tr>
       </tbody>
     </table>
-
+    <hr />
+    <div class="row">
+      <label for="agree-to-deploy-sc">I agree to the terms.</label>
+      <input
+        id="agree-to-deploy-sc"
+        aria-label="Agree to deploy checkbox"
+        type="checkbox"
+      />
+    </div>
     <hr />
     <div>
       <button id="SCConstructBackButton" class="SCBackButton">Cancel</button>
@@ -64,5 +138,6 @@ export function SCConstructorPopup(selected: DeploySC) {
         Create contract!
       </button>
     </div>
+    <hr />
   `;
 }
