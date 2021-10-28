8ae9ee1c6b1fe3b9514f022f540007adb2998139
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-28 19:20:00 +0100

    add permaweb dropdown

---
+++ lib/view/templates/permawebDropdown.ts
@@ -0,0 +1,23 @@
+import { html } from "lit-html";
+
+export const PermawebDropdown = () => html`
+  <input type="checkbox" id="permaweb_checkbox_toggle" class="dropdown_checkbox_toggle" />
+  <label
+    class="labelButton dropdown_checkbox_label"
+    id="permaweb_checkbox_label"
+    for="permaweb_checkbox_toggle"
+    >Permaweb</label
+  >
+  <ul>
+    <li>
+      <button id="upload-popup-button" class="dropdown-button">
+        Upload File
+      </button>
+    </li>
+    <li>
+      <button id="permapin-popup-button" class="dropdown-button">
+        Permapin
+      </button>
+    </li>
+  </ul>
+`;
