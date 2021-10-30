398f13bf8f8a0b4f6db897f1b112d22bb337e599
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-30 11:04:15 +0100

    add history button

--- lib/view/templates/permawebDropdown.ts
+++ lib/view/templates/permawebDropdown.ts
@@ -1,9 +1,13 @@
 import { html } from "lit-html";
 
 export const PermawebDropdown = () => html`
-  <input type="checkbox" id="permaweb_checkbox_toggle" class="dropdown_checkbox_toggle" />
+  <input
+    type="checkbox"
+    id="permaweb_checkbox_toggle"
+    class="dropdown_checkbox_toggle"
+  />
   <label
-    class="labelButton dropdown_checkbox_label"
+    class="labelButton dropdown_checkbox_label lightCoral-shadow"
     id="permaweb_checkbox_label"
     for="permaweb_checkbox_toggle"
     >Permaweb</label
@@ -18,6 +22,9 @@ export const PermawebDropdown = () => html`
       <button id="permapin-popup-button" class="dropdown-button">
         Permapin
       </button>
+    </li>
+    <li>
+      <button id="history-popup-button" class="dropdown-button">History</button>
     </li>
   </ul>
 `;
