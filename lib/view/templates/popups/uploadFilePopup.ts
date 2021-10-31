d43af117d2da2398246f3d99d24746171f0aad0e
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-31 12:42:59 +0000

    add terms to popup

--- lib/view/templates/popups/uploadFilePopup.ts
+++ lib/view/templates/popups/uploadFilePopup.ts
@@ -3,6 +3,11 @@ import { html } from "lit-html";
 export const uploadFilePopup = () => html`
   <h2 class="center">Upload a File</h2>
   <hr />
+  <small
+    >Upload a file to the permaweb. Pay only once, and it's stored forever and
+    served as a website.</small
+  >
+  <hr />
   <div id="file-dropzone" class="drop-zone">
     <span id="drop-prompt" class="drop-zone__prompt"
       >Drop File here or click to upload</span
@@ -10,15 +15,23 @@ export const uploadFilePopup = () => html`
     <input type="file" name="file" id="file-input" class="drop-zone__input" />
   </div>
   <hr />
-  <div
-    aria-label="error-display-slot"
-    class="center red"
-    id="error-display"
-  ></div>
+  <div class="center marginBottom-10">
+    <label
+      aria-labelledby="I agree to the terms"
+      class="terms-button-label"
+      id="terms-button"
+      >I agree to the terms</label
+    >
+    <input
+      aria-label="I agree to the terms checkbox"
+      id="upload-terms-checkbox"
+      type="checkbox"
+    />
+  </div>
   <div id="upload-loading-indicator"></div>
-  <div class="button-row">
+  <div class="wide-row">
     <button class="marginRight-20 backButton" id="upload-cancel">Cancel</button>
-    <button class="marginLeft-20 SCNextButton" id="upload-proceed">Next</button>
+    <button class="marginLeft-20 NextButton" id="upload-proceed">Next</button>
   </div>
   <hr />
 `;
@@ -53,14 +66,18 @@ export const uploadFileSummary = (
       </tr>
     </tbody>
   </table>
-  <label for="uploadSummary-tx">Url after upload:</label>
+  <hr />
+  <label for="uploadSummary-tx"
+    >Content will be available at this url after posting it to the
+    network:</label
+  >
   <small id="uploadSummary-tx">https://arweave.net/${id}</small>
   <hr />
   <div class="button-row">
     <button class="marginRight-20 backButton" id="uploadSummary-cancel">
       Cancel
     </button>
-    <button class="marginLeft-20 SCNextButton" id="uploadSummary-proceed">
+    <button class="marginLeft-20 NextButton" id="uploadSummary-proceed">
       Post
     </button>
   </div>
