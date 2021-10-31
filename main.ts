a9f7e450ba990faa12a6643fd86a14842d6ca8a0
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-31 12:43:55 +0000

    update, show banner runs elsewhere

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 import "./lib/state";
 import "./lib/view";
-import { showBanner } from "./lib/business/bloc";
 import "./lib/view/templates/acceptablePage";
 import { dispatch_setInit } from "./lib/dispatch/stateChange";
 (async function init() {
-  showBanner();
   dispatch_setInit();
 })();
