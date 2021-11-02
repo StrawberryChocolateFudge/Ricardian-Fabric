d52b52771421d1e3e6e04a9ce1de5f03de238d90
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-02 17:31:44 +0000

    add identity in local storage

--- lib/state/dataPropGetters.ts
+++ lib/state/dataPropGetters.ts
@@ -2,6 +2,7 @@ import {
   BlockCountry,
   ContractTypes,
   ERC20Params,
+  Identity,
   SelectedWallet,
 } from "../types";
 
@@ -81,4 +82,19 @@ export function getSelectedWalletFromDataProp(
   page: HTMLElement
 ): SelectedWallet {
   return page.dataset.selectedwallet as SelectedWallet;
+}
+
+export function getIdentityFromLocalStorage(): Identity {
+  const serializedId = window.localStorage.getItem("identity");
+  const data = new Blob([serializedId], { type: "application/download" });
+  const address = window.localStorage.getItem("address");
+
+  return { data, address };
+}
+
+export async function setIdentityToLocalStorage(identity: Identity) {
+  const serializedId = await identity.data.text();
+
+  window.localStorage.setItem("identity", serializedId);
+  window.localStorage.setItem("address", identity.address);
 }
