e55fc61036743112e71a3532a5e5d8d481e3a239
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-02 17:32:04 +0000

    removed permapin from serverside

--- lib/fetch/index.ts
+++ lib/fetch/index.ts
@@ -1,31 +1,6 @@
-import { Options, PinOptions, Status } from "../types";
+import { Options, Status } from "../types";
 
-export async function permapin(CID: string, url: string): Promise<PinOptions> {
-  try {
-    const response = await fetch(url + "/" + CID, {
-      method: "post",
-      mode: "cors",
-      cache: "no-cache",
-    });
 
-    if (response.status === 200) {
-      return { status: Status.Success, error: "", result: response };
-    } else {
-      return {
-        status: Status.Failure,
-        error:
-          "Couldn't permapin the link, visit the bridge to pin it manually.",
-        result: response,
-      };
-    }
-  } catch (error: any) {
-    return {
-      status: Status.Failure,
-      error: `Couldn't permapin the link, visit the bridge to pin it manually.`,
-      result: undefined,
-    };
-  }
-}
 
 export async function fetchGeoCodingCSV(): Promise<Options> {
   const result: Options = { status: Status.Success, error: "", data: {} };
