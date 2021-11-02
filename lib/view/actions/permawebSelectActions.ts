6591a6fe4c4b62d4f46288a71d23128283d1c3a4
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-02 17:34:09 +0000

    added actions WIP

--- lib/view/actions/permawebSelectActions.ts
+++ lib/view/actions/permawebSelectActions.ts
@@ -1,4 +1,6 @@
 import {
+  dispatch_addNewIdentityPopup,
+  dispatch_emptyWalletDropper,
   dispatch_hideElement,
   dispatch_hidePopup,
   dispatch_permapinPopup,
@@ -11,21 +13,43 @@ import {
   dispatch_renderTerms,
   dispatch_renderUploadFilePopup,
   dispatch_renderUploadSummary,
+  dispatch_showIdentityPopup,
+  dispatch_switch_identities,
+  dispatch_walletPopup,
 } from "../../dispatch/render";
-import { State, Status } from "../../types";
-import { createFileTransaction, uploadFile } from "../../wallet/arweave";
-import { getById, readFile } from "../utils";
+import { State, Status, WalletDropperType } from "../../types";
+import {
+  createFileTransaction,
+  createWallet,
+  getWalletAddress,
+  getWalletBalance,
+  uploadFile,
+} from "../../wallet/arweave";
+import { getById, readFile, readWalletFile } from "../utils";
 import { addHash } from "../../wallet/permapin/ipfsArweave";
+import { encryptWallet } from "../../crypto";
+import { downloadBlob } from "../render";
+import { dispatch_setNewIdentity } from "../../dispatch/stateChange";
 
 export function permawebSelectActions(props: State) {
   const uploadFile = getById("upload-popup-button");
   const permapin = getById("permapin-popup-button");
+  const identity = getById("identity-popup-button");
   uploadFile.onclick = function () {
     dispatch_renderUploadFilePopup(props);
   };
 
   permapin.onclick = function () {
     dispatch_permapinPopup(props, "");
+  };
+
+  identity.onclick = async function () {
+    if (props.identity.address === null) {
+      dispatch_showIdentityPopup(props, "");
+    } else {
+      const balance = await getWalletBalance(props.identity.address);
+      dispatch_showIdentityPopup(props, balance);
+    }
   };
 }
 
@@ -52,12 +76,6 @@ export function uploadFileListener(props: State) {
   };
 
   uploadButton.onclick = async function () {
-    if (window.arweaveWallet === undefined) {
-      dispatch_renderError("You need to install Arconnect!");
-      window.open("https://arconnect.io");
-      return;
-    }
-
     if (fileInput.files.length !== 1) {
       dispatch_renderError("You need to select a single file to upload!");
       return;
@@ -215,5 +233,178 @@ export function permawebTransactionAction(props: State, hash: string) {
   const permapinButton = getById("permapin-deployed-button");
   permapinButton.onclick = function () {
     dispatch_permapinPopup(props, hash);
+  };
+}
+
+export function walletCreateActions(props: State) {
+  const backbutton = getById("wallet-cancel");
+  const proceed = getById("wallet-proceed");
+  const password = getById("wallet-password-once") as HTMLInputElement;
+  const passwordAgain = getById("wallet-password-twice") as HTMLInputElement;
+  const fileInput = getById("wallet-input") as HTMLInputElement;
+  // If proceed was hidden, I need to show it again.
+  dispatch_hideElement(proceed, false);
+
+  onWalletFileDropped(props, WalletDropperType.PLAINTEXT);
+
+  //On init I empty the password fields, even tho the browser will substitute something in.ZP
+  password.value = "";
+  passwordAgain.value = "";
+
+  backbutton.onclick = async function () {
+    const balance = await getWalletBalance(props.identity.address);
+    dispatch_showIdentityPopup(props, balance);
+
+    dispatch_emptyWalletDropper(props);
+  };
+
+  proceed.onclick = async function () {
+    const pass1 = password.value;
+    const pass2 = passwordAgain.value;
+    if (pass1 !== pass2) {
+      dispatch_renderError("The passwords don't match");
+      return;
+    }
+    if (pass1.length < 8) {
+      dispatch_renderError("The passwords must be at least 8 characters long.");
+      return;
+    }
+
+    const keyFound = async (key: any) => {
+      dispatch_hideElement(proceed, true);
+      const address = await getWalletAddress(key);
+      const encryptedBlob = await encryptWallet(key, pass1);
+      downloadBlob(encryptedBlob, address + ".enc");
+      // after the blob is downloaded I redirect to Identity, and set the blob
+      dispatch_addNewIdentityPopup(props, encryptedBlob, address);
+    };
+
+    if (fileInput.files.length > 0) {
+      if (fileInput.files.length !== 1) {
+        dispatch_renderError("You can import only one file");
+        return;
+      }
+
+      if (fileInput.files[0].type !== "application/json") {
+        dispatch_renderError(
+          "File type is invalid. Must be unencrypted arweave wallet file."
+        );
+        return;
+      }
+
+      readWalletFile(fileInput.files, async (key) => {
+        if (key !== undefined && key.kty === "RSA") {
+          keyFound(key);
+        } else {
+          dispatch_renderError(
+            "File type is invalid. Must be unencrypted arweave wallet file."
+          );
+        }
+      });
+    } else {
+      const key = await createWallet();
+      keyFound(key);
+    }
+  };
+}
+
+export function onWalletFileDropped(props: State, type: WalletDropperType) {
+  const walletInput = getById("wallet-input") as HTMLInputElement;
+  const dropZone = getById("wallet-dropzone");
+  const requiredType =
+    type === WalletDropperType.PLAINTEXT
+      ? "application/json"
+      : "application/download";
+
+  dropZone.onclick = function () {
+    walletInput.click();
+  };
+
+  walletInput.onchange = function () {
+    const file = walletInput.files[0];
+
+    if (walletInput.files.length === 1 && file.type === requiredType) {
+      // It's valid
+      dispatch_promptSuccess(walletInput.files[0]);
+      dispatch_removeError();
+    } else {
+      dispatch_promptError("Invalid File");
+    }
+  };
+
+  dropZone.ondragover = function (e: Event) {
+    e.preventDefault();
+    dropZone.classList.add("drop-zone--over");
+  };
+  dropZone.ondragleave = function (e: Event) {
+    dropZone.classList.remove("drop-zone--over");
+  };
+  dropZone.ondragend = function (e: Event) {
+    dropZone.classList.remove("drop-zone--over");
+  };
+
+  dropZone.ondrop = function (e: DragEvent) {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    console.log(file.type);
+    if (e.dataTransfer.files.length === 1 && file.type === requiredType) {
+      walletInput.files = e.dataTransfer.files;
+      dispatch_promptSuccess(e.dataTransfer.files[0]);
+      dispatch_removeError();
+    } else {
+      dispatch_promptError("Invalid File");
+    }
+    dropZone.classList.remove("drop-zone--over");
+  };
+}
+
+export function AddNewIdentityActions(
+  props: State,
+  identity: Blob,
+  address: string
+) {
+  const cancelButton = getById("addNewIdentity-cancel");
+  const proceedButton = getById("addNewIdentity-proceed");
+
+  cancelButton.onclick = function () {
+    dispatch_hidePopup();
+  };
+
+  proceedButton.onclick = function () {
+    dispatch_setNewIdentity({ data: identity, address });
+    dispatch_hidePopup();
+  };
+}
+
+export function showIdentityActions(props: State) {
+  const cancelButton = getById("identity-cancel");
+  const proceedButton = getById("switchIdentity-proceed");
+  const newIdentityButton = getById("new-identity");
+  // onWalletFileDropped(props, WalletDropperType.ENCRYPTED);
+  cancelButton.onclick = function () {
+    dispatch_hidePopup();
+  };
+
+  proceedButton.onclick = function () {
+    dispatch_switch_identities(props);
+  };
+
+  newIdentityButton.onclick = function () {
+    dispatch_walletPopup(props);
+  };
+}
+
+export function switchIdentitiesActions(props: State) {
+  const cancelButton = getById("identity-cancel");
+  const proceedButton = getById("switchIdentity-proceed");
+
+  // onWalletFileDropped(props, WalletDropperType.ENCRYPTED);
+  cancelButton.onclick = async function () {
+    const balance = await getWalletBalance(props.identity.address);
+    dispatch_showIdentityPopup(props, balance);
+  };
+
+  proceedButton.onclick = function () {
+    // dispatch_switch_identities(props);
   };
 }
