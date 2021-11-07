fe215198fc4afe1f9b4fcaec2ec5b1ca7f8d0846
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-07 15:55:45 +0000

    add popupstate

--- lib/view/actions/templateSelectActions.ts
+++ lib/view/actions/templateSelectActions.ts
@@ -1,12 +1,12 @@
 import { dispatch_renderDocXDropper } from "../../dispatch/render";
-import { State } from "../../types";
+import { dispatch_setPopupState } from "../../dispatch/stateChange";
+import { PopupState, State } from "../../types";
 import { getById } from "../utils";
 
 export function templateSelectActions(props: State) {
   const importDocX = getById("import-docx-trigger");
   importDocX.onclick = function () {
     //Dispatch a popup and render the docx stuff into it
-    dispatch_renderDocXDropper(props);
+    dispatch_setPopupState(PopupState.ImportTemplate);
   };
 }
-
