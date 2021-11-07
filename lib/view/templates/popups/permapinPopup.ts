21cd28daffc00615334e9f8d5e5baf32e9fc1555
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-07 15:56:59 +0000

    add summary, removed tips for now

--- lib/view/templates/popups/permapinPopup.ts
+++ lib/view/templates/popups/permapinPopup.ts
@@ -1,4 +1,6 @@
 import { html } from "lit-html";
+import { TIP, WinstonToAr } from "../../../wallet/arweave";
+import { helperTooltips } from "../components/helperTooltips";
 
 export const PermapinPopup = () => html`
   <h2>Permapin a Contract</h2>
@@ -19,6 +21,7 @@ export const PermapinPopup = () => html`
         <td>
           <input id="CID-input-permapin" type="text" />
         </td>
+        <td>${helperTooltips("The IPFS content identifier, V0 or V1")}</td>
       </tr>
       <tr>
         <td><label for="walletPassword">Password:</label></td>
@@ -31,7 +34,15 @@ export const PermapinPopup = () => html`
             type="password"
           />
         </td>
+        <td>${helperTooltips("The password of the key file")}</td>
       </tr>
+      <!-- <tr>
+        <td><label for="tipcheckbox">Send a tip:</label></td>
+        <td>
+          <input id="tipcheckbox" type="checkbox" checked />
+        </td>
+        <td>${helperTooltips(`Support us by sending a tip. ${TIP} Ar`)}</td>
+      </tr> -->
       <tr>
         <td>
           <label id="terms-button" class="terms-button-label"
@@ -49,3 +60,44 @@ export const PermapinPopup = () => html`
   </div>
   <hr />
 `;
+
+export const PermapinSummaryPage = (arg: {
+  permapinTx: any;
+  sendTip: boolean;
+  tipTx: any;
+}) => {
+
+  const getFee = () => {
+    let reward = parseFloat(arg.permapinTx.tx.reward);
+    if (arg.sendTip) {
+      reward += parseFloat(arg.tipTx.reward);
+      reward += parseFloat(arg.tipTx.quantity);
+    }
+    const rewardInAr = WinstonToAr(reward.toString());
+    return html`<div>${rewardInAr} Ar</div>`;
+  };
+
+  return html` <h2>Permapin Summary</h2>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Fee:</td>
+          <td>${getFee()}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div id="transaction-loading"></div>
+    <div class="wide-row">
+      <button class="backButton" id="permapinPost-back">Back</button>
+      <button class="NextButton" id="permapinPost-proceed">
+        Post Transaction.
+      </button>
+    </div>
+    <hr />`;
+};
