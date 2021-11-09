bdbbcc18b8ba553efddfc3a9fbc228bc4248b03e
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-09 13:52:20 +0000

    add fetching for acceptable contract

--- lib/fetch/index.ts
+++ lib/fetch/index.ts
@@ -1,6 +1,4 @@
-import { Options, Status } from "../types";
-
-
+import { Options, StateProperties, Status } from "../types";
 
 export async function fetchGeoCodingCSV(): Promise<Options> {
   const result: Options = { status: Status.Success, error: "", data: {} };
@@ -9,6 +7,19 @@ export async function fetchGeoCodingCSV(): Promise<Options> {
       "https://arweave.net/Wl0lmZU2A1D60EqMePwX77PpFpTEIMUdKGSBM-uGlto",
       { method: "get" }
     );
+    result.data = await response.text();
+  } catch (error: any) {
+    result.status = Status.Failure;
+    result.error = error.message;
+  }
+  return result;
+}
+
+export async function fetchAcceptableContract(url: string): Promise<Options> {
+  const result: Options = { status: Status.Success, error: "", data: {} };
+
+  try {
+    const response = await fetch(url, { method: "get" });
     result.data = await response.text();
   } catch (error: any) {
     result.status = Status.Failure;
