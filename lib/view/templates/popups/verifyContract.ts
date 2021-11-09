214b9daceb2f180cf8cad5ae561f011083be0ba2
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-09 14:01:19 +0000

    added contract verification

---
+++ lib/view/templates/popups/verifyContract.ts
@@ -0,0 +1,45 @@
+import { html } from "lit-html";
+import { helperTooltips } from "../components/helperTooltips";
+
+export const VerifyContract = () => {
+  return html`<h2>Verify a contract</h2>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <label for="acceptable-contract-url">Acceptable Contract Url</label>
+          </td>
+          <td>
+            <input id="acceptable-contract-url" type="text" />
+          </td>
+          <td>
+            ${helperTooltips(
+              "You should verify a contract before you accept it. Paste here the url."
+            )}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <hr />
+    <div id="verify-loading-indicator"></div>
+    <div id="verify-result-display">
+      <p>Click on verify to proceed with the verification.</p>
+    </div>
+    <div class="wide-row">
+      <button class="backButton" id="verify-back">back</button>
+      <button class="NextButton" id="verify-proceed">Verify!</button>
+    </div>
+    <hr />`;
+};
+
+export const VerifySuccess = () =>
+  html`<p style="color: green">Verification Succeeded! Contract is valid.</p>`;
+export const VerifyFailure = () =>
+  html`<p style="color: red">Verification Failed!</p>`;
