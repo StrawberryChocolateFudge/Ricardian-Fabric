b6d6f4588b5a8c6fe9cbbc69aed9499045abbca6
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-18 01:20:33 +0000

    new ipfs CAT added

--- lib/ipfs/add.ts
+++ lib/ipfs/add.ts
@@ -1,5 +1,8 @@
 import { CID, create } from "ipfs-http-client";
 import { IPFSParams } from "../types";
+import { concat as uint8ArrayConcat } from 'uint8arrays/concat'
+import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
+import all from 'it-all'
 
 export async function IPFS_Add(
   content: string,
@@ -8,4 +11,10 @@ export async function IPFS_Add(
   const client = create(params);
   const { cid } = await client.add(content);
   return cid;
+}
+
+export async function IPFS_CAT(cid: string, params: IPFSParams): Promise<string> {
+  const client = create(params);
+  const data = uint8ArrayConcat(await all(client.cat(cid)))
+  return uint8ArrayToString(data);
 }
