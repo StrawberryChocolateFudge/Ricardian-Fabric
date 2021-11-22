ece71e07a13042c7f269eb83127c741ea0de0d38
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-22 23:44:40 +0000

    add generics to options

--- lib/fetch/index.ts
+++ lib/fetch/index.ts
@@ -1,7 +1,7 @@
 import { Options, StateProperties, Status } from "../types";
 
-export async function fetchGeoCodingCSV(): Promise<Options> {
-  const result: Options = { status: Status.Success, error: "", data: {} };
+export async function fetchGeoCodingCSV(): Promise<Options<string>> {
+  const result: Options<string> = { status: Status.Success, error: "", data: "" };
   try {
     const response = await fetch(
       "https://arweave.net/Wl0lmZU2A1D60EqMePwX77PpFpTEIMUdKGSBM-uGlto",
@@ -15,8 +15,8 @@ export async function fetchGeoCodingCSV(): Promise<Options> {
   return result;
 }
 
-export async function fetchAcceptableContract(url: string): Promise<Options> {
-  const result: Options = { status: Status.Success, error: "", data: {} };
+export async function fetchAcceptableContract(url: string): Promise<Options<string>> {
+  const result: Options<string> = { status: Status.Success, error: "", data: "" }
 
   try {
     const response = await fetch(url, { method: "get" });
