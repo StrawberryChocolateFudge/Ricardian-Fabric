1202de88bdd7d5c779b5c5e91e6b7e6654d8b89a
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-23 17:15:42 +0000

    added logos

--- lib/view/templates/dropdowns/permawebDropdown.ts
+++ lib/view/templates/dropdowns/permawebDropdown.ts
@@ -1,24 +1,26 @@
 import { html } from "lit-html";
+import { accountLogo, arweaveLogo, pinLogo, uploadLogo } from "../components/logos";
 
 export const PermawebDropdown = () => html`
   <input type="checkbox" id="permaweb_checkbox_toggle" class="dropdown_checkbox_toggle" />
   <label class="labelButton dropdown_checkbox_label lightCoral-shadow" id="permaweb_checkbox_label"
-    for="permaweb_checkbox_toggle">Permaweb</label>
+    for="permaweb_checkbox_toggle">${arweaveLogo()} Permaweb</label>
   <ul>
     <li>
       <button id="Account-popup-button" class="dropdown-button">
-        <small>Account</small>
+        <small>${accountLogo()} Account</small>
       </button>
     </li>
     <li>
       <button id="upload-popup-button" class="dropdown-button">
-        <small>Upload File (Coming soon)</small>
+        <small>${uploadLogo()} Upload File (Coming soon)</small>
       </button>
     </li>
     <li>
       <button id="permapin-popup-button" class="dropdown-button">
-        <small>Permapin</small>
+        <small>${pinLogo()} Permapin</small>
       </button>
     </li>
   </ul>
 `;
+
--- lib/view/templates/dropdowns/sanctionsDropdown.ts
+++ lib/view/templates/dropdowns/sanctionsDropdown.ts
@@ -1,51 +1,35 @@
 import { html } from "lit-html";
+import { blockLogo } from "../components/logos";
 
 export const SanctionsDropdown = (expand: boolean) => html`
   <input type="checkbox" id="sanctions_checkbox_toggle" />
-  <label
-    id="sanctions_checkbox_label"
-    class="labelButton sanctions_checkbox-label"
-    for="sanctions_checkbox_toggle"
-    >Select Sanctions</label
-  >
+  <label id="sanctions_checkbox_label" class="labelButton sanctions_checkbox-label" for="sanctions_checkbox_toggle">
+    ${blockLogo()} Select
+    Sanctions</label>
   <ul>
     <li>
-      <input
-        aria-label="ofec checkbox"
-        id="ofec_checkbox"
-        type="checkbox"
-        checked
-      /><span class="sanctions-span">OFEC sanctioned countries</span>
+      <input aria-label="ofec checkbox" id="ofec_checkbox" type="checkbox" checked /><span class="sanctions-span">OFEC
+        sanctioned countries</span>
     </li>
     <li>
-      <input
-        aria-label="eu sanctions checkbox"
-        id="eu-checkbox"
-        type="checkbox"
-        checked
-      /><span class="sanctions-span">EU sanctioned countries</span>
+      <input aria-label="eu sanctions checkbox" id="eu-checkbox" type="checkbox" checked /><span class="sanctions-span">EU
+        sanctioned countries</span>
     </li>
     <li>
-      <input
-        aria-label="UN sanctions checkbox"
-        id="un-checkbox"
-        type="checkbox"
-        checked
-      /><span class="sanctions-span">UN sanctioned countries</span>
+      <input aria-label="UN sanctions checkbox" id="un-checkbox" type="checkbox" checked /><span class="sanctions-span">UN
+        sanctioned countries</span>
     </li>
     <li>
-      <input
-        aria-label="Block USA checkbox"
-        id="usa-checkbox"
-        type="checkbox"
-      /><span class="sanctions-span">Block USA</span>
+      <input aria-label="Block USA checkbox" id="usa-checkbox" type="checkbox" /><span class="sanctions-span">Block
+        USA</span>
     </li>
     <!-- <li>
-      <input
-        aria-label="block new york checkbox"
-        id="newyork-checkbox"
-        type="checkbox"
-      /><span class="sanctions-span">Block New York</span>
-    </li> -->
+                <input
+                  aria-label="block new york checkbox"
+                  id="newyork-checkbox"
+                  type="checkbox"
+                /><span class="sanctions-span">Block New York</span>
+              </li> -->
   </ul>
 `;
+
