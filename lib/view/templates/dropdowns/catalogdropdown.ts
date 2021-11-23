e9affec5280ae29fb102f017d71d60eece55c02e
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-23 17:15:21 +0000

    added logos to catalog

--- lib/view/templates/dropdowns/catalogdropdown.ts
+++ lib/view/templates/dropdowns/catalogdropdown.ts
@@ -1,31 +1,26 @@
 import { html } from "lit-html";
+import { catalogLogo, HarmonyLogo, SmartContractLogo } from "../components/logos";
 
 export const CatalogDropdown = () => html`
-  <input
-    type="checkbox"
-    id="catalog_checkbox_toggle"
-    class="dropdown_checkbox_toggle"
-  />
-  <label
-    class="labelButton dropdown_checkbox_label lightGreenish-shadow"
-    for="catalog_checkbox_toggle"
-    >Catalog</label
-  >
+  <input type="checkbox" id="catalog_checkbox_toggle" class="dropdown_checkbox_toggle" />
+  <label class="labelButton dropdown_checkbox_label lightGreenish-shadow"
+    for="catalog_checkbox_toggle">${catalogLogo()}Catalog</label>
   <ul>
     <li>
       <button id="smart-contracts-button" class="dropdown-button">
-        <small>Smart Contracts</small>
+        ${SmartContractLogo()}<small>Smart Contracts</small>
       </button>
     </li>
     <li>
       <button id="view-proposals-button" class="dropdown-button">
-        <small>Proposals(COMING SOON)</small>
+        ${HarmonyLogo()}<small>Proposals(COMING SOON)</small>
       </button>
     </li>
     <li>
       <button id="create-proposal-button" class="dropdown-button">
-        <small>Create proposal(COMING SOON)</small>
+        ${HarmonyLogo()}<small>Create proposal(COMING SOON)</small>
       </button>
     </li>
   </ul>
 `;
+
