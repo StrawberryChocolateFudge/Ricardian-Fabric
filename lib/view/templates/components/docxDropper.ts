474c730e0572559b9e737d5fceec21f7fb4f666c
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-23 17:14:22 +0000

    add backLogo

--- lib/view/templates/components/docxDropper.ts
+++ lib/view/templates/components/docxDropper.ts
@@ -1,15 +1,15 @@
 import { html } from "lit-html";
+import { BackLogo } from "./logos";
 
 export const DocXDropper = () => html`
   <div id="docx-dropzone" class="drop-zone">
-    <span id="drop-prompt-docx" class="drop-zone__prompt"
-      >To load template, drop a .docx file here or click to select it</span
-    >
+    <span id="drop-prompt-docx" class="drop-zone__prompt">To load template, drop a .docx file here or click to select
+      it</span>
     <input type="file" name="docx" id="docx-input" class="drop-zone__input" />
   </div>
   <hr />
   <small>Loading a template will completely override the text area.</small>
   <hr />
-  <div><button id="dropper-back-button" class="backButton">Back</button></div>
+  <div><button id="dropper-back-button" class="backButton">${BackLogo()} Back</button></div>
   <hr />
 `;
