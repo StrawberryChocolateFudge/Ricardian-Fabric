c78b7352a55cb6559b70ba25c5618e94ff1640f7
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-23 17:11:12 +0000

    update, waits for the animation

--- main.ts
+++ main.ts
@@ -3,5 +3,9 @@ import "./lib/view";
 import "./lib/view/templates/acceptablePage";
 import { dispatch_setInit } from "./lib/dispatch/stateChange";
 (async function init() {
-  dispatch_setInit();
+
+
+  //Wait for the animation, then dispatch set init
+  setTimeout(function () { dispatch_setInit() }, 3000);
+
 })();
