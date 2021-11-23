26a033dbf45f71099b5c15ce435c489c988c86ce
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-23 17:14:11 +0000

    reformat

--- lib/view/templates/components/createButton.ts
+++ lib/view/templates/components/createButton.ts
@@ -3,24 +3,13 @@ import { html } from "lit-html";
 export const createButton = (disabled: boolean) => {
   if (disabled) {
     return html`
-      <button
-        class="center width-200"
-        id="save-contract"
-        role="button"
-        aria-label="Create"
-        disabled
-      >
+      <button class="center width-200" id="save-contract" role="button" aria-label="Create" disabled>
         Create
       </button>
     `;
   } else {
-    return html`<button
-      class="center width-200"
-      id="save-contract"
-      role="button"
-      aria-label="Create"
-    >
-      Create
-    </button>`;
+    return html`<button class="center width-200" id="save-contract" role="button" aria-label="Create">
+  Create
+</button>`;
   }
 };
