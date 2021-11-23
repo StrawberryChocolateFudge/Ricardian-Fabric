dd4ea3a1e521d8babc6308eb61bb56a7a91fb434
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-23 17:13:55 +0000

    added logos for acceptable page

--- lib/view/templates/components/components.ts
+++ lib/view/templates/components/components.ts
@@ -1,5 +1,6 @@
 import { html } from "lit-html";
 import { Chains } from "../../../types";
+import { BSCLogo, EthLogo, HarmonyLogo, PolygonLogo } from "./logos";
 
 export const getPriceTemplate = (
   price: string | number,
@@ -7,48 +8,46 @@ export const getPriceTemplate = (
 ) => {
   if (price !== "NONE") {
     const formattedPrice = price === "NONE" ? "" : `${price} Ar`;
-    return html` <tr>
-        <td>
-          <label aria-labelledby="price label">Price:</label>
-        </td>
-        <td>
-          <div aria-label="price">${formattedPrice}</div>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <label aria-labelledby="fee label">Fee:</label>
-        </td>
-        <td aria-label="fee">${fee} Ar</td>
-      </tr>`;
+    return html`<tr>
+  <td>
+    <label aria-labelledby="price label">Price:</label>
+  </td>
+  <td>
+    <div aria-label="price">${formattedPrice}</div>
+  </td>
+</tr>
+<tr>
+  <td>
+    <label aria-labelledby="fee label">Fee:</label>
+  </td>
+  <td aria-label="fee">${fee} Ar</td>
+</tr>`;
   }
 };
 
 export const createdDateTemplate = (date: string) => {
-  return html` <tr>
-    <td aria-labelledby="created date label">
-      <label for="createdDate"><label for="createdDate">Created:</label></label>
-    </td>
-    <td id="createdDate" aria-label="created date">${date}</td>
-  </tr>`;
+  return html`<tr>
+  <td aria-labelledby="created date label">
+    <label for="createdDate"><label for="createdDate">Created:</label></label>
+  </td>
+  <td id="createdDate" aria-label="created date">${date}</td>
+</tr>`;
 };
 
 export const signedDateTemplate = (date: string) => {
-  return html` <tr>
-    <td aria-labelledby="signed date label">
-      <label for="signed-date">Signed on:</label>
-    </td>
-    <td id="signed-date" aria-label="signed date">${date}</td>
-  </tr>`;
+  return html`<tr>
+  <td aria-labelledby="signed date label">
+    <label for="signed-date">Signed on:</label>
+  </td>
+  <td id="signed-date" aria-label="signed date">${date}</td>
+</tr>`;
 };
 
 export const issuerTemplate = (address: string) => {
   return html`
     <tr>
       <td aria-labelledby="issuer label">
-        <label for="issuer-address"
-          ><label for="issuer-address">Issuer:</label></label
-        >
+        <label for="issuer-address"><label for="issuer-address">Issuer:</label></label>
       </td>
       <td id="issuer-address" aria-label="address">${address}</td>
       <td></td>
@@ -58,15 +57,15 @@ export const issuerTemplate = (address: string) => {
 
 export const networkTemplate = (network: string, isFulfilled: boolean) => {
   const chains = {
-    [Chains.Ropsten]: getChainButton("Ropsten", "Testnet", "", isFulfilled),
-    [Chains.bscTestnet]: getChainButton("BSC", "Testnet", "", isFulfilled),
-    [Chains.polygonTestnet]: getChainButton(
+    [Chains.Ropsten]: getChainButton(Chains.Ropsten, "Ropsten", "Testnet", "", isFulfilled),
+    [Chains.bscTestnet]: getChainButton(Chains.bscTestnet, "BSC", "Testnet", "", isFulfilled),
+    [Chains.polygonTestnet]: getChainButton(Chains.polygonTestnet,
       "Polygon",
       "Testnet",
       "",
       isFulfilled
     ),
-    [Chains.harmonyTestnetShard0]: getChainButton(
+    [Chains.harmonyTestnetShard0]: getChainButton(Chains.harmonyTestnetShard0,
       "Harmony",
       "Testnet",
       "Shard 0",
@@ -77,50 +76,65 @@ export const networkTemplate = (network: string, isFulfilled: boolean) => {
   const networkEl = chains[network];
 
   return html`<tr>
-    <td aria-labelledby="Network Id label">
-      <label for="network-id">Network:</label>
-    </td>
-    <td id="network-id" aria-label="network id">
-      ${networkEl === undefined ? network : networkEl}
-    </td>
-    <td></td>
-  </tr>`;
+  <td aria-labelledby="Network Id label">
+    <label for="network-id">Network:</label>
+  </td>
+  <td id="network-id" aria-label="network id">
+    ${networkEl === undefined ? network : networkEl}
+  </td>
+  <td></td>
+</tr>`;
 };
 
 export function getChainButton(
-  chain: string,
+  chains: Chains,
+  chainName: string,
   net: string,
   shard: string,
   disabled: boolean
 ) {
   if (disabled) {
-    return html`${chain} ${net} ${shard}`;
+    return html`${getChainLogo(chains)} ${chainName} ${net} ${shard}`;
   } else {
-    return html`<button class="network-button" id="addChainButton">
-      ${chain} ${net} ${shard}
-    </button>`;
+    return html`<button class="network-button" id="addChainButton">${getChainLogo(chains)} ${chainName} ${net} ${shard}
+</button>`;
+  }
+}
+
+export function getChainLogo(chain: Chains) {
+  switch (chain) {
+    case Chains.Ropsten:
+      return EthLogo();
+    case Chains.harmonyTestnetShard0:
+      return HarmonyLogo();
+    case Chains.bscTestnet:
+      return BSCLogo();
+    case Chains.polygonTestnet:
+      return PolygonLogo();
+    default:
+      break;
   }
 }
 
 export const onlySignerTemplate = (onlySigner: string) => {
   if (onlySigner !== "NONE") {
     return html`<tr>
-      <td aria-labelledby="only signer label">Only Signer:</td>
-      <td aria-label="only signer address">${onlySigner}</td>
-      <td></td>
-    </tr>`;
+  <td aria-labelledby="only signer label">Only Signer:</td>
+  <td aria-label="only signer address">${onlySigner}</td>
+  <td></td>
+</tr>`;
   }
 };
 
 export const hashTemplate = (hash: string) => {
   return html`<tr>
-    <td aria-labelledby="Hash label">Hash</td>
-    <td aria-label="Hash">${hash}</td>
-    <td></td>
-  </tr>`;
+  <td aria-labelledby="Hash label">Hash</td>
+  <td aria-label="Hash">${hash}</td>
+  <td></td>
+</tr>`;
 };
 
-export const expiryTemplate = (date: string) => html` <tr>
+export const expiryTemplate = (date: string) => html`<tr>
   <td aria-labelledby="expires label">
     <label for="expiresDate">Expires:</label>
   </td>
