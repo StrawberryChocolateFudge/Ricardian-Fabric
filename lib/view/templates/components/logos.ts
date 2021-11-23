66a423b45c51771956fa158584c4ff186a820362
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-23 18:01:09 +0000

    added a new logo

--- lib/view/templates/components/logos.ts
+++ lib/view/templates/components/logos.ts
@@ -353,3 +353,9 @@ export const EditAgainLogo = () => html`
     <path
         d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
 </svg>`
+
+export const ContractConfigurationLogo = () => html`<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path
+        d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z" />
+</svg>`
