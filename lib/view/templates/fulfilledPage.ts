d3eb413da0ce1170302f7dc480b6023ddb056b41
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-24 18:04:35 +0000

    added signedon, the missing attribute

--- lib/view/templates/fulfilledPage.ts
+++ lib/view/templates/fulfilledPage.ts
@@ -110,12 +110,12 @@ export const fulfilledPageLayout = (props: FulfilledPageProps) => html`
   </body>
 `;
 export const fulfilledPage = (props: FulfilledPageProps) => html`
-  <div data-contracttype="fulfilled" data-version="${props.version}" data-created="${props.createdDate}"
-    data-expires="${props.expires}" data-redirectto="${props.redirectto}" data-parentUrl="${props.parentUrl}"
-    data-network="${props.network}" data-issuer="${props.issuer}" data-issuersignature="${props.issuerSignature}"
-    data-participant="${props.participant}" data-participantsignature="${props.participantSignature}"
-    data-smartcontract="${props.smartContract}" data-erc20="${props.ERC20}"
-    data-blockedcountries="${JSON.stringify(props.blockedCountries)}"
+  <div data-contracttype="fulfilled" data-version="${props.version}" data-signedon="${props.signedDate}"
+    data-created="${props.createdDate}" data-expires="${props.expires}" data-redirectto="${props.redirectto}"
+    data-parentUrl="${props.parentUrl}" data-network="${props.network}" data-issuer="${props.issuer}"
+    data-issuersignature="${props.issuerSignature}" data-participant="${props.participant}"
+    data-participantsignature="${props.participantSignature}" data-smartcontract="${props.smartContract}"
+    data-erc20="${props.ERC20}" data-blockedcountries="${JSON.stringify(props.blockedCountries)}"
     data-blockedaddresses="${JSON.stringify(props.blockedAddresses)}" id="page">
     <div id="contract-display"></div>
     <table id="display-table">
