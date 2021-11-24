8a61add46c1ed46270f2afa049d1d8e17defc233
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-24 18:03:55 +0000

    updated redirect button style

--- lib/view/templates/components/redirectCounter.ts
+++ lib/view/templates/components/redirectCounter.ts
@@ -1,12 +1,15 @@
 import { html } from "lit-html";
 
 export const redirectCounter = (count: number) =>
-  html` <p>Redirecting in ${count}</p> `;
+  html`<p>Redirecting in ${count}</p>`;
 
-export const redirectButton = html`<button
-    class="NextButton"
-    id="redirect-button"
-  >
-    Click here to redirect.
-  </button>
-  <hr />`;
+export const redirectButton = html`
+<style>
+  #redirect-button{
+    font-size:larger;
+  }
+</style>
+<button class="NextButton" id="redirect-button">
+  Click here to redirect
+</button>
+<hr />`;
