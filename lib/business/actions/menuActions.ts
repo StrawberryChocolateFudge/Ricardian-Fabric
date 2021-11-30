7171cef59f5d2461246f0ad70c033a7b09391122
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-30 14:18:42 +0000

    added smart contract button onclick

--- lib/business/actions/menuActions.ts
+++ lib/business/actions/menuActions.ts
@@ -6,8 +6,12 @@ import { verifyContractPopupTrigger } from "./verifyContractActions";
 export function menuActions(props: State) {
     verifyContractPopupTrigger();
     const createPage = getById("create-contract-button");
-
+    const smartContractButton = getById("smart-contract-catalog-button");
     createPage.onclick = function () {
         dispatch_renderCreate(props);
+    }
+
+    smartContractButton.onclick = function () {
+        console.log("smnart contract button clicked")
     }
 }
