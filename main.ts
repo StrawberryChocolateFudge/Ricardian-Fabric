3b8560b92e4fddfc9b3523b4f84bab67b65879b7
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-01 12:14:42 +0000

    import path changes

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 import "./lib/state";
 import "./lib/view";
-import "./lib/view/templates/acceptablePage";
+import "./lib/view/templates/pages/acceptablePage";
 import { dispatch_setInit } from "./lib/dispatch/stateChange";
 (async function init() {
-
-
   //Wait for the animation, then dispatch set init
-  setTimeout(function () { dispatch_setInit() }, 4000);
-
+  setTimeout(function () {
+    dispatch_setInit();
+  }, 4000);
 })();
