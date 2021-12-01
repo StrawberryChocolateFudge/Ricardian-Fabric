4883d92416bc724abcda8702e1e97d7b4538df7d
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-01 12:16:46 +0000

    WIP

--- lib/business/actions/catalogActions.ts
+++ lib/business/actions/catalogActions.ts
@@ -1,12 +1,14 @@
+import { dispatch_renderError } from "../../dispatch/render";
 import {
-  dispatch_hideElement,
-  dispatch_renderError,
-} from "../../dispatch/render";
-import { dispatch_setPopupState, dispatch_setSelectedWallet } from "../../dispatch/stateChange";
-import { PopupState, SelectedWallet, State } from "../../types";
+  dispatch_setPage,
+  dispatch_setSelectedWallet,
+} from "../../dispatch/stateChange";
+import { PageState, SelectedWallet, State } from "../../types";
 import { getById } from "../../view/utils";
 
 export function createProposalActions(props: State) {
+  //TODO: Inject Solc dependency here!
+
   const back = getById("createproposal-back");
   const proceed = getById("createproposal-proceed");
   const nameEl = getById("smartcontract-name") as HTMLInputElement;
@@ -15,11 +17,6 @@ export function createProposalActions(props: State) {
   ) as HTMLInputElement;
   const codeEl = getById("smartcontract-code") as HTMLInputElement;
 
-  const isERC20El = getById("smartcontract-isERC20") as HTMLInputElement;
-  const ERC20NameEl = getById("isERC20-name") as HTMLInputElement;
-  const ERC20SymbolEl = getById("isERC20-symbol") as HTMLInputElement;
-  const ERC20DecimalEl = getById("isERC20-decimals") as HTMLInputElement;
-
   const termsEl = getById("smartcontract-terms") as HTMLInputElement;
   const gitEl = getById("smartcontract-repo") as HTMLInputElement;
   const premiumEl = getById("smartcontract-premium") as HTMLInputElement;
@@ -27,22 +24,14 @@ export function createProposalActions(props: State) {
   const termsAcceptedEl = getById("accepted-terms") as HTMLInputElement;
 
   back.onclick = function () {
-    dispatch_setPopupState(PopupState.NONE);
+    dispatch_setPage(PageState.Catalog);
   };
 
   premiumEl.onclick = function () {
     priceEl.disabled = !premiumEl.checked;
   };
 
-  isERC20El.onclick = function () {
-    const checked = isERC20El.checked;
-    ERC20NameEl.disabled = !checked;
-    ERC20SymbolEl.disabled = !checked;
-    ERC20DecimalEl.disabled = !checked;
-  };
-
   proceed.onclick = async function () {
-
     if (nameEl.value === "") {
       dispatch_renderError("You must specify the name.");
       return;
@@ -64,21 +53,6 @@ export function createProposalActions(props: State) {
       return;
     }
 
-    if (isERC20El.checked) {
-      if (ERC20NameEl.value === "") {
-        dispatch_renderError("You must specify a name for the ERC20");
-        return;
-      }
-      if (ERC20SymbolEl.value === "") {
-        dispatch_renderError("You must specify a symbol for the ERC20");
-        return;
-      }
-      if (ERC20DecimalEl.value === "") {
-        dispatch_renderError("You must add a decimal for the token.");
-        return;
-      }
-    }
-
     if (premiumEl.checked === true) {
       if (priceEl.value === "") {
         dispatch_renderError(
@@ -94,20 +68,33 @@ export function createProposalActions(props: State) {
   };
 }
 
+export function catalogAction(props: State) {
+  const backbutton = getById("SCIntentBackButton") as HTMLButtonElement;
+  const nextButton = getById("SCIntentNextButton") as HTMLButtonElement;
+  const createProposalButton = getById("create-proposal-button");
+
+  createProposalButton.onclick = function () {
+    dispatch_setPage(PageState.Proposals);
+  };
+
+  backbutton.onclick = function () {
+    dispatch_setPage(PageState.Menu);
+  };
+
+  nextButton.onclick = function () {
+    // if (hrc20.checked) {
+    //   dispatch_SCDeploySelected(DeploySC.HRC20);
+    // }
+  };
+}
+
 export function addCatalogButtonListener(props: State) {
-  const catalogCheckboxToggle = getById("catalog_checkbox_toggle");
-  const catalogCheckboxButton = getById("catalog_checkbox_button");
-
-  catalogCheckboxButton.onclick = function () {
-    catalogCheckboxToggle.click();
-  }
-
   const smartcontract = getById("smart-contracts-button");
   //const proposals = getById("view-proposals-button");
   //const createProp = getById("create-proposal-button");
 
   smartcontract.onclick = function () {
-    dispatch_setPopupState(PopupState.Catalog);
+    // dispatch_setPopupState(PopupState.Catzalog);
   };
 
   // createProp.onclick = function () {
