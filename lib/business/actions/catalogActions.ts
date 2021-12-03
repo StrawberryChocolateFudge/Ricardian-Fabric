7065d51de39e0afc1209204ce5bb46ebf185b364
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-03 22:29:36 +0000

    update proposal

--- lib/business/actions/catalogActions.ts
+++ lib/business/actions/catalogActions.ts
@@ -1,84 +1,94 @@
-import { dispatch_renderError } from "../../dispatch/render";
+import { decryptWallet } from "../../crypto";
+import {
+  dispatch_renderError,
+  dispatch_renderProposalSummary,
+} from "../../dispatch/render";
 import {
   dispatch_setPage,
+  dispatch_setPopupState,
+  dispatch_setProposalType,
   dispatch_setSelectedWallet,
 } from "../../dispatch/stateChange";
-import { PageState, SelectedWallet, State } from "../../types";
-import { getById } from "../../view/utils";
+import {
+  PageState,
+  PopupState,
+  ProposalFormat,
+  ProposalType,
+  SelectedWallet,
+  State,
+  Status,
+} from "../../types";
+import { getById, readFile } from "../../view/utils";
+import { createProposalTransaction } from "../../wallet/arweave";
 
 export function createProposalActions(props: State) {
-  //TODO: Inject Solc dependency here!
+  const createRankButton = getById("get-rank-tab-button");
+
+  createRankButton.onclick = function () {
+    dispatch_setProposalType(ProposalType.Rank);
+  };
+
+  const createProposalButton = getById("propose-new-contract-tab-button");
+
+  createProposalButton.onclick = function () {
+    dispatch_setProposalType(ProposalType.NewSmartContract);
+  };
 
   const back = getById("createproposal-back");
-  const proceed = getById("createproposal-proceed");
-  const nameEl = getById("smartcontract-name") as HTMLInputElement;
-  const descriptionEl = getById(
-    "smartcontract-description"
-  ) as HTMLInputElement;
-  const codeEl = getById("smartcontract-code") as HTMLInputElement;
-
-  const termsEl = getById("smartcontract-terms") as HTMLInputElement;
-  const gitEl = getById("smartcontract-repo") as HTMLInputElement;
-  const premiumEl = getById("smartcontract-premium") as HTMLInputElement;
-  const priceEl = getById("smartcontract-price") as HTMLInputElement;
-  const termsAcceptedEl = getById("accepted-terms") as HTMLInputElement;
 
   back.onclick = function () {
     dispatch_setPage(PageState.Catalog);
   };
 
-  premiumEl.onclick = function () {
-    priceEl.disabled = !premiumEl.checked;
-  };
+  // TODO: get the rank from the smart contract
+  const rank = 0;
 
-  proceed.onclick = async function () {
-    if (nameEl.value === "") {
-      dispatch_renderError("You must specify the name.");
-      return;
-    }
-    if (descriptionEl.value === "") {
-      dispatch_renderError("You must add a short description.");
-      return;
-    }
-    if (codeEl.files.length !== 1) {
-      dispatch_renderError("You must add the smartcontract.");
-      return;
-    }
-    if (termsEl.files.length !== 1) {
-      dispatch_renderError("You must propose terms for the contract.");
-      return;
-    }
-    if (gitEl.value === "") {
-      dispatch_renderError("You must add a valid link for a git repo.");
-      return;
-    }
+  if (props.proposalType === ProposalType.Rank) {
+    const githubRepoUrl = getById("github-url") as HTMLInputElement;
+    const rankheader = getById("rankHeader") as HTMLHeadingElement;
+    const submitRankProposal = getById("create-rank-proposal");
 
-    if (premiumEl.checked === true) {
-      if (priceEl.value === "") {
-        dispatch_renderError(
-          "For premium contracts , you must specify a price."
-        );
+    rankheader.textContent = `Your rank is ${rank}`;
+
+    submitRankProposal.onclick = function () {
+      if (githubRepoUrl.value === "") {
+        dispatch_renderError("Empty input");
         return;
       }
-    }
-    if (termsAcceptedEl.checked === false) {
-      dispatch_renderError("You must accept the terms.");
-      return;
-    }
-  };
+
+      if (!githubRepoUrl.validity.valid) {
+        dispatch_renderError("Invalid url");
+        return;
+      }
+    };
+  }
+
+  if (props.proposalType === ProposalType.NewSmartContract) {
+    const contractProposalSubmit = getById("proposal-submit-button");
+
+    contractProposalSubmit.onclick = function () {
+      //TODO: I need tp get the ID entered,
+      // use arweave to check if the status is accepted by the network
+      //then I call the ricardian fabric smart contract
+    };
+  }
 }
 
 export function catalogAction(props: State) {
   const backbutton = getById("SCIntentBackButton") as HTMLButtonElement;
   const nextButton = getById("SCIntentNextButton") as HTMLButtonElement;
   const createProposalButton = getById("create-proposal-button");
-
+  const reviewAndVoteButton = getById("review-and-vote-button");
   createProposalButton.onclick = function () {
     dispatch_setPage(PageState.Proposals);
   };
 
   backbutton.onclick = function () {
     dispatch_setPage(PageState.Menu);
+  };
+
+  reviewAndVoteButton.onclick = function () {
+    dispatch_setPage(PageState.ReviewAndVote);
   };
 
   nextButton.onclick = function () {
@@ -108,5 +118,155 @@ export function walletSelectListener() {
     if (metamask.dataset.disabled === "false") {
       dispatch_setSelectedWallet(SelectedWallet.metamask);
     }
+  };
+}
+
+function artifactValid(data: string): boolean {
+  const artifact = JSON.parse(data);
+
+  if (artifact.bytecode === undefined) {
+    return false;
+  }
+
+  if (artifact.abi === undefined) {
+    return false;
+  }
+
+  return true;
+}
+
+export function uploadProposalActions(props: State) {
+  const backbutton = getById("create-contract-back");
+  backbutton.onclick = function () {
+    dispatch_setPopupState(PopupState.NONE);
+  };
+  const createContractProposal = getById("create-contract-proposal");
+
+  const nameEl = getById("smartcontract-name") as HTMLInputElement;
+  const descriptionEl = getById(
+    "smartcontract-description"
+  ) as HTMLInputElement;
+  const artifactEl = getById("smartcontract-artifact") as HTMLInputElement;
+
+  const termsEl = getById("smartcontract-terms") as HTMLInputElement;
+  const gitEl = getById("smartcontract-repo") as HTMLInputElement;
+  const commitEl = getById("smartcontract-commit") as HTMLInputElement;
+  const premiumEl = getById("smartcontract-premium") as HTMLInputElement;
+  const priceEl = getById("smartcontract-price") as HTMLInputElement;
+  const termsAcceptedEl = getById("accepted-terms") as HTMLInputElement;
+
+  const networkEl = getById("selected-network") as HTMLSelectElement;
+  const passwordEl = getById("arweave-key-password") as HTMLInputElement;
+
+  const categoryEl = getById("select-category") as HTMLSelectElement;
+  const implementsSimpleTerms = getById(
+    "implements-simpleterms-checkbox"
+  ) as HTMLInputElement;
+
+  premiumEl.onclick = function () {
+    priceEl.disabled = !premiumEl.checked;
+  };
+
+  createContractProposal.onclick = async function () {
+    console.log(props.Account);
+
+    if (nameEl.value === "") {
+      dispatch_renderError("You must specify the name.");
+      return;
+    }
+    if (descriptionEl.value === "") {
+      dispatch_renderError("You must add a short description.");
+      return;
+    }
+    if (artifactEl.value === "") {
+      dispatch_renderError("You must add the Artifact.");
+      return;
+    }
+
+    if (!artifactValid(artifactEl.value)) {
+      dispatch_renderError("Malformed Artifact.");
+      return;
+    }
+
+    if (gitEl.value === "") {
+      dispatch_renderError("You must add a valid link for a git repo.");
+      return;
+    }
+
+    if (commitEl.value === "") {
+      dispatch_renderError("Commit id is empty");
+      return;
+    }
+
+    if (termsEl.files.length !== 1) {
+      dispatch_renderError("You must propose terms for the contract.");
+      return;
+    }
+
+    if (premiumEl.checked === true) {
+      if (priceEl.value === "") {
+        dispatch_renderError(
+          "For premium contracts , you must specify a price."
+        );
+        return;
+      }
+
+      try {
+        parseFloat(priceEl.value);
+      } catch (err) {
+        dispatch_renderError(err);
+        return;
+      }
+    }
+    if (termsAcceptedEl.checked === false) {
+      dispatch_renderError("You must accept the terms.");
+      return;
+    }
+
+    if (passwordEl.value === "") {
+      dispatch_renderError("Misssing password");
+      return;
+    }
+
+    readFile(termsEl.files, async (data) => {
+      const proposal: ProposalFormat = {
+        name: nameEl.value,
+        description: descriptionEl.value,
+        artifact: JSON.parse(artifactEl.value),
+        terms: data,
+        git: gitEl.value,
+        commit: commitEl.value,
+        premium: premiumEl.checked,
+        price: parseFloat(priceEl.value),
+        network: networkEl.value,
+        category: categoryEl.value,
+        simpleterms: implementsSimpleTerms.checked,
+      };
+
+      console.log(proposal);
+
+      const decryptOptions = await decryptWallet(
+        props.Account.data,
+        passwordEl.value
+      );
+
+      if (decryptOptions.status !== Status.Success) {
+        dispatch_renderError(decryptOptions.error);
+        return;
+      }
+
+      const proposalTransaction = await createProposalTransaction(
+        proposal,
+        props.version,
+        decryptOptions.data,
+        nameEl.value,
+        categoryEl.value,
+        networkEl.value,
+        implementsSimpleTerms.checked
+      );
+
+      console.log(proposalTransaction);
+      dispatch_renderProposalSummary(proposalTransaction, props);
+    });
   };
 }
