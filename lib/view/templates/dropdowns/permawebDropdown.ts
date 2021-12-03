4856cc684dabfe76d40e33bf3edda790abff1149
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-03 22:34:39 +0000

    updated with an extra button

--- lib/view/templates/dropdowns/permawebDropdown.ts
+++ lib/view/templates/dropdowns/permawebDropdown.ts
@@ -1,10 +1,26 @@
 import { html } from "lit-html";
-import { accountLogo, arweaveLogo, pinLogo, uploadLogo } from "../components/logos";
+import { PageState } from "../../../types";
+import {
+  accountLogo,
+  arweaveLogo,
+  BallotLogo,
+  pinLogo,
+  uploadLogo,
+} from "../components/logos";
 
-export const PermawebDropdown = () => html`
-  <input type="checkbox" id="permaweb_checkbox_toggle" class="dropdown_checkbox_toggle" />
-  <button class="labelButton dropdown_checkbox_label lightCoral-shadow" id="permaweb_checkbox_button"
-    for="permaweb_checkbox_toggle">${arweaveLogo()} Permaweb</button>
+export const PermawebDropdown = (page: PageState) => html`
+  <input
+    type="checkbox"
+    id="permaweb_checkbox_toggle"
+    class="dropdown_checkbox_toggle"
+  />
+  <button
+    class="labelButton dropdown_checkbox_label lightCoral-shadow"
+    id="permaweb_checkbox_button"
+    for="permaweb_checkbox_toggle"
+  >
+    ${arweaveLogo()} Permaweb
+  </button>
   <ul>
     <li>
       <button id="Account-popup-button" class="dropdown-button">
@@ -16,11 +32,18 @@ export const PermawebDropdown = () => html`
         <small>${uploadLogo()} Upload File</small>
       </button>
     </li>
-    <li>
-      <button id="permapin-popup-button" class="dropdown-button">
-        <small>${pinLogo()} Permapin</small>
-      </button>
-    </li>
+    ${page === PageState.CreateRicardian
+      ? html`<li>
+          <button id="permapin-popup-button" class="dropdown-button">
+            <small>${pinLogo()} Permapin</small>
+          </button>
+        </li>`
+      : html`
+          <li>
+            <button class="dropdown-button" id="upload-proposal-button">
+              <small>${BallotLogo()} Upload proposal</small>
+            </button>
+          </li>
+        `}
   </ul>
 `;
-
