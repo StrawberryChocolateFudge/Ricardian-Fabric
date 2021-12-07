e57642c6db01494bda11bb10f233cc6fa036f54a
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-07 18:28:41 +0000

    added sidebar

---
+++ lib/view/templates/components/sideBar.ts
@@ -0,0 +1,23 @@
+import { html } from "lit-html";
+import { Menu, MenuOpen, ViewSidebar } from "./logos";
+
+export function SideBar() {
+  return html` <div id="sidebar" class="collapsed">
+      <div class="text-align-center">
+        <button id="toggleClose" title="Close sidebar" class="labelButton">
+          ${Menu()}
+        </button>
+      </div>
+      <div id="menuItems"></div>
+    </div>
+    <div id="content">
+      <button
+        disabled
+        id="toggleOpen"
+        title="Toggle sidebar"
+        class="labelButton position-fixed"
+      >
+        ${MenuOpen()}
+      </button>
+    </div>`;
+}
