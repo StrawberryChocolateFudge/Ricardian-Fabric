2f987599e3e71048b4a16c47d55ffaf89714fc1a
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-07 18:29:21 +0000

    updated the buttons in the sidebar

--- lib/view/templates/pages/menuPage.ts
+++ lib/view/templates/pages/menuPage.ts
@@ -1,36 +1,54 @@
 import { html } from "lit-html";
 import { getBlockie } from "../components/getBlockies";
+import {
+  catalogLogo,
+  GradingLogo,
+  HandShakeLogo,
+  TokenLogo,
+} from "../components/logos";
 
 export const MenuPage = (blockieData: string) => html`
   <hr />
-  ${getBlockie(blockieData, "50px", "")}
-  <div class="logo-container">
-    <div class="Fabric">Ricardian Fabric DAO</div>
-  </div>
-  <hr />
-  <div>
-    <div class="routes-buttons-container">
-      <button
-        class="lightSlateGray-shadow routes-buttons"
-        id="create-contract-button"
-      >
-        Create a Ricardian Contract
-      </button>
-      <button
-        class="lightGreenish-shadow routes-buttons"
-        id="smart-contract-catalog-button"
-      >
-        Smart Contract Catalog
-      </button>
-    </div>
-    <div class="routes-buttons-container">
-      <button class="routes-buttons" id="staking-button">
-        Stake and Profit-Sharing
-      </button>
+  <div class="text-align-center">${getBlockie(blockieData, "50px", "")}</div>
 
-      <button class="routes-buttons" id="verify-contract-button">
-        Verify an Acceptable Contract
-      </button>
-    </div>
+  <hr />
+  <div class="text-align-center">
+    <button
+      class="lightSlateGray-shadow  labelButton"
+      id="create-contract-button"
+      title="Create a Ricardian contract"
+    >
+      ${HandShakeLogo()}
+    </button>
+  </div>
+
+  <div class="text-align-center">
+    <button
+      class="lightGreenish-shadow  labelButton"
+      id="smart-contract-catalog-button"
+      title="Smart contract catalog"
+    >
+      ${catalogLogo()}
+    </button>
+  </div>
+
+  <div class="text-align-center">
+    <button
+      title="Staking an Profit-Sharing"
+      class="labelButton lightGreenish-shadow"
+      id="staking-button"
+    >
+      ${TokenLogo()}
+    </button>
+  </div>
+
+  <div class="text-align-center">
+    <button
+      title="Verify an acceptable contract"
+      class="labelButton"
+      id="verify-contract-button"
+    >
+      ${GradingLogo()}
+    </button>
   </div>
 `;
