8b714feb36d0dde8a653eb2b94c59476773638f8
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-12 19:04:07 +0000

    added the dropper

--- lib/view/templates/components/docxDropper.ts
+++ lib/view/templates/components/docxDropper.ts
@@ -3,13 +3,27 @@ import { BackLogo } from "./logos";
 
 export const DocXDropper = () => html`
   <div id="docx-dropzone" class="drop-zone">
-    <span id="drop-prompt-docx" class="drop-zone__prompt">To load template, drop a .docx file here or click to select
-      it</span>
+    <span id="drop-prompt-docx" class="drop-zone__prompt"
+      >To load template, drop a .docx file here or click to select it</span
+    >
     <input type="file" name="docx" id="docx-input" class="drop-zone__input" />
   </div>
   <hr />
   <small>Loading a template will completely override the text area.</small>
   <hr />
-  <div><button id="dropper-back-button" class="backButton">${BackLogo()} Back</button></div>
+  <div>
+    <button id="dropper-back-button" class="backButton">
+      ${BackLogo()} Back
+    </button>
+  </div>
   <hr />
+`;
+
+export const ProposalDocXDropper = () => html`
+  <div id="docx-dropzone" class="drop-zone">
+    <span id="drop-prompt-docx" class="drop-zone__prompt"
+      >To add the terms, drop a .docx file here or click to select it</span
+    >
+    <input type="file" name="docx" id="docx-input" class="drop-zone__input" />
+  </div>
 `;
