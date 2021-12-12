07e2b794ef515f1075f09da6bd832d10da30965d
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-12 18:58:38 +0000

    onDocProposalFileDropped

--- lib/business/actions/onDocFileDropped.ts
+++ lib/business/actions/onDocFileDropped.ts
@@ -102,3 +102,65 @@ export function docxImportBackButton() {
     dispatch_setPopupState(PopupState.NONE);
   };
 }
+
+export function onDocProposalFileDropped(props: State) {
+  const docxInput = getById("docx-input") as HTMLInputElement;
+  const dropZone = getById("docx-dropzone");
+
+  dropZone.onclick = function () {
+    if (docxInput.disabled) {
+      return;
+    }
+    docxInput.click();
+  };
+
+  docxInput.onchange = function () {
+    if (docxInput.disabled) {
+      return;
+    }
+    const file = docxInput.files[0];
+    if (
+      docxInput.files.length === 1 &&
+      file.type ===
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ) {
+      // It's valid
+      dispatch_promptSuccessDOCX(docxInput.files[0]);
+      dispatch_removeError();
+    } else {
+      dispatch_promptErrorDOCX("Invalid file,must be a docx file");
+    }
+  };
+
+  dropZone.ondragover = function (e: Event) {
+    e.preventDefault();
+    dropZone.classList.add("drop-zone--over");
+  };
+  dropZone.ondragleave = function (e: Event) {
+    dropZone.classList.remove("drop-zone--over");
+  };
+  dropZone.ondragend = function (e: Event) {
+    dropZone.classList.remove("drop-zone--over");
+  };
+
+  dropZone.ondrop = function (e: DragEvent) {
+    if (docxInput.disabled) {
+      return;
+    }
+
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    if (
+      e.dataTransfer.files.length === 1 &&
+      file.type ===
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ) {
+      docxInput.files = e.dataTransfer.files;
+      dispatch_promptSuccessDOCX(e.dataTransfer.files[0]);
+      dispatch_removeError();
+    } else {
+      dispatch_promptErrorDOCX("Invalid file, must be a docx file");
+    }
+    dropZone.classList.remove("drop-zone--over");
+  };
+}
