33c174b130c6d9eaf00f10a703ecb80ccc6cd6eb
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-20 15:12:35 +0000

    created popup for DAO terms

---
+++ lib/view/templates/popups/DaoTermsPopup.ts
@@ -0,0 +1,17 @@
+import { html } from "lit-html";
+
+export function DaoTermsPopup(url: string) {
+  return html`<h4>You need to sign the proposal creation terms.</h4>
+    <hr />
+    <div>
+      ${url === null
+        ? null
+        : html`<a href="${url}" target="_blank" rel="noopener">Here</a>`}
+    </div>
+    <hr />
+    <div>
+      <button class="labelButton" id="refresh-button">Refresh page</button>
+    </div>
+    <hr />
+    <hr /> `;
+}
