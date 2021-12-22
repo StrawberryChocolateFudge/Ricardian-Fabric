4414f71088b0867a10a5ae6f7f829f050127a6bd
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-22 15:23:19 +0000

    added rankProposalTable impl

--- lib/view/templates/pages/reviewAndVotePage.ts
+++ lib/view/templates/pages/reviewAndVotePage.ts
@@ -1,4 +1,5 @@
 import { html } from "lit-html";
+import { RankProposal, RemovalProposal } from "../../../types";
 import { getBlockie } from "../components/getBlockies";
 import {
   AddLogo,
@@ -7,12 +8,12 @@ import {
   ThumbsUp,
   WebAsset,
 } from "../components/logos";
-import { getCategories } from "./catalogPage";
+import { getRankPagingButtons, GetStatus } from "./manageProposals";
 
 export function ReviewAndVote() {
   return html`
     <h2>
-      Review and Vote
+      DAO
       <button
         id="create-proposal-button"
         class="labelButton"
@@ -24,42 +25,98 @@ export function ReviewAndVote() {
         ${ManageAccountLogo()}
       </button>
     </h2>
-    <h5>New smart contract proposals</h5>
-    <table class="light-shadow">
-      <thead>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <label id="categories-label" for="select-category">Category:</label>
-          </td>
-          <td>${getCategories()}</td>
-          <td><hr /></td>
-          <td>
-            <button class="labelButton">Search</button>
-          </td>
-          <td>
-            <input id="search-input" type="text" placeholder="Search for..." />
-          </td>
-          <td>
-            <label>Sort by date</label>
-          </td>
-          <td>
-            <select>
-              <option>Ascending</option>
-              <option>Descending</option>
-            </select>
-          </td>
-        </tr>
-      </tbody>
+    <div id="rank-proposal-table"></div>
+  `;
+}
+
+export function RankProposalTable(
+  ranks: RankProposal[],
+  rankIndexes: string[],
+  blockNumber: number,
+  totalPages: number,
+  currentPage: number
+) {
+  let rankProposalTRs = [];
+  for (let i = 0; i < rankIndexes.length; i++) {
+    if (rankIndexes[i] !== "0") {
+      rankProposalTRs.push({ rank: ranks[i], index: rankIndexes[i] });
+    }
+  }
+  console.log(rankProposalTRs);
+
+  return html`
+    <hr />
+    <h5>New Rank</h5>
+    <hr />
+    <table class="light-shadow width-100Percent">
+      <tr>
+        <td><label>Address</label></td>
+        <td><label>Repository</label></td>
+        <td><label>Approve</label></td>
+        <td><label>Reject</label></td>
+        <td><label>Status</label></td>
+      </tr>
+      ${rankProposalTRs.map((r) =>
+        rankProposalTR(r.rank, r.index, blockNumber)
+      )}
     </table>
+    <div>${getRankPagingButtons(totalPages, currentPage)}</div>
+  `;
+}
+
+function rankProposalTR(
+  rankProposal: RankProposal,
+  index: string,
+  blockNumer: number
+) {
+  return html`<tr>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+    </tr>
+    <tr>
+      <td>${getBlockie(rankProposal.creator, "50px", "")}</td>
+      <td>
+        <a
+          class="labelButton"
+          href="${rankProposal.repository}"
+          target="_blank"
+          rel="noopener"
+          title="${rankProposal.repository}"
+          >${WebAsset()}</a
+        >
+      </td>
+      <td>
+        <button
+          class="labelButton rankProposalApproveButton"
+          data-index="${index}"
+        >
+          ${ThumbsUp()}
+        </button>
+        ${rankProposal.approvals}
+      </td>
+      <td>
+        <button
+          class="labelButton rankProposalRejectButton"
+          data-index="${index}"
+        >
+          ${ThumbsDown()}
+        </button>
+        <span>${rankProposal.rejections}</span>
+      </td>
+      <td>
+        ${rankProposal.closed
+          ? "Closed"
+          : GetStatus(blockNumer, rankProposal.createdBlock)}
+      </td>
+    </tr>`;
+}
+
+export function SmartContractProposalsTable() {
+  return html`    </h2>
+    <h5>New smart contract proposals</h5>
     <hr />
     <table class="light-shadow">
       <tr>
@@ -80,73 +137,40 @@ export function ReviewAndVote() {
         <td><button class="labelButton">${ThumbsUp()}</button></td>
         <td><button class="labelButton">${ThumbsDown()}</button></td>
       </tr>
-    </table>
-    <hr />
-    <h5>New Rank</h5>
-    <table class="light-shadow">
-      <thead>
+    </table>`;
+}
+
+export function RemovalProposalsTable(removalProposals: RemovalProposal[]) {
+  return html` </table>
+      <hr />
+      <h5>Removal Request</h5>
+      <hr />
+      <table class="light-shadow">
         <tr>
-          <th></th>
-          <th></th>
+          <td><label>From</label></td>
+          <td><label>Id</label></td>
+          <td><label>Discussion</label></td>
+          <td><label>Name</label></td>
+          <td><label>Approve</label></td>
+          <td><label>Reject</label></td>
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <label>Sort by date</label>
-          </td>
-          <td>
-            <select>
-              <option>Ascending</option>
-              <option>Descending</option>
-            </select>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <hr />
-    <table class="light-shadow">
-      <tr>
-        <td><label>Address</label></td>
-        <td><label>Repository</label></td>
-        <td><label>Approve</label></td>
-        <td><label>Reject</label></td>
-      </tr>
-      <tr>
-        <td>${getBlockie("reviewer address", "50px", "")}</td>
-        <td>
-          <a
-            class="labelButton"
-            href="https://github.com/StrawberryChocolateFudge/HRC20"
-            target="_blank"
-            rel="noopener"
-            >${WebAsset()}</a
-          >
-        </td>
-        <td><button class="labelButton">${ThumbsUp()}</button></td>
-        <td><button class="labelButton">${ThumbsDown()}</button></td>
-      </tr>
-    </table>
-    <hr />
-    <h5>Removal Request</h5>
-    <hr />
-    <table class="light-shadow">
-      <tr>
-        <td><label>From</label></td>
-        <td><label>Id</label></td>
-        <td><label>Discussion</label></td>
-        <td><label>Name</label></td>
-        <td><label>Approve</label></td>
-        <td><label>Reject</label></td>
-      </tr>
-      <tr>
-        <td>${getBlockie("Reporter", "50px", "")}</td>
-        <td>${getBlockie("asf", "50px", "")}</td>
-        <td><a href="/" class="labelButton">Here</a></td>
-        <td><button class="labelButton">HRC-20 token</button></td>
-        <td><button class="labelButton">${ThumbsUp()}</button></td>
-        <td><button class="labelButton">${ThumbsDown()}</button></td>
-      </tr>
-    </table>
-  `;
+        ${RemovalRequestBuilder(removalProposals)}
+      </table>
+    </table>`;
+}
+
+export function RemovalRequestBuilder(removalProposals: RemovalProposal[]) {
+  // TODO: Removal proposals need to fetch the transaction ID from the acceptable,
+  // to populate the ID field blocky,the name etc
+
+  return html`${removalProposals.map(
+    (proposal: RemovalProposal) => html`<tr>
+      <td>${getBlockie(proposal.creator, "50px", "")}</td>
+      <td>${getBlockie("asf", "50px", "")}</td>
+      <td><a href="${proposal.discussionUrl}" class="labelButton">Here</a></td>
+      <td><button class="labelButton">HRC-20 token</button></td>
+      <td><button class="labelButton">${ThumbsUp()}</button></td>
+      <td><button class="labelButton">${ThumbsDown()}</button></td>
+    </tr> `
+  )}`;
 }
