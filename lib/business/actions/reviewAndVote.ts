509dd3abcb42e69f92a1cdf58c87f816e8cf2bdc
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-22 15:30:57 +0000

    proposal fetching and voting implemented

--- lib/business/actions/reviewAndVote.ts
+++ lib/business/actions/reviewAndVote.ts
@@ -1,33 +1,44 @@
-import { dispatch_setPage } from "../../dispatch/stateChange";
-import { PageState, State } from "../../types";
+import {
+  dispatch_setPage,
+  dispatch_setPopupState,
+} from "../../dispatch/stateChange";
+import {
+  AcceptedSmartContractProposal,
+  PageState,
+  PopupState,
+  RankProposal,
+  RemovalProposal,
+  SmartContractProposal,
+  State,
+} from "../../types";
 import { getById } from "../../view/utils";
-import { getAddress, requestAccounts, web3Injected } from "../../wallet/web3";
+import {
+  checkNetwork,
+  getAddress,
+  getBlockNumber,
+  requestAccounts,
+  web3Injected,
+} from "../../wallet/web3";
 import MetaMaskOnboarding from "@metamask/onboarding";
 import {
-  getCatalogDAOContract,
+  dispatch_renderError,
+  dispatch_renderReviewRankProposals,
+} from "../../dispatch/render";
+import {
+  getCatalogDAOContractWithWallet,
   getRankProposalIndex,
+  getSmartContractProposalIndex,
+  getAcceptedSmartContractIndex,
+  getRemovalProposalIndex,
+  getMyRankProposalsPaginated,
+  getMySmartContractProposalsPaginated,
+  getAcceptedSmartContractProposalsPaginated,
+  getRemovalProposalsPaginated,
+  voteOnNewRank,
 } from "../../wallet/catalogDAO/contractCalls";
-import { dispatch_renderError } from "../../dispatch/render";
+import { getPaginatedByIndexSTART } from "../utils";
 
 export async function reviewAndVotePageActions(props: State) {
-  if (!web3Injected()) {
-    dispatch_renderError("Found no injected web3, install metamask");
-    const onboarding = new MetaMaskOnboarding();
-    onboarding.startOnboarding();
-    return;
-  }
-
-  await requestAccounts();
-  // IT MUST BE HARMONY NETWORK!
-  // If it's not, prompt to switch to harmony
-  // Should show a popup prompting to switch to harmony network!
-  //await switchToHarmony(0, "Testnet");
-
-  const myAddress = await getAddress();
-  const catalogDAO = await getCatalogDAOContract();
-
-  const rankProposalIndex = await getRankProposalIndex(catalogDAO, myAddress);
-
   const createProposalButton = getById("create-proposal-button");
   createProposalButton.onclick = function () {
     dispatch_setPage(PageState.Proposals);
@@ -37,4 +48,108 @@ export async function reviewAndVotePageActions(props: State) {
   myProposalsButton.onclick = function () {
     dispatch_setPage(PageState.ManageProposals);
   };
+
+  if (!web3Injected()) {
+    dispatch_renderError("Found no injected web3, install metamask");
+    const onboarding = new MetaMaskOnboarding();
+    onboarding.startOnboarding();
+    return;
+  }
+
+  await requestAccounts();
+  const correctNetwork = await checkNetwork();
+
+  if (!correctNetwork) {
+    dispatch_renderError("You need to switch to Harmony network!");
+    dispatch_setPopupState(PopupState.WrongNetwork);
+    return;
+  }
+
+  const myAddress = await getAddress();
+  const catalogDAO = await getCatalogDAOContractWithWallet();
+  const blockNumber = await getBlockNumber();
+
+  const rankPage = await getPaginatedByIndexSTART<RankProposal[]>(
+    catalogDAO,
+    myAddress,
+    getRankProposalIndex,
+    getMyRankProposalsPaginated
+  );
+
+  dispatch_renderReviewRankProposals(props, blockNumber, rankPage);
+
+  const smartContractPage = await getPaginatedByIndexSTART<
+    SmartContractProposal[]
+  >(
+    catalogDAO,
+    myAddress,
+    getSmartContractProposalIndex,
+    getMySmartContractProposalsPaginated
+  );
+
+  const acceptedSmartContractPage =
+    await getPaginatedByIndexSTART<AcceptedSmartContractProposal>(
+      catalogDAO,
+      myAddress,
+      getAcceptedSmartContractIndex,
+      getAcceptedSmartContractProposalsPaginated
+    );
+
+  const removalPage = await getPaginatedByIndexSTART<RemovalProposal>(
+    catalogDAO,
+    myAddress,
+    getRemovalProposalIndex,
+    getRemovalProposalsPaginated
+  );
+}
+
+export async function rankProposalTableActions() {
+  const approveButtons = document.getElementsByClassName(
+    "rankProposalApproveButton"
+  );
+  const rejectButtons = document.getElementsByClassName(
+    "rankProposalRejectButton"
+  );
+  const myAddress = await getAddress();
+  const catalogDAO = await getCatalogDAOContractWithWallet();
+  const onError = (error, receipt) => {
+    if (error.message.includes("903")) {
+      dispatch_renderError(
+        "You need to accept the DAO terms before you can vote."
+      );
+    } else if (error.message.includes("911")) {
+      dispatch_renderError("You need rank to vote.");
+    } else if (error.message.includes("912")) {
+      dispatch_renderError("You voted already!");
+    } else {
+      dispatch_renderError(error.message);
+    }
+  };
+
+  const onReceipt = (receipt) => {
+    dispatch_setPage(PageState.ReviewAndVote);
+  };
+
+  const vote = async (approve: boolean, index: string) =>
+    await voteOnNewRank(
+      catalogDAO,
+      index,
+      approve,
+      myAddress,
+      onError,
+      onReceipt
+    );
+
+  for (let i = 0; i < approveButtons.length; i++) {
+    const approveButton = approveButtons[i] as HTMLButtonElement;
+    const rejectButton = rejectButtons[i] as HTMLButtonElement;
+    approveButton.onclick = async function () {
+      const index = approveButton.dataset.index;
+      vote(true, index);
+    };
+    rejectButton.onclick = async function () {
+      const index = rejectButton.dataset.index;
+      vote(false, index);
+    };
+  }
 }
