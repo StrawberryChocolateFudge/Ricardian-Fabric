6ef76aa0fff9dd4c9ac8c10b4081e45d3ca9e685
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-01-17 16:37:33 +0000

    created

---
+++ lib/view/templates/pages/connectWalletPage.ts
@@ -0,0 +1,16 @@
+import { html } from "lit-html";
+import { ConnectWalletLogo } from "../components/logos";
+
+export const ConnectWalletPage = () =>
+  html`
+    <div class="placeholder-item height200">
+      <button
+        id="connectWalletButton"
+        class="labelButton width-200 marginTop-50 unselectable"
+      >
+        <h4>Connect your wallet</h4>
+        <hr />
+        ${ConnectWalletLogo()}
+      </button>
+    </div>
+  `;
