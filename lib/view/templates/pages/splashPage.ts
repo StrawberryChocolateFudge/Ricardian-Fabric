439a22f770caa6df61846cd1ecb2ee32e93f969f
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-01-17 16:36:46 +0000

    created

---
+++ lib/view/templates/pages/splashPage.ts
@@ -0,0 +1,10 @@
+import { html } from "lit-html";
+
+export const SplashPage = () => html`
+  <div class="intro-logo-container">
+    <div class="FabricText">Ricardian Fabric</div>
+    <div class="DaoText">
+      <span>DAO</span>
+    </div>
+  </div>
+`;
