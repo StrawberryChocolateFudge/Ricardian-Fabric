085a312c10c94e4084c65150c4819081091724cb
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-01-17 16:39:27 +0000

    added query for uploaded data

--- lib/fetch/graphql.ts
+++ lib/fetch/graphql.ts
@@ -1,25 +1,63 @@
-import { request } from 'graphql-request'
-import { Options, Status } from '../types';
+import { request } from "graphql-request";
+import { Options, Status } from "../types";
 
 const ARWEAVEAPI = "https://arweave.net/graphql";
 
-
 export async function getOwnerFromTxId(txId: string): Promise<Options<string>> {
-    const options: Options<string> = { status: Status.Success, error: "", data: "" };
+  const options: Options<string> = {
+    status: Status.Success,
+    error: "",
+    data: "",
+  };
 
-    const query = `query {
+  const query = `query {
     transaction(id: "${txId}") {
        owner{
         address
       }
     }
-}`
+}`;
 
-    const result = await request(ARWEAVEAPI, query).catch(err => {
-        options.error = err.message;
-        options.status = Status.Failure;
-    });
+  const result = await request(ARWEAVEAPI, query).catch((err) => {
+    options.error = err.message;
+    options.status = Status.Failure;
+  });
 
-    options.data = result.transaction.owner.address;
-    return options
+  options.data = result.transaction.owner.address;
+  return options;
+}
+
+export async function getUploadedContracts(): Promise<Options<string>> {
+  const options: Options<string> = {
+    status: Status.Success,
+    error: "",
+    data: "",
+  };
+
+  const query = `query {
+    transactions(
+        tags: {
+            name: "App-Name",
+            values: ["Ricardian Fabric"]
+        }
+    ) {
+        edges {
+            node {
+                id
+                tags{
+                    name
+                    value
+                }
+            }
+
+        }
+    }
+}`;
+
+  const result = await request(ARWEAVEAPI, query).catch((err) => {
+    options.error = err.message;
+    options.status = Status.Failure;
+  });
+  options.data = result;
+  return options;
 }
