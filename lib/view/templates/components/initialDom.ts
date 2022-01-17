f60940180ff910f64b335c3eab2e7b9d3c2291c9
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-01-17 16:37:57 +0000

    added favicon

--- lib/view/templates/components/initialDom.ts
+++ lib/view/templates/components/initialDom.ts
@@ -6,6 +6,8 @@ export const initialStringDom = `  <!DOCTYPE html>
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon"
+      href="https://arweave.net/cGU3w4SlEESxajw-t_mhoBArT7WTwmac1mvjlBqNIi0"/>
       <title>Ricardian Fabric</title>
     </head>
     <body>
