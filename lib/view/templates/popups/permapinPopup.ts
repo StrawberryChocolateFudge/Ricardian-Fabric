3fc7ba721fe7ecd370afa6663d0a7aee61adfd34
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-01-19 13:23:53 +0000

    terms button removed

--- lib/view/templates/popups/permapinPopup.ts
+++ lib/view/templates/popups/permapinPopup.ts
@@ -4,13 +4,15 @@ import { helperTooltips } from "../components/helperTooltips";
 import { BackLogo, pinLogoWhite } from "../components/logos";
 
 export const PermapinPopup = () => html`
-<style>
-  #permapin-proceed{
-    padding-right: 10px;
-  }
-</style>
+  <style>
+    #permapin-proceed {
+      padding-right: 10px;
+    }
+  </style>
   <h2>Permapin a Contract</h2>
-  <small>You can permapin the IPFS content on Arweave to store it forever.</small>
+  <small
+    >You can permapin the IPFS content on Arweave to store it forever.</small
+  >
   <small>Only content created with Ricardian Fabric is allowed.</small>
   <table>
     <thead>
@@ -30,8 +32,13 @@ export const PermapinPopup = () => html`
       <tr>
         <td><label for="walletPassword">Password:</label></td>
         <td>
-          <input readonly onfocus="this.removeAttribute('readonly');" autocomplete="off" id="walletPassword"
-            type="password" />
+          <input
+            readonly
+            onfocus="this.removeAttribute('readonly');"
+            autocomplete="off"
+            id="walletPassword"
+            type="password"
+          />
         </td>
         <td>${helperTooltips("The password of the key file")}</td>
       </tr>
@@ -40,11 +47,13 @@ export const PermapinPopup = () => html`
                             <td>
                               <input id="tipcheckbox" type="checkbox" checked />
                             </td>
-                            <td>${helperTooltips(`Support us by sending a tip. ${TIP} Ar`)}</td>
+                            <td>${helperTooltips(
+        `Support us by sending a tip. ${TIP} Ar`
+      )}</td>
                           </tr> -->
       <tr>
         <td>
-          <label id="terms-button" class="terms-button-label">I accept the terms.</label>
+          <label>I accept the terms.</label>
         </td>
         <td><input id="permapin-terms-checkbox" type="checkbox" /></td>
       </tr>
@@ -53,7 +62,9 @@ export const PermapinPopup = () => html`
   <hr />
   <div class="wide-row">
     <button class="backButton" id="permapin-back">${BackLogo()} Back</button>
-    <button class="NextButton" id="permapin-proceed">${pinLogoWhite()} <strong>PIN!</strong></button>
+    <button class="NextButton" id="permapin-proceed">
+      ${pinLogoWhite()} <strong>PIN!</strong>
+    </button>
   </div>
   <hr />
 `;
@@ -63,7 +74,6 @@ export const PermapinSummaryPage = (arg: {
   sendTip: boolean;
   tipTx: any;
 }) => {
-
   const getFee = () => {
     let reward = parseFloat(arg.permapinTx.tx.reward);
     if (arg.sendTip) {
@@ -75,26 +85,28 @@ export const PermapinSummaryPage = (arg: {
   };
 
   return html`<h2>Permapin Summary</h2>
-<table>
-  <thead>
-    <tr>
-      <th></th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Fee:</td>
-      <td>${getFee()}</td>
-    </tr>
-  </tbody>
-</table>
-<div id="transaction-loading"></div>
-<div class="wide-row">
-  <button class="backButton" id="permapinPost-back">${BackLogo()} Back</button>
-  <button class="NextButton" id="permapinPost-proceed">
-    Post Transaction
-  </button>
-</div>
-<hr />`;
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Fee:</td>
+          <td>${getFee()}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div id="transaction-loading"></div>
+    <div class="wide-row">
+      <button class="backButton" id="permapinPost-back">
+        ${BackLogo()} Back
+      </button>
+      <button class="NextButton" id="permapinPost-proceed">
+        Post Transaction
+      </button>
+    </div>
+    <hr />`;
 };
