7502bffed68ad30f2d035bf623ab223d5248a0d5
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-01-20 16:11:37 +0000

    added the release function

--- lib/wallet/ricVault/contractCalls.ts
+++ lib/wallet/ricVault/contractCalls.ts
@@ -3,7 +3,7 @@ import Web3 from "web3";
 import { getRicVaultAbi } from "../abi/ricVaultABI";
 import { LockedTokens } from "../../types";
 
-const RICVAULTADDRESS = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
+export const RICVAULTADDRESS = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
 
 export async function getRicVaultContract() {
   const web3 = new Web3(window.ethereum);
@@ -19,7 +19,21 @@ export async function lockFunds(
   onReceipt: any
 ) {
   await ricVault.methods
-    .lockFunds(period, amount)
+    .lockFunds(period, Web3.utils.toWei(amount))
+    .send({ from })
+    .on("error", onError)
+    .on("receipt", onReceipt);
+}
+
+export async function release(
+  ricVault: Contract,
+  atIndex: string,
+  from: string,
+  onError: any,
+  onReceipt: any
+) {
+  await ricVault.methods
+    .release(atIndex)
     .send({ from })
     .on("error", onError)
     .on("receipt", onReceipt);
@@ -47,8 +61,13 @@ export async function getVaultContent(
   forAddress: string,
   atIndex: string,
   from: string
-): Promise<LockedTokens[]> {
-  return await ricVault.methods
+): Promise<LockedTokens> {
+  const vaultContent = await ricVault.methods
     .getVaultContent(forAddress, atIndex)
     .call({ from });
+
+  return {
+    ...vaultContent,
+    lockedAmount: Web3.utils.fromWei(vaultContent.lockedAmount),
+  };
 }
