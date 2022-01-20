fbcdcdfafc1fe7d6869643dc6235e2d8fd3410a0
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-01-20 16:11:56 +0000

    implemented vault page

--- lib/view/templates/pages/vaultPage.ts
+++ lib/view/templates/pages/vaultPage.ts
@@ -1,3 +1,196 @@
-import { html } from "lit-html";
+import { html, nothing } from "lit-html";
+import { LockedTokens } from "../../../types";
+import {
+  ChevronLeftBlack,
+  ChevronRightBlack,
+  KeyImage,
+  LockImage,
+  SpendLogo,
+  TokenLogoIcon,
+  ToyBlocks,
+} from "../components/logos";
+import {
+  getPageButtonEndPoint,
+  getPageButtonStartPoint,
+} from "../components/paginations";
+import { balanceDisplay } from "./tokenSalePage";
 
-export const VaultPage = () => html`<h2>Vault page</h2>`;
+export const VaultPage = () => html`<h3>Vault</h3>
+  ${balanceDisplay()} ${blockDisplay()}
+  <h6>
+    You can lock Ric and release it after the blocks specified. The Ric you use
+    while claiming Fees will go here.
+  </h6>
+  <hr />
+  <!-- <h3></h3> -->
+  <div class="text-align-center center">
+    <table class="width-200 box">
+      <tr>
+        <th></th>
+        <th></th>
+        <th></th>
+      </tr>
+      <tr>
+        <td>Lock</td>
+        <td>
+          <input
+            type="number"
+            id="lock-amount"
+            class="width-50"
+            placeholder="0"
+          />
+        </td>
+        <td>${TokenLogoIcon()} RIC</td>
+      </tr>
+      <tr>
+        <td>For</td>
+        <td>
+          <input
+            type="number"
+            id="lock-time"
+            class="width-50"
+            placeholder="0"
+          />
+        </td>
+        <td>${ToyBlocks()} Blocks</td>
+      </tr>
+      <tr>
+        <td class="width-50">
+          <button
+            id="approve-vault-button"
+            title="Approve spend for the vault"
+            class="labelButton"
+          >
+            ${SpendLogo()} Approve
+          </button>
+        </td>
+        <td><hr /></td>
+        <td class="width-50">
+          <button class="labelButton" id="lock-button" title="Lock Ric">
+            ${LockImage()} Lock
+          </button>
+        </td>
+      </tr>
+    </table>
+    <small
+      >Approved <span class="placeholder-item" id="spend"> </span> RIC to
+      spend</small
+    >
+  </div>
+  <hr />
+  <div id="vault-item-container" class="placeholder-item">
+    <hr />
+    <h3 class="center">Loading Vault</h3>
+    <hr />
+  </div> `;
+
+export const VaultItems = (
+  lockedTokens: LockedTokens[],
+  firstIndex: number,
+  lastIndex: number,
+  currentPage: number,
+  totalPages: number
+) => {
+  let indexes = [];
+  for (let i = firstIndex; i <= lastIndex; i++) {
+    indexes.push(i);
+  }
+
+  return html`<hr />
+    <div class="rowStart overflow-auto">
+      ${lockedTokens.map((token) =>
+        VaultItem(token, indexes[lockedTokens.indexOf(token)])
+      )}
+    </div>
+    <hr />
+    <div class="text-align-center">
+      ${getVaultPagingButtons(totalPages, currentPage, "vaultPagingButtons")}
+    </div> `;
+};
+
+export const VaultItem = (
+  lockedToken: LockedTokens,
+  index: number
+) => html` <div class="box padding-20 marginBottom-10">
+  <div
+    data-forperiod="${lockedToken.period}"
+    data-created="${lockedToken.created}"
+    data-index="${index}"
+    data-released="${lockedToken.released}"
+    class="all-blocks-left"
+    id="${"blocksleft" + index}"
+  ></div>
+  <div class="box-title">
+    Amount: ${TokenLogoIcon()}${lockedToken.lockedAmount} RIC
+  </div>
+  <div>
+    ${lockedToken.released
+      ? html`<p>Relased</p>`
+      : html`<button
+          disabled
+          data-lockindex="${index + 1}"
+          class="labelButton vaultReleaseButtons"
+          id="vaultRelease_${index}"
+        >
+          ${KeyImage()} Release
+        </button>`}
+  </div>
+</div>`;
+
+export const blockDisplay = () => html`
+  <div>
+    Current block: ${ToyBlocks()}
+    <span id="current-block" class="placeholder-item"></span>
+  </div>
+`;
+
+export function getVaultPagingButtons(
+  totalPages: number,
+  currentPage: number,
+  cssselector: string
+) {
+  let pageButtons = [];
+  const start = getPageButtonStartPoint(totalPages, currentPage);
+  const end = getPageButtonEndPoint(totalPages, currentPage);
+
+  for (let i = start; i <= end; i++) {
+    pageButtons.push(
+      html` <button
+        data-vaultpage="${i}"
+        class="${cssselector} labelButton ${currentPage === i
+          ? "light-shadow"
+          : null}"
+      >
+        ${i}
+      </button>`
+    );
+  }
+
+  if (pageButtons.length === 1) {
+    return null;
+  }
+
+  return html`<button
+      id="vault-page-left"
+      data-vaultpage="${currentPage}"
+      data-totalpages="${totalPages}"
+      class="labelButton ${currentPage === 1
+        ? "background-ccc cursor-notallowed"
+        : nothing}}"
+    >
+      ${ChevronLeftBlack()}</button
+    >${pageButtons.map((res) => res)}<button
+      id="vault-page-right"
+      data-vaultpage="${currentPage}"
+      data-totalpages="${totalPages}"
+      class="labelButton ${currentPage === totalPages
+        ? "background-ccc cursor-notallowed"
+        : nothing}"
+    >
+      ${ChevronRightBlack()}
+    </button>`;
+}
+
+export const LoadingVault = () => html` <hr />
+  <h3 class="center">Loading Vault</h3>
+  <hr />`;
