f58650f8f226981a4bc948c9fdf16d1407b49d08
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-02 12:45:29 +0000

    refactored the dropdown

--- lib/view/templates/dropdowns/permawebDropdown.ts
+++ lib/view/templates/dropdowns/permawebDropdown.ts
@@ -4,6 +4,7 @@ import {
   accountLogo,
   arweaveLogo,
   BallotLogo,
+  CommentLogo,
   pinLogo,
   SaveLogo,
   uploadLogo,
@@ -12,56 +13,82 @@ import {
 export const PermawebDropdown = (
   contractType: ContractTypes,
   page: PageState
-) => html`
-  <input
-    type="checkbox"
-    id="permaweb_checkbox_toggle"
-    class="dropdown_checkbox_toggle"
-  />
-  <button
-    class="labelButton dropdown_checkbox_label lightCoral-shadow"
-    id="permaweb_checkbox_button"
-    for="permaweb_checkbox_toggle"
-  >
-    ${arweaveLogo()} Permaweb
-  </button>
-  <ul>
-    <li>
-      <button id="Account-popup-button" class="dropdown-button">
-        <small>${accountLogo()} Account</small>
-      </button>
-    </li>
+) => {
+  return html`
+    <input
+      type="checkbox"
+      id="permaweb_checkbox_toggle"
+      class="dropdown_checkbox_toggle"
+    />
+    <button
+      class="labelButton dropdown_checkbox_label lightCoral-shadow"
+      id="permaweb_checkbox_button"
+      for="permaweb_checkbox_toggle"
+    >
+      ${arweaveLogo()} Permaweb
+    </button>
+    <ul>
+      <li>
+        <button id="Account-popup-button" class="dropdown-button">
+          <small>${accountLogo()} Account</small>
+        </button>
+      </li>
+      ${getDropdowns(contractType, page)}
+    </ul>
+  `;
+};
 
-    ${contractType === ContractTypes.create
-      ? html` <li>
-          <button id="upload-popup-button" class="dropdown-button">
-            <small>${uploadLogo()} Upload File</small>
-          </button>
-        </li>`
-      : null}
-    ${contractType === ContractTypes.create
-      ? html`${page === PageState.CreateRicardian
-          ? html`<li>
-                <button id="permapin-popup-button" class="dropdown-button">
-                  <small>${pinLogo()} Permapin</small>
-                </button>
-              </li>
-              <li>
-                <button id="upload-currentpage-button" class="dropdown-button">
-                  <small>${SaveLogo()} Save page</small>
-                </button>
-              </li> `
-          : html`
-              <li>
-                <button class="dropdown-button" id="upload-proposal-button">
-                  <small>${BallotLogo()} Upload proposal</small>
-                </button>
-              </li>
-            `}`
-      : html`<li>
-          <button id="permapin-popup-button" class="dropdown-button">
-            <small>${pinLogo()} Permapin</small>
-          </button>
-        </li>`}
-  </ul>
-`;
+function getDropdowns(contractType: ContractTypes, page: PageState) {
+  switch (contractType) {
+    case ContractTypes.create:
+      switch (page) {
+        case PageState.CreateRicardian:
+          return html`
+            <li>
+              <button id="upload-popup-button" class="dropdown-button">
+                <small>${uploadLogo()} Upload File</small>
+              </button>
+            </li>
+            <li>
+              <button id="permapin-popup-button" class="dropdown-button">
+                <small>${pinLogo()} Permapin</small>
+              </button>
+            </li>
+          `;
+        case PageState.Proposals:
+          return html`
+            <li>
+              <button id="upload-popup-button" class="dropdown-button">
+                <small>${uploadLogo()} Upload File</small>
+              </button>
+            </li>
+            <li>
+              <button class="dropdown-button" id="upload-proposal-button">
+                <small>${BallotLogo()} Upload proposal</small>
+              </button>
+            </li>
+          `;
+        case PageState.trails:
+          return html` <li>
+              <button id="upload-popup-button" class="dropdown-button">
+                <small>${uploadLogo()} Upload File</small>
+              </button>
+            </li>
+            <li>
+              <button class="dropdown-button" id="upload-comment">
+                <small>${CommentLogo()}Add Comment</small>
+              </button>
+            </li>`;
+        default:
+          break;
+      }
+      break;
+    default:
+      return html`<li>
+        <button id="permapin-popup-button" class="dropdown-button">
+          <small>${pinLogo()} Permapin</small>
+        </button>
+      </li>`;
+      break;
+  }
+}
