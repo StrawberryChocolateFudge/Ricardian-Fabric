e9ad91330a16562dc073a305fdd1a214cf14c91f
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-02 12:53:01 +0000

    styles changed

--- lib/view/templates/pages/connectWalletPage.ts
+++ lib/view/templates/pages/connectWalletPage.ts
@@ -3,7 +3,7 @@ import { ConnectWalletLogo } from "../components/logos";
 
 export const ConnectWalletPage = () =>
   html`
-    <div class="placeholder-item height200">
+    <div class="center height200">
       <button
         id="connectWalletButton"
         class="labelButton width-200 marginTop-50 unselectable"
