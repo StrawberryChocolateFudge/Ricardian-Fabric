1a9fd2a3f79df6646127f73f005b385795cfa20f
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-02 12:48:20 +0000

    added query parser

--- lib/view/utils.ts
+++ lib/view/utils.ts
@@ -347,3 +347,45 @@ export function copyAddressToClipboard() {
   const addressEl = getById("arweave-address");
   copyStringToClipboard(addressEl.dataset.address);
 }
+
+// THANKS TO https://gist.github.com/alisey/4552101
+// USED FOR PARSING QUERY STRINGS
+// Examples:
+// parseQueryString('a=1&a=2')                =>  {a: '2'}
+// parseQueryString('a=1', true)              =>  {a: ['1']}
+// parseQueryString('a=1&a=2', true)          =>  {a: ['1', '2']}
+// parseQueryString('a[][]=1&a[][]=2', true)  =>  {'a[][]': ['1', '2']}
+// parseQueryString('?a;b#c')                 =>  {'?a': '', 'b#c': ''}
+export function parseQueryString(query: string, groupByName: boolean) {
+  var parsed, hasOwn, pairs, pair, name, value;
+
+  if (typeof query != "string") {
+    throw "Invalid input";
+  }
+
+  parsed = {};
+  hasOwn = parsed.hasOwnProperty;
+  query = query.replace(/\+/g, " ");
+  pairs = query.split(/[&;]/);
+
+  for (var i = 0; i < pairs.length; i++) {
+    pair = pairs[i].match(/^([^=]*)=?(.*)/);
+    if (pair[1]) {
+      try {
+        name = decodeURIComponent(pair[1]);
+        value = decodeURIComponent(pair[2]);
+      } catch (e) {
+        throw "Invaid %-encoded sequence";
+      }
+
+      if (!groupByName) {
+        parsed[name] = value;
+      } else if (hasOwn.call(parsed, name)) {
+        parsed[name].push(value);
+      } else {
+        parsed[name] = [value];
+      }
+    }
+  }
+  return parsed;
+}
