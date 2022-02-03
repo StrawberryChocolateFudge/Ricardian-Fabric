0a0dd15163692987dc3b29de96b5a6c907a4ddb2
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-03 14:44:34 +0000

    added icon to redirect button

--- lib/view/templates/components/redirectCounter.ts
+++ lib/view/templates/components/redirectCounter.ts
@@ -1,15 +1,15 @@
 import { html } from "lit-html";
+import { RedirectIcon } from "./logos";
 
 export const redirectCounter = (count: number) =>
   html`<p>Redirecting in ${count}</p>`;
 
-export const redirectButton = html`
-<style>
-  #redirect-button{
-    font-size:larger;
-  }
-</style>
-<button class="NextButton" id="redirect-button">
-  Click here to redirect
-</button>
-<hr />`;
+export const redirectButton = html` <style>
+    #redirect-button {
+      font-size: larger;
+    }
+  </style>
+  <button class="labelButton" id="redirect-button">
+    ${RedirectIcon()} Click here to redirect
+  </button>
+  <hr />`;
