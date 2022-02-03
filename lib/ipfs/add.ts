ced89547b38888701d35ddaeaf97db0f54a110f4
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-03 14:42:44 +0000

    deprecating IPFS_CAT

--- lib/ipfs/add.ts
+++ lib/ipfs/add.ts
@@ -1,8 +1,8 @@
 import { CID, create } from "ipfs-http-client";
 import { IPFSParams } from "../types";
-import { concat as uint8ArrayConcat } from 'uint8arrays/concat'
-import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
-import all from 'it-all'
+import { concat as uint8ArrayConcat } from "uint8arrays/concat";
+import { toString as uint8ArrayToString } from "uint8arrays/to-string";
+import all from "it-all";
 
 export async function IPFS_Add(
   content: string,
@@ -12,9 +12,12 @@ export async function IPFS_Add(
   const { cid } = await client.add(content);
   return cid;
 }
-
-export async function IPFS_CAT(cid: string, params: IPFSParams): Promise<string> {
+//DEPRECATED, THIS WAS WAY TOO SLOW COMPARED TO A FETCH
+export async function IPFS_CAT(
+  cid: string,
+  params: IPFSParams
+): Promise<string> {
   const client = create(params);
-  const data = uint8ArrayConcat(await all(client.cat(cid)))
+  const data = uint8ArrayConcat(await all(client.cat(cid)));
   return uint8ArrayToString(data);
 }
