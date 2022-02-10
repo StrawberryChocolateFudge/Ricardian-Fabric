852ad84db99be0c98db9d651bf7841db2282089e
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-10 16:40:30 +0000

    added smart contracts to my proposals

--- lib/business/actions/myProposalsActions.ts
+++ lib/business/actions/myProposalsActions.ts
@@ -2,8 +2,9 @@ import {
   dispatch_removeLoadingIndicator,
   dispatch_renderError,
   dispatch_renderLoadingIndicator,
-  dispatch_renderMyProposals,
+  dispatch_renderMyAcceptedSmartContractProposals,
   dispatch_renderMyRankProposals,
+  dispatch_renderMySmartContractProposals,
 } from "../../dispatch/render";
 import {
   checkNetwork,
@@ -18,28 +19,35 @@ import {
   dispatch_setPopupState,
 } from "../../dispatch/stateChange";
 import {
+  AcceptedSmartContractProposal,
   MyProposals,
   PageState,
   PaginatedProposal,
   PopupState,
   RankProposal,
+  SmartContractProposal,
   State,
   Status,
 } from "../../types";
 import {
   closeRankProposal,
+  closeSmartContractProposal,
+  getAcceptedSmartContractProposalsPaginated,
   getCatalogDAOContractWithWallet,
   getMyProposals,
   getMyRankProposalsPaginated,
+  getMySmartContractProposalsPaginated,
 } from "../../wallet/catalogDAO/contractCalls";
 import {
   getProposals,
+  hasError,
   OptionsBuilder,
   proposalsToFetch,
   startPaginatingAProposal,
 } from "../utils";
 import { getById } from "../../view/utils";
 import { Contract } from "web3-eth-contract";
+import { addContractDetailsPopup } from "./reviewAndVote";
 
 export async function myProposalsActions(props: State) {
   if (!web3Injected()) {
@@ -80,26 +88,31 @@ export async function myProposalsActions(props: State) {
     myProposals.rank.slice().reverse(),
     1
   );
+
   await rankFetcher(paginatedRank, catalogDAO, myAddress, props, blockNumber);
-  // const smartCProposalsToFetch = proposalsToFetch(
-  //   paginatedProposalData.smartContract
-  // );
+  const paginatedSmartContract = startPaginatingAProposal(
+    myProposals.smartContract.slice().reverse(),
+    1
+  );
+  await smartContractProposalFetcher(
+    paginatedSmartContract,
+    catalogDAO,
+    myAddress,
+    props,
+    blockNumber
+  );
 
-  // const smartCProposals = await getProposals<SmartContractProposal[]>(
-  //   catalogDAO,
-  //   myAddress,
-  //   smartCProposalsToFetch,
-  //   getMySmartContractProposalsPaginated
-  // );
-
-  // const acceptedToFetch = proposalsToFetch(paginatedProposalData.accepted);
-
-  // const acceptedProposals = await getProposals<AcceptedSmartContractProposal[]>(
-  //   catalogDAO,
-  //   myAddress,
-  //   acceptedToFetch,
-  //   getAcceptedSmartContractProposalsPaginated
-  // );
+  const paginatedAcceptedContract = startPaginatingAProposal(
+    myProposals.acceptedSCProposals.slice().reverse(),
+    1
+  );
+  await acceptedSmartContractProposalFetcher(
+    paginatedAcceptedContract,
+    catalogDAO,
+    myAddress,
+    props,
+    blockNumber
+  );
 
   // const removalToFetch = proposalsToFetch(paginatedProposalData.removal);
 
@@ -111,21 +124,7 @@ export async function myProposalsActions(props: State) {
   // );
 
   // dispatch_removeLoadingIndicator("my-proposals-container");
-  // dispatch_renderMyProposals(
-  //   props,
-  //   paginatedProposalData,
-  //   {
-  //     rankIndexes: rankProposalsToFetch,
-  //     rank: rankProposals,
-  //     smartContractIndexes: smartCProposalsToFetch,
-  //     smartContract: smartCProposals,
-  //     acceptedIndexes: acceptedToFetch,
-  //     accepted: acceptedProposals,
-  //     removalIndexes: removalToFetch,
-  //     removal: removalProposals,
-  //   },
-  //   blockNumber
-  // );
+  addContractDetailsPopup(props);
 }
 
 export async function rankFetcher(
@@ -148,6 +147,178 @@ export async function rankFetcher(
     rankProposalsToFetch,
     paginatedRank,
   ]);
+}
+
+export async function smartContractProposalFetcher(
+  paginatedSmartContract: PaginatedProposal,
+  catalogDAO: Contract,
+  myAddress: string,
+  props: State,
+  blockNumber: number
+) {
+  const smartContractProposalsToFetch: string[] = proposalsToFetch(
+    paginatedSmartContract
+  );
+
+  const smartContractProposals = await getProposals<SmartContractProposal[]>(
+    catalogDAO,
+    myAddress,
+    smartContractProposalsToFetch,
+    getMySmartContractProposalsPaginated
+  );
+
+  dispatch_renderMySmartContractProposals(props, blockNumber, [
+    smartContractProposals,
+    smartContractProposalsToFetch,
+    paginatedSmartContract,
+  ]);
+}
+
+export async function acceptedSmartContractProposalFetcher(
+  paginatedContract: PaginatedProposal,
+  catalogDAO: Contract,
+  myAddress: string,
+  props: State,
+  blockNumber: number
+) {
+  const acceptedProposalsToFetch: string[] =
+    proposalsToFetch(paginatedContract);
+
+  const acceptedProposals = await getProposals<AcceptedSmartContractProposal[]>(
+    catalogDAO,
+    myAddress,
+    acceptedProposalsToFetch,
+    getAcceptedSmartContractProposalsPaginated
+  );
+
+  dispatch_renderMyAcceptedSmartContractProposals(props, blockNumber, [
+    acceptedProposals,
+    acceptedProposalsToFetch,
+    paginatedContract,
+  ]);
+}
+
+export async function myAcceptedSmartContractProposalTableActions(
+  props: State,
+  indexes: string[]
+) {
+  const removeButtons = document.getElementsByClassName(
+    "contract-remove-button"
+  );
+  const claimRewardButtons = document.getElementsByClassName(
+    "contract-claim-reward-button"
+  );
+
+  const paginationButtons = document.getElementsByClassName(
+    "myAcceptedSmartContractPaginationButton"
+  );
+  const pageLeftButton = getById("acceptedcontract-page-left");
+  const pageRightButton = getById("acceptedcontract-page-right");
+
+  //TODO:
+}
+
+export async function mySmartContractProposalsTableActions(
+  props: State,
+  smartContractProposalIndexes: string[]
+) {
+  const closeSmartContractButtons = document.getElementsByClassName(
+    "smartContractProposalButtonId"
+  );
+  const paginationButtons = document.getElementsByClassName(
+    "mySmartContractPaginationButton"
+  );
+
+  const pageLeftButton = getById("smartcontract-page-left");
+  const pageRightButton = getById("smartcontract-page-right");
+
+  const catalogDAO = await getCatalogDAOContractWithWallet();
+  const myAddressOptions = await OptionsBuilder(() => getAddress());
+  if (hasError(myAddressOptions)) {
+    return;
+  }
+  const myAddress = myAddressOptions.data;
+
+  for (let i = 0; i < closeSmartContractButtons.length; i++) {
+    const button = closeSmartContractButtons[i] as HTMLButtonElement;
+    button.onclick = async function () {
+      const index = button.dataset.proposalindex;
+
+      const onError = (error, receipt) => {
+        dispatch_renderError(error.message);
+      };
+      const onReceipt = (receipt) => {
+        dispatch_setPage(PageState.ManageProposals);
+      };
+      await closeSmartContractProposal(
+        catalogDAO,
+        index,
+        myAddress,
+        onError,
+        onReceipt
+      );
+    };
+  }
+
+  // add the onclick for the smart contract pagination buttons
+
+  for (let i = 0; i < paginationButtons.length; i++) {
+    const paginationButton = paginationButtons[i] as HTMLButtonElement;
+    paginationButton.onclick = async function () {
+      const pageIndex = parseInt(paginationButton.dataset.smartcontractpage);
+      const blockNumber = await getBlockNumber();
+      const paginatedSmartContract = startPaginatingAProposal(
+        smartContractProposalIndexes,
+        pageIndex
+      );
+      await smartContractProposalFetcher(
+        paginatedSmartContract,
+        catalogDAO,
+        myAddress,
+        props,
+        blockNumber
+      );
+    };
+  }
+
+  pageLeftButton.onclick = async function () {
+    const index = parseInt(pageLeftButton.dataset.smartcontractpage);
+    if (index > 1) {
+      const blockNumber = await getBlockNumber();
+      const paginatedSmartcontract = startPaginatingAProposal(
+        smartContractProposalIndexes,
+        index - 1
+      );
+
+      await smartContractProposalFetcher(
+        paginatedSmartcontract,
+        catalogDAO,
+        myAddress,
+        props,
+        blockNumber
+      );
+    }
+  };
+
+  pageRightButton.onclick = async function () {
+    const index = parseInt(pageRightButton.dataset.smartcontractpage);
+    const total = parseInt(pageRightButton.dataset.totalpages);
+    if (index < total) {
+      const blockNumber = await getBlockNumber();
+      const paginatedSmartContractProposal = startPaginatingAProposal(
+        smartContractProposalIndexes,
+        index + 1
+      );
+
+      await smartContractProposalFetcher(
+        paginatedSmartContractProposal,
+        catalogDAO,
+        myAddress,
+        props,
+        blockNumber
+      );
+    }
+  };
 }
 
 export async function myRankProposalsTableActions(
