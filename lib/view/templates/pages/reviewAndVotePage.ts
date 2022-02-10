21eb57d17ce8fbdf1a09c2de68e4be24a1a86b2d
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-10 16:37:36 +0000

    added voting on smart contracts

--- lib/view/templates/pages/reviewAndVotePage.ts
+++ lib/view/templates/pages/reviewAndVotePage.ts
@@ -1,5 +1,10 @@
 import { html } from "lit-html";
-import { RankProposal, RemovalProposal } from "../../../types";
+import {
+  PaginatedProposal,
+  RankProposal,
+  RemovalProposal,
+  SmartContractProposal,
+} from "../../../types";
 import { VOTINGPERIODBLOCKS } from "../../../wallet/catalogDAO/contractCalls";
 import { getBlockie } from "../components/getBlockies";
 import {
@@ -13,6 +18,7 @@ import {
 } from "../components/logos";
 import {
   getRankPagingButtons,
+  getSmartContractPagingButtons,
   GetStatus,
   getStatusCondition,
 } from "./manageProposals";
@@ -44,6 +50,8 @@ export function ReviewAndVote() {
     </div>
 
     <div id="rank-proposal-table"></div>
+    <hr />
+    <div id="smart-contract-proposal-table"></div>
   `;
 }
 
@@ -60,19 +68,19 @@ export function RankProposalTable(
       rankProposalTRs.push({ rank: ranks[i], index: rankIndexes[i] });
     }
   }
-
   return html`
     <hr />
     <h5>New Rank</h5>
     <hr />
     <table class="light-shadow width-100Percent">
       <tr>
-        <td><label>Address</label></td>
-        <td><label>Repository</label></td>
+        <td><label>Index</label></td>
+        <td><label>Creator</label></td>
+        <td><label>Link</label></td>
         <td><label>Approve</label></td>
         <td><label>Reject</label></td>
         <td><label>Status</label></td>
-        <td><label>Block</label></td>
+        <td></td>
       </tr>
       ${rankProposalTRs.map((r) =>
         rankProposalTR(r.rank, r.index, blockNumber)
@@ -94,21 +102,11 @@ function rankProposalTR(
   blockNumer: number
 ) {
   const finished = getStatusCondition(blockNumer, rankProposal.createdBlock);
-  let approvalCSS = "";
-  let rejectionCSS = "";
-  const approvals = parseInt(rankProposal.approvals);
-  const rejections = parseInt(rankProposal.rejections);
+  const [approvalCSS, rejectionCSS] = getCSS(
+    { approvals: rankProposal.approvals, rejections: rankProposal.rejections },
+    finished
+  );
 
-  if (finished) {
-    if (approvals < 10) {
-      // If the approvals are less than 10, its rejected;
-      rejectionCSS = "background-lightcoral";
-    } else if (rejections > approvals) {
-      rejectionCSS = "background-lightcoral";
-    } else if (approvals > rejections) {
-      approvalCSS = "background-lightgreen";
-    }
-  }
   return html`<tr>
       <td><hr /></td>
       <td><hr /></td>
@@ -117,6 +115,7 @@ function rankProposalTR(
       <td><hr /></td>
     </tr>
     <tr>
+      <td><label>${index}</label></td>
       <td>${getBlockie(rankProposal.creator, "50px", "")}</td>
       <td>
         <a
@@ -154,13 +153,8 @@ function rankProposalTR(
           : GetStatus(blockNumer, rankProposal.createdBlock)}
       </td>
       <td>
-        <div
-          title="${getExpiresElementTitle(
-            rankProposal.createdBlock,
-            blockNumer
-          )}"
-        >
-          ${rankProposal.createdBlock}
+        <div>
+          ${getExpiresElementTitle(rankProposal.createdBlock, blockNumer)}
         </div>
       </td>
     </tr>`;
@@ -168,38 +162,216 @@ function rankProposalTR(
 
 function getExpiresElementTitle(createdBlock: string, blockNumber: number) {
   if (!getStatusCondition(blockNumber, createdBlock)) {
-    return `Expires in ${
-      parseInt(createdBlock) + VOTINGPERIODBLOCKS - blockNumber
-    } blocks.`;
+    return html`<small
+      >${parseInt(createdBlock) + VOTINGPERIODBLOCKS - blockNumber} blocks
+      left.</small
+    >`;
   } else {
-    return "Expired";
+    return "";
   }
 }
 
-export function SmartContractProposalsTable() {
-  return html`    </h2>
+export function SmartContractProposalsTable(
+  blockNumber: number,
+  smartContracts: SmartContractProposal[],
+  indexes: string[],
+  paging: PaginatedProposal
+) {
+  let smartContractProposalTRs = [];
+  for (let i = 0; i < indexes.length; i++) {
+    if (indexes[i] !== "0") {
+      smartContractProposalTRs.push({
+        smartContract: smartContracts[i],
+        index: indexes[i],
+      });
+    }
+  }
+
+  return html` <hr />
     <h5>New smart contract proposals</h5>
     <hr />
-    <table class="light-shadow">
+    <table class="light-shadow width-100Percent">
       <tr>
-        <td><label>Id</label></td>
-        <td><label>Rank</label></td>
-        <td><label>Category</label></td>
-        <td><label>Compare</label></td>
-        <td><label>Name</label></td>
+        <td><label>Index</label></td>
+        <td><label>Creator</label></td>
+        <td><label>Contract</label></td>
+        <td><label>Front end</label></td>
+        <td><label>Fees</label></td>
+        <td><label>Update </label></td>
         <td><label>Approve</label></td>
         <td><label>Reject</label></td>
+        <td><label>Status</label></td>
+        <td></td>
       </tr>
-      <tr>
-        <td>${getBlockie("asf", "50px", "")}</td>
-        <td>1</td>
-        <td>Tokens</td>
-        <td><button class="labelButton">Artifact</button></td>
-        <td><button class="labelButton">HRC-20 token</button></td>
-        <td><button class="labelButton">${ThumbsUp()}</button></td>
-        <td><button class="labelButton">${ThumbsDown()}</button></td>
-      </tr>
-    </table>`;
+      ${smartContractProposalTRs.map((c) =>
+        smartContractProposalTR(c.smartContract, c.index, blockNumber)
+      )}
+    </table>
+    <div>
+      ${getSmartContractPagingButtons(
+        paging.totalPages,
+        paging.currentPage,
+        "smartContractPagePaginationButton",
+        "smartcontract"
+      )}
+    </div>`;
+}
+
+function smartContractProposalTR(
+  smartContractProposal: SmartContractProposal,
+  index: string,
+  blockNumber: number
+) {
+  const finished = getStatusCondition(
+    blockNumber,
+    smartContractProposal.createdBlock
+  );
+  const [approvalCSS, rejectionCSS] = getCSS(
+    {
+      approvals: smartContractProposal.approvals,
+      rejections: smartContractProposal.rejections,
+    },
+    finished
+  );
+  smartContractProposal.suspicious;
+  return html`<tr>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+      <td><hr /></td>
+    </tr>
+    <tr>
+      <td><label>${index}</label></td>
+      <td>${getBlockie(smartContractProposal.creator, "50px", "")}</td>
+      <td>
+        <button
+          class="contract-page-popup border-none cursor-pointer"
+          data-arweavetx="${smartContractProposal.arweaveTxId}"
+          title="Uploaded proposal details"
+        >
+          ${getBlockie(smartContractProposal.arweaveTxId, "50px", "")}
+        </button>
+      </td>
+      <td>
+        <label>${smartContractProposal.hasFrontend ? "YES" : "NO"}</label>
+      </td>
+      <td>
+        <label>${smartContractProposal.hasFees ? "YES" : "NO"}</label>
+      </td>
+      <td>
+        <label
+          title="${smartContractProposal.isUpdate
+            ? "Update of Accepted Contract number " +
+              smartContractProposal.updateOf
+            : "Not an update"}"
+          >${smartContractProposal.isUpdate
+            ? smartContractProposal.updateOf
+            : "NO"}</label
+        >
+      </td>
+      <td>
+        <button
+          class="labelButton smartContractProposalApproveButton ${approvalCSS}"
+          data-index="${index}"
+          data-arweavetx="${smartContractProposal.arweaveTxId}"
+          title="${smartContractProposal.approvals}"
+          ?disabled=${finished}
+        >
+          ${ThumbsUp()}
+        </button>
+      </td>
+      <td>
+        <button
+          class="labelButton smartContractProposalRejectButton ${rejectionCSS}"
+          data-index="${index}"
+          data-arweavetx="${smartContractProposal.arweaveTxId}"
+          title="${smartContractProposal.rejections}"
+          ?disabled=${finished}
+        >
+          ${ThumbsDown()}
+        </button>
+      </td>
+      <td>
+        ${smartContractProposal.closed
+          ? "Closed"
+          : IfSuspiciousCloseFinished(
+              smartContractProposal,
+              blockNumber,
+              smartContractProposal.createdBlock,
+              index
+            )}
+      </td>
+      <td>
+        <div
+          title="${getExpiresElementTitle(
+            smartContractProposal.createdBlock,
+            blockNumber
+          )}"
+        >
+          ${getExpiresElementTitle(
+            smartContractProposal.createdBlock,
+            blockNumber
+          )}
+        </div>
+      </td>
+    </tr>`;
+}
+function IfSuspiciousCloseFinished(
+  smartContractProposal: SmartContractProposal,
+  blockNumber: number,
+  createdBlock: string,
+  contractindex: string
+) {
+  if (
+    parseInt(smartContractProposal.suspicious) > 9 &&
+    !smartContractProposal.penalized
+  ) {
+    if (getStatusCondition(blockNumber, createdBlock)) {
+      // If the condition that I use inside getStatus is true, it should return finished
+      // In this case I return a button to close the suspicious proposal!
+      return html`<button
+        class="labelButton close-suspicious-proposal-buttons"
+        data-index="${contractindex}"
+      >
+        Close
+      </button>`;
+    } else {
+      return GetStatus(blockNumber, createdBlock);
+    }
+  } else {
+    if (smartContractProposal.penalized) {
+      return html`Penalized`;
+    } else {
+      return GetStatus(blockNumber, createdBlock);
+    }
+  }
+}
+
+function getCSS(
+  smartContractProposal: { approvals: any; rejections: any },
+  finished: boolean
+): [string, string] {
+  let approvalCSS = "";
+  let rejectionCSS = "";
+  const approvals = parseInt(smartContractProposal.approvals);
+  const rejections = parseInt(smartContractProposal.rejections);
+
+  if (finished) {
+    if (approvals < 10) {
+      // If the approvals are less than 10, its rejected;
+      rejectionCSS = "background-lightcoral";
+    } else if (rejections > approvals) {
+      rejectionCSS = "background-lightcoral";
+    } else if (approvals > rejections) {
+      approvalCSS = "background-lightgreen";
+    }
+  }
+  return [approvalCSS, rejectionCSS];
 }
 
 export function RemovalProposalsTable(removalProposals: RemovalProposal[]) {
