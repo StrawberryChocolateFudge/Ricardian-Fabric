0983c947ac398db8c0970ca82527daa3b3a6ef24
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-16 17:04:28 +0000

    dynamic rendering from abi taken from fetched data

--- lib/view/templates/popups/SCContructorPopup.ts
+++ lib/view/templates/popups/SCContructorPopup.ts
@@ -1,14 +1,15 @@
-import { html } from "lit-html";
-import { DeploySC } from "../../../types";
+import { html, nothing } from "lit-html";
 import { findConstructorParameters } from "../../../wallet/web3";
 import { getHRC20Abi } from "../../../wallet/abi/HRC20";
 import { BackLogo } from "../components/logos";
+import { ProposalFormat } from "../../../types";
 
-export function SCConstructorPopup(selected: DeploySC) {
-  let constructorParams;
-  if (selected === DeploySC.HRC20) {
-    constructorParams = findConstructorParameters(getHRC20Abi());
-  }
+export function SCConstructorPopup(selected: ProposalFormat) {
+  //Find the ABI of the proposal and
+
+  const artifact = selected.artifact as any;
+
+  let constructorParams = findConstructorParameters(artifact.abi);
 
   const getTypeFromType = (type: string) => {
     let checkedType = type;
@@ -31,39 +32,42 @@ export function SCConstructorPopup(selected: DeploySC) {
     switch (checkedType) {
       case "bool":
         return "checkbox";
-        break;
       case "string":
         return "text";
-        break;
       case "number":
         return "number";
-        break;
       case "address":
         return "text";
-        break;
       case "byte":
         return "text";
-        break;
       default:
         return "text";
-        break;
     }
   };
 
   const list = constructorParams.map((params) => {
     return html`<tr>
-  <td>
-    <label for="${params.name}-input">${params.name} (${params.type})</label>
-  </td>
-  <td>
-    <input id="${params.name}-input" type="${getTypeFromType(params.type)}" />
-  </td>
-  <td></td>
-</tr>`;
+      <td>
+        <label for="${params.name}-input">${params.name}:</label>
+      </td>
+      <td>
+        <input
+          id="${params.name}-input"
+          type="${getTypeFromType(params.type)}"
+        />
+      </td>
+      <td></td>
+    </tr>`;
   });
-
   return html`
-    <h2>Deploy a smart contract</h2>
+    <h4>Deploy ${selected.name}</h4>
+    ${selected.simpleterms
+      ? html`<small
+          >This contract uses a Ricardian contract. The contract address will be
+          set after deployment</small
+        >`
+      : nothing}
+    <hr />
     <p>Contructor parameters:</p>
     <table>
       <thead>
@@ -77,15 +81,22 @@ export function SCConstructorPopup(selected: DeploySC) {
         ${list}
       </tbody>
     </table>
-    <small>Make sure you enter the correct details.This is non-reversible.</small>
-    <hr />
+    <small
+      >Make sure you enter the correct details.This is non-reversible.</small
+    >
     <div class="row">
       <label for="agree-to-deploy-sc">I agree to the terms.</label>
-      <input id="agree-to-deploy-sc" aria-label="Agree to deploy checkbox" type="checkbox" />
+      <input
+        id="agree-to-deploy-sc"
+        aria-label="Agree to deploy checkbox"
+        type="checkbox"
+      />
     </div>
     <hr />
     <div>
-      <button id="SCConstructBackButton" class="backButton">${BackLogo()} Cancel</button>
+      <button id="SCConstructBackButton" class="backButton">
+        ${BackLogo()} Cancel
+      </button>
       <button id="SCConstructCreateButton" class="NextButton">
         Create contract!
       </button>
