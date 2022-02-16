61bc6ce9229bb4a2bbad9c8c2b05ab8fe908e530
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-16 17:29:06 +0000

    added catalogActions

--- lib/business/actions/catalogActions.ts
+++ lib/business/actions/catalogActions.ts
@@ -1,29 +1,157 @@
-import { dispatch_setSelectedWallet } from "../../dispatch/stateChange";
-import { SelectedWallet, State } from "../../types";
+import {
+  dispatch_renderAvailableContractsToCatalog,
+  dispatch_renderCatalogContractLoadingIndicator,
+  dispatch_renderContractDisplayPage,
+} from "../../dispatch/render";
+import {
+  dispatch_setPopupState,
+  dispatch_setSelectedWallet,
+} from "../../dispatch/stateChange";
+import {
+  getAllCatalogProposals,
+  getCatalogProposalByCategory,
+} from "../../fetch/graphql";
+import {
+  AcceptedSmartContractProposal,
+  ArweaveQueryResult,
+  PopupState,
+  SelectedWallet,
+  State,
+} from "../../types";
 import { getById } from "../../view/utils";
+import {
+  getAllAccepted,
+  getAllRemoved,
+  getCatalogDAOContractWithRPC,
+} from "../../wallet/catalogDAO/contractCalls";
+import { hasError, OptionsBuilder } from "../utils";
 
-export function catalogAction(props: State) {
-  const nextButton = getById("SCIntentNextButton") as HTMLButtonElement;
+export async function catalogAction(props: State) {
+  const categorySelectEl = getById("select-category") as HTMLSelectElement;
 
-  nextButton.onclick = function () {
-    // if (hrc20.checked) {
-    //   dispatch_SCDeploySelected(DeploySC.HRC20);
-    // }
+  // based on what category is selected, I render the contracts
+  const catalogDAOOptions = await OptionsBuilder(() =>
+    getCatalogDAOContractWithRPC()
+  );
+
+  if (hasError(catalogDAOOptions)) {
+    return;
+  }
+
+  const allAcceptedOptions = await OptionsBuilder(() =>
+    getAllAccepted(catalogDAOOptions.data)
+  );
+  if (hasError(allAcceptedOptions)) {
+    return;
+  }
+
+  const allRemovedOptions = await OptionsBuilder(() =>
+    getAllRemoved(catalogDAOOptions.data)
+  );
+  if (hasError(allRemovedOptions)) {
+    return;
+  }
+
+  // compare the removed with the accepted and remove the required ids
+  const [allowedToDisplay, ids] = getAllowedToDisplay(
+    allAcceptedOptions.data,
+    allRemovedOptions.data
+  );
+
+  const getContracts = async function () {
+    dispatch_renderCatalogContractLoadingIndicator(props);
+    const category = categorySelectEl.value;
+    // Now I use the graphQL to get the contracts with the required category
+
+    const uploadedProposals = await fetchContractsPerCategory(category);
+
+    // iterate over the uploaded proposals and check if they are allowed to display
+    const available = filterCategoryUploads(uploadedProposals, ids);
+    setTimeout(
+      () =>
+        dispatch_renderAvailableContractsToCatalog(
+          props,
+          allowedToDisplay,
+          ids,
+          available
+        ),
+      1000
+    );
   };
+
+  await getContracts();
+
+  categorySelectEl.onclick = getContracts;
 }
 
-export function addCatalogButtonListener(props: State) {
-  const smartcontract = getById("smart-contracts-button");
-  //const proposals = getById("view-proposals-button");
-  //const createProp = getById("create-proposal-button");
+function filterCategoryUploads(
+  uploadedProposals: ArweaveQueryResult[],
+  ids: string[]
+): Array<any> {
+  let filtered = [];
+  for (let i = 0; i < uploadedProposals.length; i++) {
+    const prop = uploadedProposals[i];
+    if (ids.includes(prop.node.id)) {
+      filtered.push(prop);
+    }
+  }
+  return filtered;
+}
 
-  smartcontract.onclick = function () {
-    // dispatch_setPopupState(PopupState.Catzalog);
-  };
+async function fetchContractsPerCategory(category: string) {
+  let res;
 
-  // createProp.onclick = function () {
-  //   dispatch_setPopupState(PopupState.createProposal);
-  // };
+  if (category === "All") {
+    res = await getAllCatalogProposals();
+  } else {
+    res = await getCatalogProposalByCategory(category);
+  }
+
+  if (hasError(res)) {
+    return [];
+  } else {
+    return res.data.transactions.edges;
+  }
+}
+
+function getAllowedToDisplay(
+  accepted: Array<AcceptedSmartContractProposal>,
+  removed: Array<AcceptedSmartContractProposal>
+): [Array<AcceptedSmartContractProposal>, Array<string>] {
+  const allowedToDisplay: Array<AcceptedSmartContractProposal> = [];
+  const ids: Array<string> = [];
+  for (let i = 0; i < accepted.length; i++) {
+    const txId = accepted[i].arweaveTxId;
+    let add = true;
+    for (let j = 0; j < removed.length; j++) {
+      const remTxId = removed[j].arweaveTxId;
+      if (txId === remTxId) {
+        add = false;
+      }
+    }
+    if (add) {
+      allowedToDisplay.push(accepted[i]);
+      ids.push(accepted[i].arweaveTxId);
+    }
+  }
+  return [allowedToDisplay, ids];
+}
+
+export function catalogContentActions(props: State) {
+  const smartcontracts = document.getElementsByClassName("contract-page-popup");
+
+  for (let i = 0; i < smartcontracts.length; i++) {
+    const cont = smartcontracts[i] as HTMLButtonElement;
+
+    cont.onclick = function () {
+      const tx = cont.dataset.arweavetxid;
+      const proposal: AcceptedSmartContractProposal = JSON.parse(
+        cont.dataset.proposal
+      );
+      dispatch_setPopupState(PopupState.emptyPopup);
+      dispatch_renderContractDisplayPage(props, tx, false, proposal);
+    };
+  }
 }
 
 export function walletSelectListener() {
