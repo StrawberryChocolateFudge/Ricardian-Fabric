956f3bcf71cba8713124a366133bea2da2e0dffb
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-02-24 13:02:56 +0000

    Updated the logo

--- lib/view/templates/pages/connectWalletPage.ts
+++ lib/view/templates/pages/connectWalletPage.ts
@@ -1,16 +1,16 @@
 import { html } from "lit-html";
-import { ConnectWalletLogo } from "../components/logos";
+import { ConnectWalletLogo, MetamaskLogo } from "../components/logos";
 
 export const ConnectWalletPage = () =>
   html`
     <div class="center height200">
       <button
         id="connectWalletButton"
-        class="labelButton width-200 marginTop-50 unselectable"
+        class="labelButton width-200 marginTop-50 unselectable light-shadow"
       >
         <h4>Connect your wallet</h4>
         <hr />
-        ${ConnectWalletLogo()}
+        ${MetamaskLogo()}
       </button>
     </div>
   `;
